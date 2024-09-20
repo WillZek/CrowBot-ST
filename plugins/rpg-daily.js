304c6d133e0f7f8d482dd767f3dbcde524e65dd8
ianalejandrook15x <sm.iam1836@gmail.com>
2024-09-19 23:21:38 -0300

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -18,7 +18,7 @@ conn.reply(m.chat, `🎁 *Recompensa Diaria*
 Recursos:
 ✨ Xp : *+${isPrems ? exppremium : exp}*
 💎 Diamantes : *+${d}*
-🪙 IanCoins : *+${coin}*`, m, )
+🪙 CrowCoins : *+${coin}*`, m, )
 
 global.db.data.users[m.sender].lastclaim = new Date * 1
 
