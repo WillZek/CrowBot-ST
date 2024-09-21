1cda6c4df5432c8d39c1251091dc2f1fdc39a3c5
will-BOT-MD <ninopina10@gmail.com>
2024-09-20 20:54:34 -0600

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,10 +1,12 @@
-const handler = async (m) => {
-m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝘿𝙊 🥳\n𝘼𝙃𝙊𝙍𝘼 𝙎𝙄 𝙀𝙎𝙏𝙊𝙔 𝘿𝙄𝙎𝙋𝙊𝙉𝙄𝘽𝙇𝙀.\n\n𝙏𝙃𝙄𝙎 𝘾𝙃𝘼𝙏 𝙒𝘼𝙎 𝙐𝙉𝘽𝘼𝙉 😌\n𝙉𝙊𝙒 𝙄 𝘼𝙈 𝘼𝙑𝘼𝙄𝙇𝘼𝘽𝙇𝙀.`)
-global.db.data.chats[m.chat].isBanned = false 
+let handler = async (m, { conn }) => {
+if (!(m.chat in global.db.data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
+  let chat = global.db.data.chats[m.chat]
+  if (!chat.isBanned) return m.reply('*Este chat no está baneado!!*')
+chat.isBanned = false
+conn.reply(m.chat,  '*BOT ONLINE 🚀*', m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: ag, body: '𝐂𝐡𝐚𝐭 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐝𝐨', previewType: 0, thumbnail: imagen4, sourceUrl: [md, yt, tiktok].getRandom()}}}) 
 }
-handler.help = ['unbanchat'];
-handler.tags = ['owner'];
-handler.command = /^unbanchat$/i;
-handler.botAdmin = true
-handler.admin = true
-export default handler;
+handler.command = /^unbanchat$/i
+//handler.botAdmin = true
+handler.rowner = true
+
+export default handler
