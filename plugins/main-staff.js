d3fd058e042ea5700d0e8d0ca0554d32ae13630a
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-02 17:49:54 -0300

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,42 +1,42 @@
-
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `🚩 *EQUIPO DE AYUDANTES*
-💛 *Bot:* ${global.botname}
-✨️  *Versión:* ${global.vs}
-
-👑 *Propietario:*
-
-• Ianalejandrook15x
-💛 *Rol:* Propietario 👑
-💛 *Número:* ${creador}
-✨️ *GitHub:* https://github.com/ianalejandrook15x
-
-💛COLABORADORES
-
-• Miguel
-💛 *Rol:* Desarrollador ☁
-💛 *Número:* 543876631319
-✨️ *GitHub:* ---
-await conn.sendFile(m.chat, icons, 'nino.jpg', staff.trim(), fkontak, true, {
-contextInfo: {
-'forwardingScore': 200,
-'isForwarded': false,
-externalAdReply: {
-showAdAttribution: true,
-renderLargerThumbnail: false,
-title: `🥷 Developers 👑`,
-body: `🚩 Staff Oficial`,
-mediaType: 1,
-sourceUrl: redes,
-thumbnailUrl: icono
-}}
-}, { mentions: m.sender })
-m.react(emoji)
-
-}
-handler.help = ['staff']
-handler.command = ['colaboradores', 'staff']
-handler.register = true
-handler.tags = ['main']
-
-export default handler
+    let staff = `EQUIPO DE AYUDANTES DEL BOT
+    💛 *Bot:* ${botname}
+    💛 *Versión:* ${vs}
+    💛 *Libreria:* ${libreria + baileys}
+    
+    👑 *Pɾσριҽƚαɾισ:*
+    
+    • 
+    💛 *Rol:* Nιñσ Pιñα
+    💛 *Número:* ${creador}
+    💛 *GitHub:* https://github.com/Willzek
+    
+    🍄  *Colaboradores:*
+    
+    • ιαɳαʅҽʝαɳԃɾσσƙ15x
+    💛 *Rol:* Colaborador
+    💛 *Número:* Wa.me/5493876639332
+    💛 *GitHub:* https://github.com/ianalejandrook15x`
+    await conn.sendFile(m.chat, icons, 'crow.jpg', staff.trim(), fkontak, true, {
+    contextInfo: {
+    'forwardingScore': 200,
+    'isForwarded': false,
+    externalAdReply: {
+    showAdAttribution: true,
+    renderLargerThumbnail: false,
+    title: `👑 Developers 👑`,
+    body: `🌟 STAFF DEL BOT`,
+    mediaType: 1,
+    sourceUrl: redes,
+    thumbnailUrl: icono
+    }}
+    }, { mentions: m.sender })
+    await m.react(emoji)
+    
+    }
+    handler.help = ['staff']
+    handler.command = ['colaboradores', 'staff']
+    handler.register = true
+    handler.tags = ['main']
+    
+    export default handler
