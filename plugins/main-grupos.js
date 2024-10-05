8ac4a2b704c25c0c7f66871e6868fc37577ca98b
Niño Piña <ninopina10@gmail.com>
2024-10-04 22:05:12 -0600

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,9 +1,8 @@
-
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
 
-let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* 💛
+let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad oficial* 💛
 
 1- 『✯ 𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗖𝗿𝗼𝘄𝗕𝗼𝘁✯』
 *✰* ${grupo}
@@ -15,7 +14,14 @@ let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para con
 ☁ Canal :
 *✰* ${channel}
 
-> ${dev}`
+> ${dev}
+
+*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
+ • ¿Te interesa tener un bot propio, bot para grupo o un curso de como hacer bots personalizados? ¡Únete a nuestro grupo de ventas para solicitar un servicio! 📝
+ [ \`https://chat.whatsapp.com/HAt711AdgXFG1BI9FIACKr\` ]
+ 
+ • Pregunta por lo que quieras!💛
+Beneficios: Soporte Perzonalizado, Asistencia 24/7, Grupo VIP🏆🌟`
 
 await conn.sendFile(m.chat, imagen2, "Crow.jpg", grupos, m, null, rcanal)
 
