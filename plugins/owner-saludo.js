3e53e0991d15477c6d76a0dc2b4584cf567d2f30
Niño Piña <ninopina10@gmail.com>
2024-10-27 14:15:43 -0600

    Actualizar owner-saludo.js

--- plugins/owner-saludo.js
+++ plugins/owner-saludo.js
@@ -1,9 +1,5 @@
 const handler = async (m, {conn, command}) => {
   console.log(`/////////////////////////////////////////////////////////////////\n\nEl único reporte con fallos en este comando, no se presenta aquí.\n\n/////////////////////////////////////////////////////////////////`);
-let handler = async (m, { conn }) => {
-    // React con un emoji al mensaje
-    m.react('🥰');
-
   m.reply('*[🌠] ¡Hola, Saludos!, CrowBot te saludo🥰💛*');
 };
 handler.command = /^(saludo)$/i;
