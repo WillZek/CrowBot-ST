dfb518ad9773f266c8115cf98ec76335aac67337
Niño Piña <ninopina10@gmail.com>
2024-10-27 14:13:26 -0600

    Actualizar owner-saludo.js

--- plugins/owner-saludo.js
+++ plugins/owner-saludo.js
@@ -1,43 +1,11 @@
-//Codígo creado por Niño Piña wa.me/50557865603
-
-import fs from 'fs';
-import path from 'path';
+const handler = async (m, {conn, command}) => {
 
 let handler = async (m, { conn }) => {
     // React con un emoji al mensaje
     m.react('🥰');
-
-    // Lista de mensajes
-    const messages = [
-        "*¡SALUDO! 💛 Espero que tu día esté lleno de alegría y oportunidades brillantes.*\n> ৎ୭࠭͢𝑪𝒓𝒐𝒘𝑩𝒐𝒕𓆪͟͞ ",
-        "*¡SALUDO! ☀️ Que este nuevo día te traiga sonrisas y momentos inolvidables.*\n> ৎ୭࠭͢𝑪𝒓𝒐𝒘𝑩𝒐𝒕𓆪͟͞ ",
-        "*¡SALUDO! 🌟 Espero que hoy encuentres belleza en cada pequeño detalle.*\n> ৎ୭࠭͢𝑪𝒓𝒐𝒘𝑩𝒐𝒕𓆪͟͞ ",
-        "*¡SALUDO! ✨ Que este día esté lleno de inspiración y que cada paso te acerque a tus sueños.*\n> ৎ୭࠭͢𝑪𝒓𝒐𝒘𝑩𝒐𝒕𓆪͟͞ ",
-        "*¡SALUDO! 🌷 Espero que hoy sea un día lleno de luz y amor.*\n> ৎ୭࠭͢𝑪𝒓𝒐𝒘𝑩𝒐𝒕𓆪͟͞ ",
-        "*¡SALUDO! 👑 Que el día de hoy esté lleno de alegría y oportunidades para crecer.*\n> ৎ୭࠭͢𝑪𝒓𝒐𝒘𝑩𝒐𝒕𓆪͟͞ "
-    ];
-
-    // Seleccionar un mensaje aleatorio
-    let randomMessage = messages[Math.floor(Math.random() * messages.length)];
-
-    if (m.isGroup) {
-        let imagenes = [
-'https://qu.ax/tPREp.jpg',                  'https://qu.ax/PkVwv.jpg',             'https://qu.ax/LgBRM.jpg',
-'https://qu.ax/NoDdH.jpg',
-'https://qu.ax/xlqkf.jpg',
-        ];
-
-        // Seleccionar una imagen aleatoria
-        const imagen = imagenes[Math.floor(Math.random() * imagenes.length)];
-
-        // Envía la imagen y el mensaje correspondiente
-        conn.sendMessage(m.chat, { imagen: { url: imagenes }, gifPlayback: true, caption: randomMessage }, { quoted: m });
-    }
-}
-
-handler.help = ['saludo/Greeting'];
-handler.tags = ['owner'];
-handler.command = ['saluda','crowsaluda','saludos','saludo','saludar'];
-handler.group = true;
-
+  console.log(`/////////////////////////////////////////////////////////////////\n\nEl único reporte con fallos en este comando, no se presenta aquí.\n\n/////////////////////////////////////////////////////////////////`);
+  m.reply('*[ 🌹 ] Hmm, parece que me necesitas. Qué inesperado... 😒.*');
+};
+handler.command = /^(saludo)$/i;
+handler.owner = true;
 export default handler;
