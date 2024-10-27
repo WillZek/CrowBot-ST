e6e2e9ea2c30eb07a94a3ada51698691751f3e5f
Niño Piña <ninopina10@gmail.com>
2024-10-27 09:15:11 -0600

    Actualizar owner-saludo.js

--- plugins/owner-saludo.js
+++ plugins/owner-saludo.js
@@ -1,6 +1,7 @@
 const handler = async (m, {conn, command}) => {
-  console.log(`/////////////////////////////////////////////////////////////////\n\nEl único reporte con fallos en este comando, no se presenta aquí.\n\n/////////////////////////////////////////////////////////////////`);
-  m.reply('*[ 🌠 ] CrowBot Te Saluda🥰.*');
+console.log(`/////////////////////////////////////////////////////////////////\n\nEl único reporte con fallos en este comando, no se presenta aquí.\n\n/////////////////////////////////////////////////////////////////`);
+const crowImageUrl = 'https://qu.ax/tPREp.jpg'; // Reemplaza esto con la URL de la imagen del cuervo
+m.reply(`*[ 🌠 ] CrowBot Te Saluda🥰.*\n![Cuervo](${crowImageUrl})`);
 };
 handler.command = /^(saludo)$/i;
 handler.owner = true;
