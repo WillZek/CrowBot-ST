37d1f462aecdb57e539bba6e4a5a375288706ddd
Niño Piña <ninopina10@gmail.com>
2024-10-27 17:10:41 -0600

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -4,32 +4,30 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 
 let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad oficial* 💛
 
-1- 『✯ 𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗖𝗿𝗼𝘄𝗕𝗼𝘁✯』
+1- 『✯ ['⏤͟͞ू⃪𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗖𝗿𝗼𝘄𝗕𝗼𝘁⁞࿐✯』
 *✰* ${grupo}
 
 *─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
 
 ➠ Enlace anulado? entre aquí! 
 
- ['⏤͟͞ू⃪ ፝͜⁞ *Canal* ✰⃔࿐', :
+[🌠] 𓆩 *Canal* ⁞✰⃔࿐:
 *✰* ${channel}
 
 > ${dev}
 
 *─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
- • ¿Te interesa tener un bot propio, bot para grupo o un curso de como hacer bots personalizados? ¡Únete a nuestro grupo de ventas para solicitar un servicio! 📝
- [ \`https://chat.whatsapp.com/HAt711AdgXFG1BI9FIACKr\` ]
+ • *¿Te interesa tener un bot propio, bot para grupo o un curso de como hacer bots personalizados? ¡Únete a nuestro grupo de ventas para solicitar un servicio! 📝*
+[`https://chat.whatsapp.com/HAt711AdgXFG1BI9FIACKr`]
  
- • Pregunta por lo que quieras!💛
-Beneficios: Soporte Perzonalizado, Asistencia 24/7, Grupo VIP🏆🌟`
-> ⏤͟͞ू⃪ ፝͜⁞𝐂𝐫𝐨𝐰𝐁𝐨𝐭✰⃔࿐',
+ • *Pregunta por lo que quieras!💛
+Beneficios: Soporte Perzonalizado, Asistencia 24/7, Grupo VIP🏆🌟*`
 
 await conn.sendFile(m.chat, imagen2, "Crow.jpg", grupos, m, null, rcanal)
 
 await m.react(emojis)
 
 }
-
 handler.help = ['grupos']
 handler.tags = ['main']
 handler.command = ['grupos', 'iangrupos', 'gruposian']
