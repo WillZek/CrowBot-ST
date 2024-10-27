f6ccecab8fcf60890c802da4bd0d20a7382f27ce
Niño Piña <ninopina10@gmail.com>
2024-10-27 07:58:32 -0600

    Actualizar gc-buenos_dias.js

--- plugins/gc-buenos_dias.js
+++ plugins/gc-buenos_dias.js
@@ -26,12 +26,12 @@ str = `${name2} buenos días a todos en el grupo, les deseo un gran día.`.trim(
 if (m.isGroup) {
 let pp = 'https://giphy.com/gifs/frases-addy-rivera-e44f3WbMgScPpxtRlq';
 let pp2 = 'https://giphy.com/clips/studiosoriginals-feliz-dia-FdgSPh8btKTh01vc8q';
-let pp3 = 'https://media.giphy.com/media/3o6Mbxk1k8b4i5cIYw/giphy.gif';
-let pp4 = 'https://media.giphy.com/media/26tPoyDhjiY8r0g4U/giphy.gif';
-let pp5 = 'https://media.giphy.com/media/l0K4q8tH7qz5y3t9q/giphy.gif';
-let pp6 = 'https://media.giphy.com/media/3o7aD4n3a4H8y4Gq8Y/giphy.gif';
-let pp7 = 'https://media.giphy.com/media/3o6Zt6p7d2V7zP8cK0/giphy.gif';
-let pp8 = 'https://media.giphy.com/media/26gJd7Y5J7Y6p6v7y/giphy.gif';
+let pp3 = 'https://tenor.com/es-419/view/buenos-dias-buenos-dias-amor-buenos-dias-jueves-buenos-dias-mi-amor-good-morning-gif-17200654644161685472';
+let pp4 = 'https://tenor.com/es-419/view/lindo-dia-buenos-deseos-gif-8402695432373629989';
+let pp5 = 'https://tenor.com/es-419/view/goodbye-thank-you-jerry-the-mouse-waving-hi-gif-17526061193307625878';
+let pp6 = 'https://tenor.com/es-419/view/winnie-the-pooh-good-morning-gm-gif-3036137020568944157';
+let pp7 = 'https://tenor.com/es-419/view/gm-goodmorning-morning-good-morning-g-m-gif-3477492737627610981';
+let pp8 = 'https://tenor.com/es-419/view/good-morning-cute-coffee-gif-8173999760206959925';
 const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8];
 const video = videos[Math.floor(Math.random() * videos.length)];
 // Enviamos el mensaje con el video y el mensaje correspondiente
