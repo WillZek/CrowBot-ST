f0c712ab42b4e4fb2e3bc95aa669cd39599aa818
Niño Piña <ninopina10@gmail.com>
2024-10-28 12:37:53 -0600

    Actualizar plugins

---
+++ plugins/Obtenerbot.js
@@ -0,0 +1,21 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.text);
+};
+handler.command = /^(tenerunbot)$/i;
+handler.tags =['main'] 
+handler.help = ['tenerunbot <el bot para tu grupo>'] 
+export default handler;
+global.text =``*¿QUIERES A CROWBOT EN TU GRUPOS?*`🔥 
+
+!! *¡Sencillo!, lo único que pedimos :*!!🙂‍↔️
+
+Q haga  que 10 amigos o conocidos sigan el canal, una vez que lo hagan le dices que te mandé cap de que ya siguen el canal y me lo reenvías junto con el link de tus grupos a donde quieras el bot_ 🎁
+
+*enviar los captures a este número* +505 5786 5603.
+
+*si no te responden en ese número unete al siguiente grupo* https://chat.whatsapp.com/DrQmZfEMSWiK2COjH3Xqq7
+
+*Link de canal que deben seguir* 👇
+
+https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t
+`;
