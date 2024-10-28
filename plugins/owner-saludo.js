3213635c99966940617510d2248bb4f72ef8c968
Niño Piña <ninopina10@gmail.com>
2024-10-28 12:46:31 -0600

    Actualizar owner-saludo.js

--- plugins/owner-saludo.js
+++ plugins/owner-saludo.js
@@ -1,3 +1,5 @@
+//Código Creado Por Niño Piña: wa.me/50557865603
+
 const handler = async (m, {conn, command}) => {
   console.log(`/////////////////////////////////////////////////////////////////\n\nEl único reporte con fallos en este comando, no se presenta aquí.\n\n/////////////////////////////////////////////////////////////////`);
   m.reply('*[🌠] ¡Hola, Saludos!, CrowBot te saludo🥰💛*');
