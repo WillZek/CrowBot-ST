c83d917f8632421e8af5d5d368560790409ad348
Niño Piña <ninopina10@gmail.com>
2024-10-29 22:19:02 -0600

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,12 +1,15 @@
-//import db from '../lib/database.js'
-
-let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = false
-    m.reply('✅ Bot activo  en este grupo')   
+let handler = async (m, { conn }) => {
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🎌 *¡Este chat no está registrado!*', m, fake)
+let chat = global.db.data.chats[m.chat]
+if (!chat.isBanned) return conn.reply(m.chat, '🌸 *El bot no está baneada en este chat*', m, fake)
+chat.isBanned = false
+await conn.reply(m.chat, '🚩 *IanBot-MD ya fué desbaneado en este chat*', m, fake)
 }
-handler.help = ['unbanchat']
-handler.tags = ['owner']
-handler.command = ['chaton', 'unbanchat'] 
+handler.help = ['unbanchat'];
+handler.tags = ['grupo'];
+handler.command = ['unbanchat','desbanearchat','desbanchat']
+handler.admin = true 
+handler.botAdmin = true
+handler.group = true
 
 export default handler
