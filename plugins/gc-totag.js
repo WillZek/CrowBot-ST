06f97269e366fff94687afb7a3dfd0879fe7d178
Niño Piña <ninopina10@gmail.com>
2024-11-03 09:21:42 -0600

    Actualizar gc-totag.js

--- plugins/gc-totag.js
+++ plugins/gc-totag.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, participants}) => {
 
 handler.help = ['totag']
 handler.tags = ['group']
-handler.command = /^(totag|tag)$/i
+handler.command = /^(totag|tag|n)$/i
 
 handler.admin = true
 handler.group = true
