822ae9e304676426943d9be7cac1d21dbabe0b02
Niño Piña <ninopina10@gmail.com>
2024-11-15 15:34:39 -0600

    Actualizar owner-sugerencia.js

--- plugins/owner-sugerencia.js
+++ plugins/owner-sugerencia.js
@@ -1,13 +1,21 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw '⚠ *_️Ingrese la sugerencia que desea enviar._*'
-if (text.length < 10) throw '⚠️ *_Especifique bien la sugerencia, mínimo 10 caracteres._*'
-if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar la sugerencia._*'
-const teks = `╭───────────────────\n│⊷〘 *S U G E R E N C I A* 🌟 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`
-// Aquí se envía el mensaje al grupo
-await conn.sendMessage('120363361152463013@g.us', teks, { mentions: conn.parseMention(teks) })
-m.reply('⚠️ *_La sugerencia se envió al grupo, gracias por colaborar!_*')
+// Verifica si se ingresó texto
+if (!text) throw '⚠ *_️Ingrese la sugerencia que desea enviar._*';
+// Verifica la longitud del texto
+if (text.length < 10) throw '⚠️ *_Especifique bien la sugerencia, mínimo 10 caracteres._*';
+if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar la sugerencia._*';
+// Formatea el mensaje de la sugerencia
+const teks = `╭───────────────────\n│⊷〘 *S U G E R E N C I A* 🌟 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`;
+// Envía el mensaje al grupo
+try {
+await conn.sendMessage('HFYmBzD7MJvLwXpr5CmUqF@g.us', teks, { mentions: conn.parseMention(teks) });
+m.reply('⚠️ *_La sugerencia se envió al grupo, gracias por colaborar!_*');
+} catch (error) {
+console.error(error);
+m.reply('⚠️ *_Ocurrió un error al enviar la sugerencia, intenta de nuevo._*');
 }
-handler.help = ['sugerir']
-handler.tags = ['info']
-handler.command = /^(sugerir|sugerencia)$/i
-export default handler
+}
+handler.help = ['sugerir'];
+handler.tags = ['info'];
+handler.command = /^(sugerir|sugerencia)$/i;
+export default handler;
