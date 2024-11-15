4ba77cd579d64acc4960ee8aa96d6e5eb3c9d867
Niño Piña <ninopina10@gmail.com>
2024-11-15 15:30:17 -0600

    Actualizar owner-sugerencia.js

--- plugins/owner-sugerencia.js
+++ plugins/owner-sugerencia.js
@@ -4,7 +4,7 @@ if (text.length < 10) throw '⚠️ *_Especifique bien la sugerencia, mínimo 10
 if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar la sugerencia._*'
 const teks = `╭───────────────────\n│⊷〘 *S U G E R E N C I A* 🌟 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`
 // Aquí se envía el mensaje al grupo
-await conn.sendMessage('HFYmBzD7MJvLwXpr5CmUqF@g.us', teks, { mentions: conn.parseMention(teks) })
+await conn.sendMessage('120363361152463013@g.us', teks, { mentions: conn.parseMention(teks) })
 m.reply('⚠️ *_La sugerencia se envió al grupo, gracias por colaborar!_*')
 }
 handler.help = ['sugerir']
