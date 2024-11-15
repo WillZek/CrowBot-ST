fb1a984e3563336ce1ebf532a77ce045ac0f8c8e
Niño Piña <ninopina10@gmail.com>
2024-11-15 15:24:47 -0600

    Actualizar owner-sugerencia.js

--- plugins/owner-sugerencia.js
+++ plugins/owner-sugerencia.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw '⚠ *_️Ingrese la sugerencia que desea hacer._*'
-if (text.length < 10) throw '🔮 *_Especifique bien la sugerencia, mínimo 10 caracteres._*'
-if (text.length > 1000) throw '🔮 *_Máximo 1000 caracteres para enviar la sugerencia._*'
-const teks = `╭───────────────────\n│⊷〘 *S U G E R E N C I A* 💡 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`
-await conn.reply(global.groupChatId + '@g.us', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
-m.reply('⚠️ *_La sugerencia se envió al grupo, gracias por tu aporte!_*\n💥 ¡Sigue brillando, Niño Piña! 🌟')
+if (!text) throw '⚠ *_️Ingrese la sugerencia que desea enviar._*'
+if (text.length < 10) throw '⚠️ *_Especifique bien la sugerencia, mínimo 10 caracteres._*'
+if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar la sugerencia._*'
+const teks = `╭───────────────────\n│⊷〘 *S U G E R E N C I A* 🌟 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`
+await conn.reply('https://chat.whatsapp.com/HFYmBzD7MJvLwXpr5CmUqF', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+m.reply('⚠️ *_La sugerencia se envió al grupo, gracias por colaborar!_*')
 }
 handler.help = ['sugerir']
 handler.tags = ['info']
-handler.command = /^(sugerencia|sugerir|sugerir|idea)$/i
+handler.command = /^(sugerir|sugerencia)$/i
 export default handler
