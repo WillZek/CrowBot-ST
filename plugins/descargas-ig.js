40fcdfa1235be1e35d65d60b73d40d296b70c338
Niño Piña <ninopina10@gmail.com>
2024-11-15 19:45:18 -0600

    Actualizar descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -15,7 +15,7 @@ let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🎞️ *Tu video de instagram.*\n' + textbot, fkontak)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🎞️ *Tu video de instagram,*reportar número manda cp +51 928 215 461*\n' + textbot, fkontak)
 }} catch {
 await m.react(error)
 conn.reply(m.chat, '⚙️ Ocurrió un error.', m, fake)}}
