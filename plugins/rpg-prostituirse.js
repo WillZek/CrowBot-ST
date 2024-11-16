bf8e142c5f21fbd786269c4619a11abcc04db03d
Niño Piña <ninopina10@gmail.com>
2024-11-16 13:28:17 -0600

    Actualizar rpg-prostituirse.js

--- plugins/rpg-prostituirse.js
+++ plugins/rpg-prostituirse.js
@@ -5,12 +5,12 @@ let user = global.db.data.users[m.sender]
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `🚩 Espera ⏱️ *${tiempo2}* Para Seguir Prostituyendote.`, m, rcanal)
+conn.reply(m.chat, `*⏰ Espera 🌠* *${tiempo2}* *Para Seguir Prostituyendote.*`, m, rcanal)
 return
 }
 let rsl = Math.floor(Math.random() * 5000)
 cooldowns[m.sender] = Date.now()
-await conn.reply(m.chat, `🚩 ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) XP 🍭.`, m, rcanal)
+await conn.reply(m.chat, `🌠 ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) XP 🔮.`, m, rcanal)
 user.exp += rsl
 }
 handler.help = ['prostituirse']
