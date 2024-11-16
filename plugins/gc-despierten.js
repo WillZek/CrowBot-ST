d4ff6f7b8cc4fbfdb42e2682393eb1ebb40c780d
Niño Piña <ninopina10@gmail.com>
2024-11-16 12:50:33 -0600

    Actualizar gc-despierten.js

--- plugins/gc-despierten.js
+++ plugins/gc-despierten.js
@@ -1,17 +1,15 @@
-import fs from 'fs';
-import path from 'path';
 let handler = async (m, { conn }) => {
 // React con un emoji al mensaje
 m.react('🌞');
 // Mensaje que se enviará
 const message = "Levántense webones🥵";
 if (m.isGroup) {
-// Aquí puedes poner la ruta del sticker que deseas enviar
-const stickerPath = 'https://i.ibb.co/g4MHfC3/crow.webp'; // Reemplaza con la ruta o URL del sticker
+// URL del sticker
+const stickerUrl = 'https://i.ibb.co/g4MHfC3/crow.webp';
 // Envía el mensaje
 await conn.sendMessage(m.chat, { text: message }, { quoted: m });
 // Envía el sticker
-await conn.sendMessage(m.chat, { sticker: fs.readFileSync(stickerPath) }, { quoted: m });
+await conn.sendMessage(m.chat, { url: stickerUrl }, { sendMediaAsSticker: true });
 }
 }
 handler.help = ['despertar'];
