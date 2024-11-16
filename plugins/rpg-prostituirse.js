80c54647883c7471caab8644439d6973052545ba
Niño Piña <ninopina10@gmail.com>
2024-11-16 13:21:24 -0600

    Actualizar rpg-prostituirse.js

--- plugins/rpg-prostituirse.js
+++ plugins/rpg-prostituirse.js
@@ -5,7 +5,7 @@ let user = global.db.data.users[m.sender]
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `🚩 Espera ⏱️ *${tiempo2}* para volver a Trabajar.`, m, rcanal)
+conn.reply(m.chat, `🚩 Espera ⏱️ *${tiempo2}* Para Seguir Prostituyendote.`, m, rcanal)
 return
 }
 let rsl = Math.floor(Math.random() * 5000)
@@ -15,7 +15,7 @@ user.exp += rsl
 }
 handler.help = ['prostituirse']
 handler.tags = ['rpg']
-handler.command = ['prostituirse', 'prostituta', 'trabajo', 'chamba']
+handler.command = ['prostituirse', 'prostituta', 'prostituir', 'sentones']
 handler.register = true
 export default handler
 function toNum(number) {
