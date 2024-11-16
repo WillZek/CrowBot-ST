fb7164828652cf615677d63bc1b26d401be63491
Niño Piña <ninopina10@gmail.com>
2024-11-15 22:43:41 -0600

    Actualizar owner-sugerencia.js

--- plugins/owner-sugerencia.js
+++ plugins/owner-sugerencia.js
@@ -12,6 +12,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['sugerencia']
 handler.tags = ['info']
-handler.command = ['sugerencia', 'sug']
+handler.command = ['sugerencia', 'sugerir']
 
 export default handler
