ce2b86d35fe78b061c639390546da23e98481779
Niño Piña <ninopina10@gmail.com>
2024-11-16 10:31:23 -0600

    Actualizar jadibot-deletebot.js

--- plugins/jadibot-deletebot.js
+++ plugins/jadibot-deletebot.js
@@ -13,7 +13,7 @@ await fs.rmdir("./CrowJadiBot/" + uniqid, { recursive: true, force: true })
 await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: fkontak })
 } catch(err) {
 if (err.code === 'ENOENT' && err.path === `./CrowJadiBot/${uniqid}`) {
-await parentw.sendMessage(m.chat, { text: "🌸 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
+await parentw.sendMessage(m.chat, { text: "🌠 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
 } else {
 await m.react(error)
 }}}
