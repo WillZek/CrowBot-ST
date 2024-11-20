2d08e7d900e04387cbdc183e6269d996159e9bc8
Niño Piña <ninopina10@gmail.com>
2024-11-20 15:52:03 -0600

    Actualizar owner-prestamo.js

--- plugins/owner-prestamo.js
+++ plugins/owner-prestamo.js
@@ -1,16 +1,20 @@
 let handler = async(m, { conn, command, text }) => {
-if (!text) return m.reply(`*🚩 Ingresa El Motivo Para Prestar Dinero*`)
-if (text.length < 11) return m.reply(`*🚩Ingresa Almenos 11 características, para prestar dinero*`)
-let texto = `*_💸 El Owner @${m.sender.split`@`[0]} Necesita Que Le Prestes Dinero ৎ୭࠭͢𝐂𝐫𝐨𝐰𝐁𝐨𝐭𓆪͟͞ _*\n*➪ *Motivo*: ${text}*`
-m.reply('*_🚀 Enviando mensaje de préstamo a todos los owners de CrowBot._*')
+  if (!text) return m.reply(`*🌠 Ingresa El Motivo Del Préstamo*`)
+    if (text.length < 11) return m.reply(`*🚩 Ingresa Al Menos 11 Caracteres*`)
+
+let texto = `*_🌠 El Owner @${m.sender.split`@`[0]} Necesita Dinero y a hecho un préstamo a su staff ৎ୭࠭͢𝐂𝐫𝐨𝐰𝐁𝐨𝐭𓆪͟͞ _*\n*➪ *Motivo*: ${text}*`
+m.reply('*_🚀 Enviando mensaje de reunión a todos los owners de CrowBot._*')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
-let data = (await conn.onWhatsApp(jid))[0] || {}
-if (data.exists) {
-conn.sendMessage(data.jid, texto, m)
-}
-}
+                        let data = (await conn.onWhatsApp(jid))[0] || {}
+                        if (data.exists)
+                        conn.sendPayment(data.jid, '999999999', texto, m)
+
+                           // conn.reply(data.jid, text, m, { mentions: [m.sender] })
+                    }
+
 }
 handler.tags = ['owner']
-handler.command = handler.help = ['prestamo', 'prestar']
+handler.command = handler.help =['prestamo','prestar']
 handler.rowner = true
+
 export default handler
