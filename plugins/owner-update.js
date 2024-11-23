c1787ee47f8e563047b071d1fec65ae40b29df77
Niño Piña <ninopina10@gmail.com>
2024-11-22 19:46:40 -0600

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,49 +1,14 @@
 import { execSync } from 'child_process'
-
-var handler = async (m, { conn, text }) => {
-
-m.react('🚀') 
-try {
-
-const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
-let messager = stdout.toString()
-
-if (messager.includes('☘️ Ya estoy actualizada.')) messager = '☘️ Ya estoy actualizada a la última versión.'
-
-if (messager.includes('💫 Actualizando.')) messager = '✨️ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
-conn.reply(m.chat, messager, m, rcanal,)
-
-} catch { 
-try {
-
-const status = execSync('git status --porcelain')
-
-if (status.length > 0) {
-const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
-if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('YoshiSession/') || line.includes('npm-debug.log')) {
-return null
-}
-return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
-if (conflictedFiles.length > 0) {
-const errorMessage = `💭 Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\n\n✰ *ARCHIVOS EN CONFLICTO*\n\n${conflictedFiles.join('\n')}`
-await conn.reply(m.chat, errorMessage, m, rcanal,)
-}
-}
-} catch (error) {
-console.error(error)
-let errorMessage2 = '⚠️ Ocurrió un error inesperado.'
-if (error.message) {
-errorMessage2 += '\n⚠️ Mensaje de error: ' + error.message;
-}
-await conn.reply(m.chat, errorMessage2, m, rcanal,)
-}
-}
-
-}
-
-handler.help = ['update', 'actualizar']
+let handler = async (m, { conn, text }) => {
+await m.react('🕓')
+if (conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+await conn.reply(m.chat, stdout.toString(), m)
+await m.react('✅')
+}}
+handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar']
+handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
 handler.rowner = true
 
 export default handler
