1d82b85fdba951b1a4d115647dbed011d30728ff
Niño Piña <ninopina10@gmail.com>
2024-12-03 20:16:43 -0600

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
 try {
-let res = await fetch('https://api.github.com/repos/Dev-Diego/YaemoriBot-MD')
+let res = await fetch('https://api.github.com/repos/WillZek/CrowBot-ST')
 
 if (!res.ok) throw new Error('Error al obtener datos del repositorio')
 let json = await res.json()
