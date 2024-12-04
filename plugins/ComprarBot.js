ca58ba203b90d4d79d6fcbbfc7281607874c5f06
Niño Piña <ninopina10@gmail.com>
2024-12-03 23:26:49 -0600

    Actualizar ComprarBot.js

--- plugins/ComprarBot.js
+++ plugins/ComprarBot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
 // No Quites Los Créditos🦌
 m.react('🎄');
 // Mensaje que se enviará
-const message = "〔⛄ *CROW - BOT* ❄️〕\n\n> *🦌FELIZ NAVIDAD🎄*\n\n*BOT PARA GRUPO* :\n> wa.me/50557865603\n\n*BOT PERZONALIZADO* :\n> wa.me/50557865603"; m, rcanal)
+const message = "〔⛄ *CROW - BOT* ❄️〕\n\n> *🦌FELIZ NAVIDAD🎄*\n\n*BOT PARA GRUPO* :\n> wa.me/50557865603\n\n*BOT PERZONALIZADO* :\n> wa.me/50557865603";
 if (m.isGroup) {
 // Feliz Navidad🎄
 const imageUrl = 'https://i.ibb.co/qJNL5Bg/file.jpg';
