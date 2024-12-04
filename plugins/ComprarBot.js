2ae525ee40ca484d824f49a537845f2ca39041f5
Niño Piña <ninopina10@gmail.com>
2024-12-03 22:56:43 -0600

    Actualizar ComprarBot.js

--- plugins/ComprarBot.js
+++ plugins/ComprarBot.js
@@ -5,7 +5,7 @@ handler.command = /^(ComprarBot|Comprar|comprar|ComprarBot)$/i;
 export default handler;
 
 global.ComprarBot = `
-〔 *CROW - BOT* 〕
+〔⛄ *CROW - BOT* ❄️〕\n\n> *🦌FELIZ NAVIDAD🎄*
 
 *BOT PARA GRUPO* :
 > wa.me/50557865603
