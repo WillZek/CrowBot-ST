185586877f9d25c3cd6585084cade0da587e4440
Niño Piña <ninopina10@gmail.com>
2024-12-05 10:53:06 -0600

    Actualizar owner-backup.js

--- plugins/owner-backup.js
+++ plugins/owner-backup.js
@@ -1,15 +1,18 @@
 import fs from 'fs'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-await m.reply(`❮🪐❯ » *Enviando base de datos de ${wm}...*`)
+await m.reply(`❮⛄❯ » *Enviando base de datos de ${wm}...*`)
 try {
 await m.react(rwait)
 let d = new Date
 let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
-let database = await fs.readFileSync(`./database.json`)
-let creds = await fs.readFileSync(`./${sessions}/creds.json`)
-await conn.reply(m.chat, `*• Fecha:* ${date}`, m, fake)
+let database = await fs.readFileSync(`./media/database/db.json`)
+let marry = await fs.readFileSync(`./media/database/marry.json`)
+let creds = await fs.readFileSync(`./CrowSession/creds.json`)
+await conn.reply(m.chat, `*• Fecha:* ${date}`, m)
 await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: fkontak })
+await m.react(done)
+await conn.sendMessage(m.sender, {document: marry, mimetype: 'application/json', fileName: `marry.json`}, { quoted: fkontak })
 await m.react(done)
 await conn.sendMessage(m.sender, {document: creds, mimetype: 'application/json', fileName: `creds.json`}, { quoted: fkontak })
 await m.react(done)
