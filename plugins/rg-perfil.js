492b7d54b602ed94b59f0bd1ef694070fe775a65
Niño Piña <ninopina10@gmail.com>
2024-12-05 09:56:10 -0600

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -1,41 +1,88 @@
-import PhoneNumber from 'awesome-phonenumber'
-import fetch from 'node-fetch'
+import PhoneNumber from 'awesome-phonenumber';
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+const loadMarriages = () => {
+    if (fs.existsSync('./src/database/marry.json')) {
+        const data = JSON.parse(fs.readFileSync('./src/database/marry.json', 'utf-8'));
+        global.db.data.marriages = data;
+    } else {
+        global.db.data.marriages = {};
+    }
+};
+
 var handler = async (m, { conn }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => imagen1)
-let { premium, level, cookies, exp, lastclaim, registered, regTime, age, role } = global.db.data.users[m.sender]
-let username = conn.getName(who)
-let noprem = `
-👤 *PERFIL DE USUARIO*
+    loadMarriages();
+
+    let who;
+    if (m.quoted && m.quoted.sender) {
+        who = m.quoted.sender;
+    } else {
+        who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    }
+
+    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => imagen1);
+    let { premium, level, genre, birth, description, yenes, exp, lastclaim, registered, regTime, age, role } = global.db.data.users[who] || {};
+    let username = conn.getName(who);
+
+    genre = genre === 0 ? 'No especificado' : genre || 'No especificado';
+    age = registered ? (age || 'Desconocido') : 'Sin especificar';
+    birth = birth || 'No Establecido';
+    description = description || 'Sin Descripción';
+    role = role || 'Aldeano';
+
+    let isMarried = who in global.db.data.marriages;
+    let partner = isMarried ? global.db.data.marriages[who] : null;
+    let partnerName = partner ? conn.getName(partner) : 'Nadie';
+    let api = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`);
+    let userNationalityData = api.data.result;
+    let userNationality = userNationalityData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido';
+
+    let noprem = `
+「 👤 *PERFIL DE USUARIO* 」
 ☁️ *Nombre:* ${username}
-💥 *Tag:* @${who.replace(/@.+/, '')}
+💠 *Edad:* ${age}
+⚧️ *Genero:* ${genre}
+🎂 *Cumpleaños:* ${birth} 
+👩‍❤️‍👩 *Casad@:* ${isMarried ? partnerName : 'Nadie'}
+📜 *Descripción:* ${description}
 🌀 *Registrado:* ${registered ? '✅': '❌'}
+🌐 *Pais:* ${userNationality}
 
-👑 *RECURSOS*
-🌟 *Estrellas:* ${let}
-💥 *Nivel:* ${level}
-💫 *Experiencia:* ${exp}
-✨️ *Rango:* ${role}
+「 💰 *RECURSOS* 」
+💴 *Estrellas:* ${estrellas || 0}
+🌟 *Nivel:* ${level || 0}
+✨ *Experiencia:* ${exp || 0}
+⚜️ *Rango:* ${role}
+👑 *Premium:* ${premium ? '✅': '❌'}
+`.trim();
 
-💖 *Premium:* ${premium ? '✅': '❌'}
-`.trim()
-let prem = `╭──⪩ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ⪨
-│⧼👤⧽ *ᴜsᴜᴀʀɪᴏ:* 「${username}」
-│⧼💌⧽ *ʀᴇɢɪsᴛʀᴀᴅᴏ:* ${registered ? '✅': '❌'}
-│⧼🔱⧽ *ʀᴏʟ:* Vip 👑
-╰───⪨
+    let prem = `╭──⪩ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ⪨
+│⧼👤⧽ *ᴜsᴜᴀʀɪᴏ:* *${username}*
+│⧼💠⧽ *ᴇᴅᴀᴅ:* *${age}*
+│⧼⚧️⧽ *ɢᴇɴᴇʀᴏ:* *${genre}*
+│⧼🎂⧽ *ᴄᴜᴍᴘʟᴇᴀɴ̃ᴏs:* ${birth}
+│⧼👩‍❤️‍👩⧽ *ᴄᴀsᴀᴅᴏ:* ${isMarried ? partnerName : 'Nadie'}
+📜 *ᴅᴇsᴄʀɪᴘᴄɪᴏɴ:* ${description}
+│⧼🌀⧽ *ʀᴇɢɪsᴛʀᴀᴅᴏ:* ${registered ? '✅': '❌'}
+│⧼🌐⧽ *ᴘᴀɪs:* ${userNationality}
+
+╰─────────────────⪨
 
 ╭────⪩ 𝐑𝐄𝐂𝐔𝐑𝐒𝐎𝐒 ⪨
-│⧼🍪⧽ *ᴇsᴛʀᴇʟʟᴀs:* ${let}
-│⧼🔰⧽ *ɴɪᴠᴇʟ:* ${level}
-│⧼💫⧽ *ᴇxᴘᴇʀɪᴇɴᴄɪᴀ:* ${exp}
+│⧼💴⧽ *estrellas:* ${estrellas || 0}
+│⧼🌟⧽ *ɴɪᴠᴇʟ:* ${level || 0}
+│⧼✨⧽ *ᴇxᴘᴇʀɪᴇɴᴄɪᴀ:* ${exp || 0}
 │⧼⚜️⧽ *ʀᴀɴɢᴏ:* ${role}
-╰───⪨ *𝓤𝓼𝓾𝓪𝓻𝓲𝓸 𝓓𝓮𝓼𝓽𝓪𝓬𝓪𝓭𝓸* ⪩`.trim()
-conn.sendFile(m.chat, pp, 'perfil.jpg', `${premium ? prem.trim() : noprem.trim()}`, m, rcanal, { mentions: [who] })
+╰───⪨ *𝓤𝓼𝓾𝓪𝓻𝓲𝓸 𝓓𝓮𝓼𝓽𝓪𝓬𝓪𝓭𝓸* ⪩`.trim();
+
+    conn.sendFile(m.chat, pp, 'perfil.jpg', `${premium ? prem.trim() : noprem.trim()}`, m, { mentions: [who] });
 }
-handler.help = ['profile']
-handler.register = true
-handler.group = true
-handler.tags = ['rg']
-handler.command = ['profile', 'perfil']
-export default handler
+
+handler.help = ['profile'];
+handler.register = true;
+handler.group = true;
+handler.tags = ['rg'];
+handler.command = ['profile', 'perfil'];
+
+export default handler;
