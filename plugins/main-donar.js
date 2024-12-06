a374d5704b768e4e6badcfde96aad411df23a5fa
Niño Piña <ninopina10@gmail.com>
2024-12-06 10:52:10 -0600

    Actualizar plugins

---
+++ plugins/main-donar.js
@@ -0,0 +1,19 @@
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+
+    let don = `
+≡ ${mssg.donate}
+
+▢ *PayPal*
+• *Link :* ninopina10@gmail.com
+`
+let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
+conn.sendFile(m.chat, img, 'img.jpg', don, m, null, rpyp)
+//conn.sendPayment(m.chat, '2000', 'USD', don, m.sender, m)
+}
+
+handler.help = ['donate']
+handler.tags = ['main']
+handler.command = ['apoyar', 'donate', 'donar'] 
+
+export default handler
