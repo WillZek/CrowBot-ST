ef38140e4dd88a11308566f284efdff4f5bb8c03
Niño Piña <ninopina10@gmail.com>
2024-12-07 17:15:16 -0600

    Actualizar owner-enviarmsgcanal.js

--- plugins/owner-enviarmsgcanal.js
+++ plugins/owner-enviarmsgcanal.js
@@ -3,15 +3,15 @@ const handler = async (m, {conn, text}) => {
 if (!text) return await conn.reply(m.chat, `🌠 Y el texto que quiere transmitir al canal?`, m)
 
 try {
-await m.react(🕐)
+await m.react('🕐')
 
 await conn.reply(m.chat, `🌠 El texto se envió correctamente al canal.`, m, fake)
 await conn.reply(global.idchannel, text, null, fake)
-await m.react(✖️)
+await m.react('✖️')
 
 } catch (error) {
 await conn.reply(m.chat, `🥀 No se pudo enviar el mensaje al canal.`, m, rcanal)
-await m.react(🔮)}}
+await m.react('🔮')}}
 
 handler.command = ['enviarmsg', 'enviarmsgcanal', 'enviarmsgchannel', 'canals']
 handler.rowner = true
