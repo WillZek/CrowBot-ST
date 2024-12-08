603bf7857476652c925b991aee879e99cf8bd898
Niño Piña <ninopina10@gmail.com>
2024-12-07 18:14:26 -0600

    Actualizar owner-sugerencia.js

--- plugins/owner-sugerencia.js
+++ plugins/owner-sugerencia.js
@@ -1,14 +1,18 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) return conn.reply(m.chat, '🌠 ¿Que comando quieres sugerir?', m)
-    if (text.length < 10) return conn.reply(m.chat, '🌠 La sugerencia debe ser mas de 10 characteres.', m)
-    if (text.length > 1000) return conn.reply(m.chat, '🌠 Maximo de la sugerencia es de 1000 characteres.', m)
-    const teks = `🌠 Sugerencia de nuevo comando del usuario *${nombre}*
+    if (text.length < 10) return conn.reply(m.chat, '🌠 La sugerencia debe ser más de 10 caracteres.', m)
+    if (text.length > 1000) return conn.reply(m.chat, '🌠 Máximo de la sugerencia es de 1000 caracteres.', m)
+    
+    const teks = `🌠 Sugerencia de nuevo comando del usuario *${m.sender}*
 
-🛡️ Han Sugerido Un Comando:
+🛡️ Han sugerido un comando:
 > ${text}`
-    await conn.reply('50557865603@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+    
+    // Cambia el ID del grupo al que deseas enviar la sugerencia
+    const groupChatId = 'ID_DEL_GRUPO_AQUI@s.whatsapp.net';
+    await conn.reply(groupChatId, m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
 
-    m.reply('🌠 La sugerencia se envió a mi propietario.')
+    m.reply('🌠 La sugerencia se envió al grupo.')
 }
 handler.help = ['sugerencia']
 handler.tags = ['Owner']
