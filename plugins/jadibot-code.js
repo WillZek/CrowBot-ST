140c1414d435fb92ebd7f54c9306edd4ac7478f6
Niño Piña <ninopina10@gmail.com>
2024-12-11 12:08:00 -0600

    Actualizar jadibot-code.js

--- plugins/jadibot-code.js
+++ plugins/jadibot-code.js
@@ -35,9 +35,9 @@ let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => 
     if (!fs.existsSync("./CrowJadiBot/"+ authFolderB)){
         fs.mkdirSync("./CrowJadiBot/"+ authFolderB, { recursive: true });
     }
-    args[0] ? fs.writeFileSync("./GokuJadiBot/" + authFolderB + "/creds.json", JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t')) : ""
+    args[0] ? fs.writeFileSync("./CrowJadiBot/" + authFolderB + "/creds.json", JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t')) : ""
 
-const {state, saveState, saveCreds} = await useMultiFileAuthState(`./GokuJadiBot/${authFolderB}`)
+const {state, saveState, saveCreds} = await useMultiFileAuthState(`./CrowJadiBot/${authFolderB}`)
 const msgRetryCounterMap = (MessageRetryMap) => { };
 const msgRetryCounterCache = new NodeCache()
 const {version} = await fetchLatestBaileysVersion();
