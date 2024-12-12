771de8d26e760ce8b3c88532f6ec2e419113f0cc
Niño Piña <ninopina10@gmail.com>
2024-12-12 10:12:12 -0600

    Actualizar descargar-mediafire.js

--- plugins/descargar-mediafire.js
+++ plugins/descargar-mediafire.js
@@ -4,7 +4,7 @@ import * as cheerio from 'cheerio';
 import {mediafiredl} from '@bochilteam/scraper';
 
 const handler = async (m, {conn, args, usedPrefix, command}) => {
-    if (!args[0]) throw `*🎄Ingresa un link de mediafire junto al comando. Ejemplo: \n${usedPrefix + command}* https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk`;
+    if (!args[0]) throw `\`\`\`[🌠] Ingresa un link de mediafire junto al comando. Ejemplo: \n${usedPrefix + command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk\`\`\``;
 m.react('❄️')
   try {
     const resEX = await mediafiredl(args[0]);
@@ -52,7 +52,7 @@ contextInfo: { externalAdReply :{ showAdAttribution: true,
 })
       await conn.sendFile(m.chat, link, name, '', m, null, {mimetype: mime, asDocument: true});
     } catch {
-      await m.reply(`*🎄Ingresa un link de mediafire junto al comando.* Ejemplo: \n${usedPrefix + command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk`);
+      await m.reply(`\`\`\`[🌺] Ingresa un link de mediafire junto al comando. Ejemplo: \n${usedPrefix + command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk\`\`\``);
     }
   }
 };
