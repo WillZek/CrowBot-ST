bc0eade06200ff7a71ac839625bceb7177fbcb29
Niño Piña <ninopina10@gmail.com>
2024-12-12 19:56:36 -0600

    Actualizar owner-getdb.js

--- plugins/owner-getdb.js
+++ plugins/owner-getdb.js
@@ -6,7 +6,11 @@ let handler = async (m, { conn, text }) => {
     let sesi = await fs.readFileSync('./media/database/db.json')
     return await conn.sendFile(m.chat, sesi, 'db.json' , '', m, null, { mimetype: 'application/json', asDocument: true })
 }
-
+    }
+  } catch (e) {
+    m.reply('*[❗] Error, intentelo después* ${e.message}');
+m.react('✖️')
+  }
 handler.command = /^(getdb)$/i
 handler.rowner = true
 
