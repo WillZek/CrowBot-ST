15945ffe36c67dfee66e6202212d81339af78adf
Niño Piña <ninopina10@gmail.com>
2024-12-18 11:09:45 -0600

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, imagen1, m, rcanal)
+return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
 }
 handler.help = ['unreg']
 handler.tags = ['rg']
