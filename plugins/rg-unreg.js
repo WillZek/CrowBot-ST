a4aff6aefa3eb0984bdaf29ec2b76a5e7f5feb8c
Niño Piña <ninopina10@gmail.com>
2024-12-18 11:23:08 -0600

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,15 +1,27 @@
-
 let handler = async (m, { conn, text }) => {
-    let user = global.db.data.users[m.sender]
+    let user = global.db.data.users[m.sender];
 
-    user.registered = false
-    let imagenUrl = 'https://i.ibb.co/vQDtxJg/file.jpg'
+    user.registered = false;
+    let imagenUrl = 'https://i.ibb.co/vQDtxJg/file.jpg';
 
-    return conn.sendMessage(m.chat, { text: `*『✅』 Usted Ya No Está En Mi Base De Datos*`, image: { url: imagenUrl } }, { quoted: m })
+    if (!conn) {
+        console.error('Conexión no disponible');
+        return;
+    }
+
+    try {
+        return await conn.sendMessage(m.chat, {
+            text: `*『✅』 Usted Ya No Está En Mi Base De Datos*`,
+            image: { url: imagenUrl }
+        }, { quoted: m });
+    } catch (error) {
+        console.error('Error al enviar el mensaje:', error);
+        return m.reply('Ocurrió un error al intentar enviar el mensaje.');
+    }
 }
 
-handler.help = ['unreg']
-handler.tags = ['rg']
-handler.command = /^unreg(ister)?$/i
-handler.register = true
-export default handler
+handler.help = ['unreg'];
+handler.tags = ['rg'];
+handler.command = /^unreg(ister)?$/i;
+handler.register = true;
+export default handler;
