3d799bf21eea85deb182c352679b17d7983cc9dc
Niño Piña <ninopina10@gmail.com>
2024-12-18 11:12:07 -0600

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,11 +1,13 @@
 
 let handler = async (m, { conn, text }) => {
+    let user = global.db.data.users[m.sender]
 
-let user = global.db.data.users[m.sender]
-
-user.registered = false
-return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
+    user.registered = false
+    // URL de la imagen que deseas enviar
+    let imagenUrl = 'https://example.com/tu-imagen.jpg' // Cambia esto por la URL de tu imagen
+    return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, imagenUrl, m)
 }
+
 handler.help = ['unreg']
 handler.tags = ['rg']
 handler.command = /^unreg(ister)?$/i
