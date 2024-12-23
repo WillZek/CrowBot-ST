c61453d248de127519d0f88f8d604c6204087489
Niño Piña <ninopina10@gmail.com>
2024-12-23 12:24:22 -0600

    Actualizar jadibot-code.js

--- plugins/jadibot-code.js
+++ plugins/jadibot-code.js
@@ -84,7 +84,8 @@ if (methodCode && !conn.authState.creds.registered) {
     }
 
     setTimeout(async () => {
-        let codeBot = await conn.requestPairingCode(cleanedNumber);
+     m.reply(m.chat, '${wait}', rcanal); 
+   let codeBot = await conn.requestPairingCode(cleanedNumber);
         codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot;
         let txt = ` –  *S E R B O T  -  S U B B O T*\n\n`
             txt += `┌  👑  *Usa este Código para convertirte en un Sub Bot*\n`
@@ -94,8 +95,8 @@ if (methodCode && !conn.authState.creds.registered) {
             txt += `│  🌠  *3* : Selecciona *Vincular con el número de teléfono*\n` 
             txt += `└  🌠  *4* : Escriba el Codigo\n\n`
             txt += `*👑Nota:* Este Código solo funciona en el número en el que se solicitó\n\n> *Sigan El Canal*https://whatsapp.com/channel/0029VakfOZfHFxP7rNrUQk2d`
-         await parent.reply(m.chat, wait, txt, m, rcanal)
-         await parent.reply(m.chat, wait, codeBot, m, rcanal)
+         await parent.reply(m.chat, txt, m, rcanal)
+         await parent.reply(m.chat, codeBot, m, rcanal)
         rl.close()
     }, 3000)
 }
