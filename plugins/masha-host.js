969f268098bc327ddd773b6fc95891ce11ab45e8
Niño Piña <ninopina10@gmail.com>
2024-12-22 19:09:19 -0600

    Actualizar masha-host.js

--- plugins/masha-host.js
+++ plugins/masha-host.js
@@ -1,7 +1,12 @@
 // Código Hecho Por Niño Piña wa.me/50557865603
 let handler = async (m, { conn }) => {
 m.react('💫');
-const message = "*AQUI ESTAN LOS PRECIOS.*\n\n> Renovación💫";
+const message = "🌐 *`MASHA-HOSTING`*\n> *¡HOSTING DE CALIDAD QUE IMPULSA TU ÉXITO! 🚀*\n🌟 *¡Desata el poder de tu presencia digital con nuestros servers! ⚡️ Rápidos, seguros y listos para la acción. ¡No te quedes atrás, únete a nuestro host de calidad y potencia tus proyectos alojando, bot de WhatsAp, servidores de Minecraf, páginas web y muchos más! 💥"*\n\n💰 *Precios a partir de: [1$ En Adelante!]*\n\n📞 *Contáctanos:*
+• *Wa.me/59597657755*
+•  *Wa.me/5224312686*
+• *Wa.me/5055786568*\n\n📝 *¡Regístrate ahora y lleva tu proyecto al siguiente nivel!*
+> *[https://dash.masha-host.shop/home]*\n\n🌌 *Canal Oficial*
+https://whatsapp.com/channel/0029VaoyLfA0LKZKjEh5Yh1J\n\n*¡Haz que tu experiencia digital sea una explosión de éxito! 🧑‍🚀💣*";
 if (m.isGroup) {
 const imageUrl = 'https://f.uguu.se/aPQnLyQb.jpg'; // No Cambien El Link Zorras
 await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
