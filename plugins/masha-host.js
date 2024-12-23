50afdd1cc93f90da1e76ec155aa77d4e9f8b6107
Niño Piña <ninopina10@gmail.com>
2024-12-22 19:21:18 -0600

    Actualizar masha-host.js

--- plugins/masha-host.js
+++ plugins/masha-host.js
@@ -1,7 +1,7 @@
 // Código Hecho Por Niño Piña wa.me/50557865603
 let handler = async (m, { conn }) => {
 m.react('💫');
-const message = "🌐 *`MASHA-HOSTING`*\n> *¡HOSTING DE CALIDAD QUE IMPULSA TU ÉXITO! 🚀*\n🌟 *¡Desata el poder de tu presencia digital con nuestros servers! ⚡️ Rápidos, seguros y listos para la acción. ¡No te quedes atrás, únete a nuestro host de calidad y potencia tus proyectos alojando, bot de WhatsAp, servidores de Minecraf, páginas web y muchos más! 💥"*\n\n💰 𝗣𝗿𝗲𝗰𝗶𝗼𝘀 𝗔𝗽𝗮𝗿𝘁𝗶𝗿 𝗱𝗲:\n1$ en adelante\n\n📞 𝗖𝗼𝗻𝘁𝗮𝗰𝘁𝗮𝗻𝗼𝘀\n• *Wa.me/59597657755*/n• *Wa.me/5224312686*/n• *Wa.me/5055786568*\n\n📝 *¡Regístrate ahora y lleva tu proyecto al siguiente nivel!*
+const message = "🌐 *`MASHA-HOSTING`*\n> *¡HOSTING DE CALIDAD QUE IMPULSA TU ÉXITO! 🚀*\n🌟 *¡Desata el poder de tu presencia digital con nuestros servers! ⚡️ Rápidos, seguros y listos para la acción. ¡No te quedes atrás, únete a nuestro host de calidad y potencia tus proyectos alojando, bot de WhatsAp, servidores de Minecraf, páginas web y muchos más! 💥"*\n\n💰 𝗣𝗿𝗲𝗰𝗶𝗼𝘀 𝗔𝗽𝗮𝗿𝘁𝗶𝗿 𝗱𝗲:\n1$ en adelante\n\n📞 𝗖𝗼𝗻𝘁𝗮𝗰𝘁𝗮𝗻𝗼𝘀\n• *Wa.me/59597657755*/n• *wa.me/5224312686*/n• *Wa.me/5055786568*\n\n📝 *¡Regístrate ahora y lleva tu proyecto al siguiente nivel!*
 > *[https://dash.masha-host.shop/home]*\n\n🌌 *Canal Oficial*
 https://whatsapp.com/channel/0029VaoyLfA0LKZKjEh5Yh1J\n\n*¡Haz que tu experiencia digital sea una explosión de éxito! 🧑‍🚀💣*";
 if (m.isGroup) {
