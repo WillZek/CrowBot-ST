3c9530b29ee85ba8684925f68978ecb1be554279
Niño Piña <ninopina10@gmail.com>
2024-12-23 12:33:45 -0600

    Actualizar plugins

---
+++ plugins/crow-totalerres.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn }) => {
+    let pluginsConErrores = Object.values(global.plugins).filter(
+        (v) => !v.help || !v.tags
+    );
+
+    let totalErrores = pluginsConErrores.length;
+
+    conn.reply(m.chat, `*» Total de Plugins con Errores:* ${totalErrores}`, m);
+}
+
+handler.help = ['totalerrores']
+handler.tags = ['main']
+handler.command = ['totalerrores', 'terrores', 'toerr']
+handler.register = true
+export default handler;
