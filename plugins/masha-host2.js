8cc51224c9468f244db6d49ca593a62c270af1ae
Niño Piña <ninopina10@gmail.com>
2024-12-22 21:45:17 -0600

    Actualizar masha-host2.js

--- plugins/masha-host2.js
+++ plugins/masha-host2.js
@@ -47,12 +47,13 @@ let handler = async (m, { conn }) => {
             } 
         };
 
-        const imageUrl = "../Menu.jpg"; // No Cambien El Link Zorras
-        await conn.sendMessage(m.chat, text, imagen2, ...global.rchannel, imagen2 }, { quoted: m });
+        const imageUrl = 'https://f.uguu.se/aPQnLyQb.jpg'; // No Cambien El Link Zorras
+        await conn.sendMessage(m.chat, { text, image: { url: imageUrl }, ...global.rchannel }, { quoted: m });
     } catch (error) {
         console.error(`*Error*: ${error.message}`);
     }
 }
+handler.help
 handler.help = ['precios2'];
 handler.tag = ['main'];
 handler.command = ['precios2', 'p2'];
