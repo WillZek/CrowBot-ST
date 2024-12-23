552167c264dc0b0cf24435f297c993368f0bf9f9
Niño Piña <ninopina10@gmail.com>
2024-12-22 19:46:01 -0600

    Actualizar masha-host.js

--- plugins/masha-host.js
+++ plugins/masha-host.js
@@ -4,25 +4,25 @@ let handler = async (m, { conn }) => {
     const message = "🌐 *`MASHA-HOSTING`*  \n" +
     "> *¡HOSTING DE CALIDAD QUE IMPULSA TU ÉXITO! 🚀* \n" +
     "🌟 *¡Desata el poder de tu presencia digital con nuestros servers! ⚡️ Rápidos, seguros y listos para la acción. ¡No te quedes atrás, únete a nuestro host de calidad y potencia tus proyectos alojando, bot de WhatsAp, servidores de Minecraf, páginas web y muchos más! 💥\n" +
-    
+
     "💰 *Precios a partir de: [1$ En Adelante!]*  \n" +
     "📞 *Contáctanos:*\n" +
     "• *Wa.me/595976####*\n" +
     "•  *Wa.me/522431268###*\n" +
     "• *Wa.me/5055786560* \n\n" +
-    
+
     "📝 *¡Regístrate ahora y lleva tu proyecto al siguiente nivel!*  \n" +
     "> *[https://dash.masha-host.shop/home]*\n\n" +
-    
+
     "🌌 *Canal Oficial*\n" +
     "https://whatsapp.com/channel/0029VaoyLfA0LKZKjEh5Yh1J\n\n" +
-    
+
     "*¡Haz que tu experiencia digital sea una explosión de éxito! 🧑‍🚀💣*";
-    
-        const imageUrl = 'https://f.uguu.se/aPQnLyQb.jpg'; // No Cambien El Link Zorras
-        await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
-    }
+
+    const imageUrl = 'https://f.uguu.se/aPQnLyQb.jpg'; // No Cambien El Link Zorras
+    await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
 }
+
 handler.help = ['precios1'];
 handler.tags = ['main'];
 handler.command = ['precios1', 'p1'];
