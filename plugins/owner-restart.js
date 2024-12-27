e6c78c428c0e290350c3d9494055b7833bd6a6f1
Niño Piña <ninopina10@gmail.com>
2024-12-27 10:29:34 -0600

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,6 +1,21 @@
 
 import { spawn } from 'child_process'
 
+    if (!process.send) throw '*『✦』Reiniciar: node start.js*\n*『✦』Reiniciar: node index.js*';
+
+    if (conn.user.jid === conn.user.jid) {
+        const progreso = [
+      "♻️iniciando proceso de reinicio de TECNO-BOT",
+            "□□□□□ 0%",
+            "■□□□□ 20%",
+            "■■□□□ 40%",
+            "■■■□□ 60%",
+            "■■■■□ 80%",
+            "■■■■■ 100%",
+        ];
+
+        const { key } = await conn.sendMessage(m.chat, { text: progreso[0] }, { quoted: m });
+
 var handler = async (m, { conn, isROwner, text }) => {
 
 if (!process.send) throw 'Dont: node luffy.js\nDo: node index.js'
