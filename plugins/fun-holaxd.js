25a15b40860c1d32c957bb47f96728e5a3879e34
Niño Piña <ninopina10@gmail.com>
2024-12-28 17:24:46 -0600

    Actualizar fun-holaxd.js

--- plugins/fun-holaxd.js
+++ plugins/fun-holaxd.js
@@ -1,3 +1,14 @@
 {
+let handler = async(usedPrefix, commando);
 let txt = `hola xdd`;
-let img = 
+let img = 'https://qu.ax/yyCo.jpeg',
+conn.sendFile(m.chat, txt, img)
+} catch (error) {
+m.reply(`Error: ${error.message}`)
+m.react(done)
+}
+handler.help = ['holaxd'];
+handler.tag = ['fun'];
+handler.command = ['holaxd', 'hxd'];
+
+export default handler;
