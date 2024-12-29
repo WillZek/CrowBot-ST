49029c3d62144e8a0720d284602738426e0b41bc
Niño Piña <ninopina10@gmail.com>
2024-12-28 18:46:17 -0600

    Actualizar fun-cr7xd.js

--- plugins/fun-cr7xd.js
+++ plugins/fun-cr7xd.js
@@ -2,21 +2,21 @@ import fs from 'fs';
 
 const handler = async(m, { conn, usedPrefix, command }) => {
 
- try {
-let txt = `Cristiano Ronaldo Siuuu`;
+try {
+let txt = `Cristiano Ronaldo`;
 let img = 'https://files.catbox.moe/w1ciuo.jpg';
 
-await conn.sendMessage(m.chat, { image: { url: img }, caption: txt});
+conn.sendMessage(m.chat, { image: { url: img }, caption: txt });
 m.react('🎉');
 
 } catch (error) {
 m.reply(`Error: ${error.message}`);
 m.react('💥');
- }
+ } 
 };
 
 handler.help = ['cr7xd'];
-handler.tag = ['fun']
-handler.command = ['cr7xd', 'crxd'];
+handler.tag = ['fun'];
+handler.command ['cr7xd', 'cxd'];
 
 export default handler;
