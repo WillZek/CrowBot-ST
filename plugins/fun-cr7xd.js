fa9d0c065bdec67436ee451c30c8418fe0757bb6
Niño Piña <ninopina10@gmail.com>
2024-12-28 18:30:08 -0600

    Actualizar plugins

---
+++ plugins/fun-cr7xd.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+const handler = async(m, { conn, usedPrefix, command });
+
+try {
+let txt = `Cristiano Ronaldo Siuuu`;
+let img = 'https://files.catbox.moe/w1ciuo.jpg';
+
+await conn.sendMessage(m.chat, { image: { url: img }, caption: txt});
+m.react('🎉')
+
+} catch (error) {
+m.reply(`Error: ${error.message}`);
+m.react('💥')
+ }
+};
+
+handler.help = ['cr7xd'];
+handler.tag = ['fun']
+handler.command = ['cr7xd', 'crxd'];
+
+export default handler;
