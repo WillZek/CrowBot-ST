63ba9bc56ecf7668fc479d224e061f912150f9eb
Niño Piña <ninopina10@gmail.com>
2024-12-28 18:16:18 -0600

    Actualizar plugins

---
+++ plugins/fun-mesirvexd.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+ 
+  {
+const handler = async(m.chat, usedPrefix, command)
+
+try{
+    let txt = `Siuu`;
+    let img = 'https://files.catbox.moe/w1ciuo.jpg';
+m.react('🎉')
+await conn.sendMessage(chat, { image: { url: img }, caption: txt })
+
+} catch (error) { 
+m.reply(`*Error:* ${error.message});
+m.react('💥')
+ }
+};
+
+handler.help = ['messirvexd'];
+handler.tag = ['fun'];
+handler.command ['messirvexd', 'mxd'];
+
+export default handler;
