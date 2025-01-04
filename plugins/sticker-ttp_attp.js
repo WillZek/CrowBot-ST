8769a66e35e8b945dcd45e2081b335d936f59628
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-04 13:39:07 -0600

    Actualizar sticker-ttp_attp.js

--- plugins/sticker-ttp_attp.js
+++ plugins/sticker-ttp_attp.js
@@ -3,8 +3,8 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `𝙀𝙨𝙘𝙧𝙞𝙗𝙖 𝙥𝙖𝙧𝙖 𝙦𝙪𝙚 𝙚𝙡 𝙩𝙚𝙭𝙩𝙤 𝙨𝙚 𝙘𝙤𝙣𝙫𝙞𝙚𝙧𝙩𝙖 𝙚𝙡 𝙨𝙩𝙞𝙘𝙠𝙚𝙧\n𝙀𝙟𝙚𝙢𝙥𝙡𝙤\n*${usedPrefix + command}* Nuevo Sticker`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: imagen1, sourceUrl: redes.getRandom()}}})
 let teks = encodeURI(text)
-conn.fakeReply(m.chat, `Calma estoy haciendo tu texto a sticker 👏\n\n> *Esto puede demorar unos minutos*`, '0@s.whatsapp.net', `CrowBot`, 'status@broadcast', null, fake)
-//m.reply(`Calma crack estoy haciendo tu texto a sticker 👏\n\n> *Esto puede demorar unos minutos*`) 
+conn.fakeReply(m.chat, `${espera}\n\n> *Esto puede demorar unos minutos*`, '0@s.whatsapp.net', `CrowBot`, 'status@broadcast', null, fake)
+//m.reply(`${espera}\n\n> *Esto puede demorar unos minutos*`) 
 
 if (command == 'attp') {
 let stiker = await sticker(null,`https://api.erdwpe.com/api/maker/attp?text=${teks}`,global.packname, global.author)
