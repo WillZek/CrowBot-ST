44c2a944b02f3d797d108670830b24a2ba9c8eb5
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-04 13:55:12 -0600

    Actualizar masha-host.js

--- plugins/masha-host.js
+++ plugins/masha-host.js
@@ -1,7 +1,7 @@
 // Código Hecho Por Niño Piña wa.me/50557865603
 let handler = async (m, { conn }) => {
     m.react('💫');
-    const message = `🌐 *`MASHA-HOSTING`*  \n" +
+    let text = `🌐 *`MASHA-HOSTING`*  \n" +
     "> *¡HOSTING DE CALIDAD QUE IMPULSA TU ÉXITO! 🚀* \n" +
     "🌟 *¡Desata el poder de tu presencia digital con nuestros servers! ⚡️ Rápidos, seguros y listos para la acción. ¡No te quedes atrás, únete a nuestro host de calidad y potencia tus proyectos alojando, bot de WhatsAp, servidores de Minecraf, páginas web y muchos más! 💥*\n" +
 
@@ -23,7 +23,7 @@ let handler = async (m, { conn }) => {
     let rchannel = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363314192605628@newsletter", serverMessageId: 100, newsletterName: '🌲【✫𝚃𝙴𝙰𝙼  乂 𝚂𝚃𝙰𝚁𝙲𝙾𝚁𝙴✫】🎅', }, externalAdReply: { showAdAttribution: true, title: packname, body: '❄️𝐅𝐞𝐥𝐢𝐳 𝐍𝐚𝐯𝐢𝐳𝐚𝐝⛄', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
 
     const img = 'https://f.uguu.se/aPQnLyQb.jpg'; // No Cambien El Link Zorras
-    await conn.sendMessage(m.chat, { image: { url: img }, caption: message });
+    await conn.sendMessage(m.chat, { image: { url: img }, caption: text });
 }
 
 handler.help = ['precios1'];
