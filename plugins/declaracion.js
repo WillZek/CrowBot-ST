91f317a3608020e6684902e761be8788b5a68b71
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-06 13:03:49 -0600

    Actualizar declaracion.js

--- plugins/declaracion.js
+++ plugins/declaracion.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     if (command === 'declaracion') {
-        const imageUrl = 'https://qu.ax/VccqK.jpg';
+        const imageUrl = 'https://files.catbox.moe/cqvoel.jpg';
 
         const messageText = `Hola Valentina \nVengo a decirte que de hace mucho me gustas pero no fui capaz de demostrar amor y cariño, te quiero pedir disculpas por mi comportamiento en dejarte hablar\nPero con el tiempo me di cuenta que el error fue mio y quiero pedirte disculpas\nEstraño los abrazos que nos dabamos demostraban cariño, realmente quiero que me perdones y empezar otra vez. \n\n¿ Me Perdonas ?\n\n\n`;
 
@@ -11,7 +11,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 };
 
 let siHandler = async (m, { conn }) => {
-    const yesImageUrl = 'https://qu.ax/abKS.jpg';
+    const yesImageUrl = 'https://files.catbox.moe/sn1g4f.jpg';
     const yesMessageText = `¡Qué alegría que hayas aceptado! Me siento increíblemente feliz y emocionado por lo que está por venir. Desde que te conocí, he soñado con este momento, y ahora que es real, no puedo esperar para vivir momentos inolvidables contigo.\n\nGracias por darme esta oportunidad. 💖`;
 
     await conn.sendMessage(m.chat, { 
@@ -21,7 +21,7 @@ let siHandler = async (m, { conn }) => {
 };
 
 let noHandler = async (m, { conn }) => {
-    const noImageUrl = 'https://qu.ax/eFBg.jpg';
+    const noImageUrl = 'https://files.catbox.moe/cqvoel.jpg';
     const noMessageText = `Entiendo y agradezco tu sinceridad. Aunque no haya sido el resultado que esperaba, valoro mucho nuestra amistad y quiero que sepas que seguiré aquí para ti. 😊`;
 
     await conn.sendMessage(m.chat, { 
