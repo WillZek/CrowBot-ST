83a09fa964c48659230d98bb3c21c4a63527e3ac
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-06 13:00:03 -0600

    Actualizar declaracion.js

--- plugins/declaracion.js
+++ plugins/declaracion.js
@@ -12,33 +12,21 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
 let siHandler = async (m, { conn }) => {
     const yesImageUrl = 'https://qu.ax/abKS.jpg';
-    const yesAudioUrl = 'https://qu.ax/lyds.mp3';
     const yesMessageText = `¡Qué alegría que hayas aceptado! Me siento increíblemente feliz y emocionado por lo que está por venir. Desde que te conocí, he soñado con este momento, y ahora que es real, no puedo esperar para vivir momentos inolvidables contigo.\n\nGracias por darme esta oportunidad. 💖`;
 
     await conn.sendMessage(m.chat, { 
         image: { url: yesImageUrl }, 
         caption: yesMessageText
     }, { quoted: m });
-
-    await conn.sendMessage(m.chat, { 
-        audio: { url: yesAudioUrl }, 
-        mimetype: 'audio/mpeg'
-    }, { quoted: m });
 };
 
 let noHandler = async (m, { conn }) => {
     const noImageUrl = 'https://qu.ax/eFBg.jpg';
     const noMessageText = `Entiendo y agradezco tu sinceridad. Aunque no haya sido el resultado que esperaba, valoro mucho nuestra amistad y quiero que sepas que seguiré aquí para ti. 😊`;
-    const noAudioUrl = 'https://qu.ax/Pgxz.mp3';
 
     await conn.sendMessage(m.chat, { 
         image: { url: noImageUrl }, 
         caption: noMessageText
-    }, { quoted: m });
-
-    await conn.sendMessage(m.chat, { 
-        audio: { url: noAudioUrl }, 
-        mimetype: 'audio/mpeg'
     }, { quoted: m });
 };
 
