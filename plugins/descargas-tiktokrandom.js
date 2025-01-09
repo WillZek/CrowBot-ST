04b78fcd49d0877aaa40a52b9a928818eeea86a5
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-09 14:24:05 -0600

    Update descargas-tiktokrandom.js

--- plugins/descargas-tiktokrandom.js
+++ plugins/descargas-tiktokrandom.js
@@ -18,7 +18,7 @@ m.reply(eror)
 })
 }
 handler.help = ['tiktokrandom']
-handler.tags = ['downloader']
+handler.tags = ['descargas']
 handler.command = /^(tiktokrandom|ttrandom)$/i
 handler.limit = true 
 handler.register = true
