50823654da63dcc7b9798d1b368f19c63c7e9860
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-09 15:50:46 -0600

    Actualizar tools-blackbox.js

--- plugins/tools-blackbox.js
+++ plugins/tools-blackbox.js
@@ -133,7 +133,7 @@ let handler = async (m, { text, usedPrefix, command, conn }) => {
 handler.help = ['blackbox <pregunta>'];
 handler.tags = ['tools'];
 handler.command = /^(blackbox)$/i;
-handler.estrellas = 3
+handler.estrellas = 3;
 handler.premium = false;
 handler.register = true;
 
