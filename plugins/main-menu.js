7ca44e97b28df6b6c28522f62b762cbb73d2c1c1
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-09 15:40:21 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -3,8 +3,6 @@ import { join } from 'path'
 import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 
-let imagen1 = 'https://files.catbox.moe/ftx8eu.jpg';
-
 let tags = {
   'main': '♥️INFO💚',
   'clanes': '♥️CLANES💚',
