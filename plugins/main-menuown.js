5b6ee21bc8c631d084a59fa3c366eae5e4d59707
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-11 17:10:28 -0600

    Actualizar main-menuown.js

--- plugins/main-menuown.js
+++ plugins/main-menuown.js
@@ -195,7 +195,7 @@ await m.react('👑')
     throw e
   }
 }
-handler.help = ['menuow']
+handler.help = ['menuow (Menu Para Mi Owner)']
 handler.tags = ['crow']
 handler.command = ['menuow', 'menuowner', 'menucreador']
 
