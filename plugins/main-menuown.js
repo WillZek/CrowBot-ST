f76826526fcdcb87f0517c43ae99250b4d3ae485
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-11 16:56:05 -0600

    Actualizar main-menuown.js

--- plugins/main-menuown.js
+++ plugins/main-menuown.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'owner': 'MenuOwner',
 }
-let img = 'https://files.catbox.moe/w1ltfq.jpg'
+let img = 'https://files.catbox.moe/w3s79j.jpg'
 
   try {
           // DEFAULT MENU
