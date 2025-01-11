d348dada872cb4c2ae956b17a46ab70c8caa75e7
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-11 17:40:34 -0600

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -11,7 +11,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
     m.reply('🚩 Ocurrio un error.');
   }
 };
-handler.tags = ['mods'];
+handler.tags = ['owner'];
 handler.help = ['autoadmin'];
 handler.command = ['autoadmin'];
 handler.mods = true;
