0934c67dd25a84153921a026a62e41edb8e71086
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-11 17:10:51 -0600

    Actualizar main-menuto.js

--- plugins/main-menuto.js
+++ plugins/main-menuto.js
@@ -195,7 +195,7 @@ await m.react('⚙️')
     throw e
   }
 }
-handler.help = ['menuto']
+handler.help = ['menuto (Menu De Herramientas)']
 handler.tags = ['crow']
 handler.command = ['menuto', 'menuherramientas', 'menuhe']
 
