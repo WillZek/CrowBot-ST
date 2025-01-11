955536684163a44acb6abca3e3ef11b1117b2498
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-11 17:40:05 -0600

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -10,7 +10,7 @@ let res = await conn.groupAcceptInvite(code)
 m.reply(`🚩 Me uní correctamente al Grupo`)
 }
 handler.help = ['join <link>']
-handler.tags = ['mods']
+handler.tags = ['owner']
 handler.command = ['join', 'entrar'] 
 handler.mods = true
 
