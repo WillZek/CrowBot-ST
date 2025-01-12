e4b95909da5f2ab72080d34b867909f2a1e859e0
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-12 13:21:12 -0600

    Actualizar owner-getdb.js

--- plugins/owner-getdb.js
+++ plugins/owner-getdb.js
@@ -1,17 +1,22 @@
-
 import fs from 'fs'
-let handler = async (m, { conn, text }) => {
- try {
-    m.react('✅')
-    let sesi = await fs.readFileSync('./media/database/db.json')
-    return await conn.sendFile(m.chat, sesi, 'db.json' , '', m, null, { mimetype: 'application/json', asDocument: true })
-}
-    }
-  } catch (e) {
-    m.reply(`*[❗] Error, intentelo después* ${e.message}`);
-m.react('✖️')
-  }
-handler.command = /^(getdb)$/i
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+await m.reply(`*Enviando base de datos de ${wm}...*`)
+try {
+await m.react(rwait)
+let d = new Date
+let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
+let database = await fs.readFileSync(`./media/database/db.json`)
+await conn.reply(m.chat, `*• Fecha:* ${date}`, m)
+await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: fkontak })
+await m.react(done)
+} catch {
+await m.react(error)
+conn.reply(m.chat, `❌️» *Ocurrió un error.*`, m, fake)}}
+
+handler.help = ['getdb']
+handler.tags = ['owner']
+handler.command = ['getdb', 'database', 'copia']
 handler.rowner = true
 
 export default handler
