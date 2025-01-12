a1db46198ac07ce783a1be32f211c84cf8c1d154
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-11 22:37:47 -0600

    Actualizar gc-unwarn.js

--- plugins/gc-unwarn.js
+++ plugins/gc-unwarn.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, text, command, usedPrefix}) => {
   else who = m.chat;
   const user = global.db.data.users[who];
   const bot = global.db.data.settings[conn.user.jid] || {};
-  const warntext = `*[❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} @${global.suittag}*`;
+  const warntext = `*[❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} @User*`;
   if (!who) throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
   if (m.mentionedJid.includes(conn.user.jid)) return;
   if (user.warn == 0) throw '*[❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝚃𝙸𝙴𝙽𝙴 0 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰𝚂*';
@@ -16,4 +16,5 @@ handler.command = /^(unwarn|delwarn|deladvertir|deladvertencia|delwarning)$/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
+
 export default handler;
