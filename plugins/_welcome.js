4eb21d56b5fc017a7d3c3f2f2ea57aba93d0ed9d
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-11 21:38:57 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,20 +3,39 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let img = imagen1
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/w5id3b.jpg')
+  let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `🍭 *≺ CROW BOT - ST* \n『 Bιҽɳʋҽɳιԃσ 』\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bιҽɳʋҽɳιԃσ/α 」\n「 ${groupMetadata.subject} 」\n\n> *WillZek*`
-await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 27) {
+    if (chat.sWelcome) {
+      let user = `@${m.messageStubParameters[0].split`@`[0]}`
+      let welcome = chat.sWelcome.replace('@user', () => user);
+      await conn.sendAi(m.chat, botname, textbot, welcome, img, img, canal)
+    } else {
+      let bienvenida = `┌─★ *Crow Bot - ST \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+      await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal)
+    }
   }
 
-  if (chat.welcome && m.messageStubType == 28) {
-    let bye = ` ⛄ *≺ CROW BOT- ST* \n「 Aԃισʂ 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Sҽ ϝυҽ 」\n「 Nυɳƈα ƚҽ ϙυιʂιɱσʂ αϙυι 」\n\n> *WillZek*`
-await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 28) {
+    if (chat.sBye) {
+      let user = `@${m.messageStubParameters[0].split`@`[0]}`
+      let bye = chat.sBye.replace('@user', () => user);
+      await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
+    } else {
+      let bye = `┌─★ Crow Bot - ST \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+      await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
+    }
   }
 
-  if (chat.welcome && m.messageStubType == 32) {
-    let kick = ` ⛄ *≺ CROW BOT - ST* \n「 Aԃισʂ 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Sҽ ϝυҽ 」\n「 Nυɳƈα ƚҽ ϙυιʂιɱσʂ αϙυι 」\n\n> *WillZek*`
-await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 32) {
+    if (chat.sBye) {
+      let user = `@${m.messageStubParameters[0].split`@`[0]}`
+      let bye = chat.sBye.replace('@user', () => user);
+      await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
+    } else {
+      let kick = `┌─★ Crow Bot ST \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+      await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal)
+    }
 }}
