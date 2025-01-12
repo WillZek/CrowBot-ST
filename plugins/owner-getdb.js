ee0d0fc14c0f99313533e28c5d8d3570b46b7506
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-12 13:16:54 -0600

    Actualizar owner-getdb.js

--- plugins/owner-getdb.js
+++ plugins/owner-getdb.js
@@ -1,14 +1,14 @@
 
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
-
+ try {
     m.react(done)
     let sesi = await fs.readFileSync('./media/database/db.json')
     return await conn.sendFile(m.chat, sesi, 'db.json' , '', m, null, { mimetype: 'application/json', asDocument: true })
 }
     }
   } catch (e) {
-    m.reply('*[❗] Error, intentelo después* ${e.message}');
+    m.reply(`*[❗] Error, intentelo después* ${e.message}`);
 m.react('✖️')
   }
 handler.command = /^(getdb)$/i
