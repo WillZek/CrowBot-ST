c1564ad09a36e44d8a4c8744755239d0234d013d
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-13 11:00:13 -0600

    Actualizar gc-add.js

--- plugins/gc-add.js
+++ plugins/gc-add.js
@@ -7,7 +7,8 @@ else who = m.chat
 let name = await conn.getName(m.sender)        
 let user = global.db.data.users[who]
 let nom = conn.getName(m.sender) 
-if (!text) throw `🌠 Ingrese el número de la persona que quieres añadir a este grupo.\n\n💛 Ejemplo:\n*${usedPrefix + command}* 66666666666`
+if (!text) throw `🌠 Ingrese el número de la persona que quieres añadir a este grupo.\n\n💛 Ejemplo:\n*${usedPrefix + command}* 50557865603`
+    if (!who) return m.reply('⚠️️ *Tienes Que Ingresar El Número De La Persona Junto Al Comando sin el +*')
 if (text.includes('+')) throw  `🌠 Ingrese el número todo junto sin el *(+)*`
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
