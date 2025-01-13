0a634f016173c185b598688461901f6d81be57b4
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-13 10:33:08 -0600

    Actualizar buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -18,7 +18,7 @@ const handler = async (m, {conn, command, args}) => {
   }).join('\n\n');
   try {
     const ss = `https://image.thum.io/get/fullpage/${url}`;
-    await conn.sendFile(m.chat, ss, 'error.png', xd + '\n> ' + url + '\n\n' + msg, title: 'GOOGLE SEARCH', m, { title: 'GOOGLE SEARCH', body: dev, sourceUrl: channel});
+    conn.sendFile(m.chat, ss, 'error.png', xd + '\n> ' + url + '\n\n' + msg, m, { title: 'GOOGLE SEARCH', body: dev, sourceUrl: channel });
   } catch {
     m.reply(msg);
   }
