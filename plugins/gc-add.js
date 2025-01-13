4b52183b328561087835f8083fa0b8cf2f0efb91
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-13 11:21:20 -0600

    Actualizar gc-add.js

--- plugins/gc-add.js
+++ plugins/gc-add.js
@@ -11,7 +11,7 @@ let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 }
 handler.help = ['invitar *<numero>*']
 handler.tags = ['grupo']
-handler.command = ['add', invite','invitar'] 
+handler.command = ['add', 'invite','invitar'] 
 handler.group = true
 handler.admin = false
 handler.botAdmin = true
