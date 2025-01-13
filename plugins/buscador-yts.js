101c3e8778a6919dc076c94ed18a66f1ba00e49d
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-12 20:12:19 -0600

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,36 +2,42 @@ import yts from 'yt-search'
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
 
-if (!text) return conn.reply(m.chat, `🌸 *Escriba el título de algún vídeo de Youtube\n\nEjemplo, !${command} Yuki Suou*`, m, rcanal, )
+    if (!text) return conn.reply(m.chat, `🌸 *Escriba el título de algún vídeo de Youtube\n\nEjemplo, !${command} Yuki Suou*`, m, rcanal)
 
-conn.reply(m.chat, wait, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
+    conn.reply(m.chat, wait, m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: null,
+                mediaType: 1,
+                showAdAttribution: true,
+                title: packname,
+                body: wm,
+                previewType: 0,
+                thumbnail: icons,
+                sourceUrl: channel
+            }
+        }
+    })
 
-let results = await yts(text)
-let tes = results.all
-let teks = results.all.map(v => {
-switch (v.type) {
-case 'video': return `🎩 *Título:* 
-» ${v.title}
+    let results = await yts(text)
+    let tes = results.all
+    let teks = results.all.map(v => {
+        switch (v.type) {
+            case 'video': return {
+                title: `🌸 *Título:* \n» ${v.title}`,
+                url: `🔗 *Enlace:* \n» ${v.url}`,
+                duration: `🕝 *Duración:* \n» ${v.timestamp}`,
+                uploaded: `🚩 *Subido:* \n» ${v.ago}`,
+                views: `👀 *Vistas:* \n» ${v.views}`,
+                thumbnail: v.thumbnail
+            }
+        }
+    }).filter(v => v)
 
-🔗 *Enlace:* 
-» ${v.url}
-
-🕝 *Duración:*
-» ${v.timestamp}
-
-🍭 *Subido:* 
-» ${v.ago}
-
-👀 *Vistas:* 
-» ${v.views}`}}).filter(v => v).join('\n\n$••••••••••••••••••••••••••••••••••••\n\n')
-
-conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
-
+    for (const video of teks) {
+        let message = `${v.title}\n${v.url}\n${v.timestamp}\n${v.ago}\n${v.views}`
+        conn.sendFile(m.chat, video.thumbnail, 'yts.jpeg', message, fkontak, m)
+    }
 }
 handler.help = ['ytsearch']
 handler.tags = ['buscador']
