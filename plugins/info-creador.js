2a1921167537eb9ef2f8c89dc8e4de8137121d8e
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-14 11:10:27 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,6 +1,6 @@
 function handler(m) {
   const data = global.creadorbot.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { title: wm, body: dev, sourceUrl: 'Wa.me/50557865603?text=Hola+Owner+De+CrowBot', showAdAttribution: true }}})
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { title: wm, body: dev, showAdAttribution: true }}})
 
 
 }
