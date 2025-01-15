bcdc6adc2b91d65291b5214777c0b11d7e7bae5d
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-15 13:46:55 -0600

    Actualizar rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -2,7 +2,7 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-let img = await (await fetch(`https://qu.ax/yyCo.jpeg`)).buffer()
+let img = await (await fetch(`https://files.catbox.moe/gjgkk4.jpg`)).buffer()
 let name = conn.getName(m.sender)
 let user = global.db.data.users[m.sender]
 if (!canLevelUp(user.level, user.exp, global.multiplier)) {
@@ -15,12 +15,12 @@ await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)}
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 if (before !== user.level) {
-let txt = `🍪 F E L I C I T A C I O N E S 🍪\n\n` 
+let txt = `🍭 F E L I C I T A C I O N E S 🍭\n\n` 
 txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
-txt += `• 🍄 Nivel anterior : ${before}\n`
-txt += `• 🍄 Nuevos niveles : ${user.level}\n`
-txt += `• 🍄 Fecha : ${new Date().toLocaleString('id-ID')}\n\n`
-txt += `🍄 *Nota:* _Cuanto más a menudo interactúes con *CrowBot*, mayor será tu nivel_`
+txt += `• 🎩 Nivel anterior : ${before}\n`
+txt += `• 🎩 Nuevos niveles : ${user.level}\n`
+txt += `• 🎩 Fecha : ${new Date().toLocaleString('id-ID')}\n\n`
+txt += `🎩 *Nota:* _Cuanto más a menudo interactúes con *CrowBot*, mayor será tu nivel_`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)}}
 
 handler.help = ['levelup']
