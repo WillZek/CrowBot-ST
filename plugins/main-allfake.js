ba399240556c53be24e95c5a35abd744e4fdc1fa
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-14 18:41:48 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -97,8 +97,8 @@ global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@
 
 //Id Channel
 global.idchannel = '120363381910502266@newsletter'
-global.canalIdM = ["120363338362822764@newsletter", "120363368073378190@newsletter"]
-global.canalNombreM = ["🌠 𝐋𝐮𝐦𝐢𝐧𝐚𝐫𝐲'𝐬 | 𝐖𝐞𝐛 𝐇𝐨𝐬𝐭𝐢𝐧𝐠 🌌", "✯ 𝐂𝐫𝐨𝐰𝐁𝐨𝐭 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ✯"]
+global.canalIdM = ["120363338362822764@newsletter", "120363368073378190@newsletter", "120363387375075395@newsletter"]
+global.canalNombreM = ["🌠 𝐋𝐮𝐦𝐢𝐧𝐚𝐫𝐲'𝐬 | 𝐖𝐞𝐛 𝐇𝐨𝐬𝐭𝐢𝐧𝐠 🌌", "✯ 𝐂𝐫𝐨𝐰𝐁𝐨𝐭 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ✯", "⏤͟͟͞͞☆ 𝐂𝐫𝐨𝐰𝐁𝐨𝐭 𝐓𝐞𝐬𝐭 『𝐏𝐨𝐝𝐜𝐚𝐬𝐭』"]
 global.channelRD = await getRandomChannel()
 // global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "3876577197-120363302285079181@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${packname}`, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
 
