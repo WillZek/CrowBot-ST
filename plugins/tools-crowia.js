f6048dfc7b981e0055f69a77e2e48513304f3ca8
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-15 14:27:29 -0600

    Actualizar plugins

---
+++ plugins/tools-crowia.js
@@ -0,0 +1,64 @@
+//código modificado por Niño Piña
+import { randomBytes } from "crypto"
+import axios from "axios"
+
+let handler = async (m, { conn, text }) => {
+    if (!text) throw '¿Como puedo ayudarte hoy?';
+    try {
+        conn.reply(m.chat, m);
+        let data = await chatGpt(text)
+await conn.sendMessage(m.chat, { text: data,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false, 
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: `ᥴr᥆ᥕᑲ᥆𝗍 ᑲᥡ ᥕіᥣᥣzᥱk`,
+body: ``,
+"previewType": "PHOTO",
+thumbnailUrl: 'https://tinyurl.com/2awg2bch', 
+sourceUrl: 'https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W'}}},
+{ quoted: m})
+    } catch (err) {
+        m.reply('error cik:/ ' + err);
+    }
+}
+
+handler.command = handler.help = ['crowbot'];
+handler.estrellas = 3;
+handler.tags = ['tools'];
+
+export default handler;
+
+async function chatGpt(query){
+try {
+
+const { id_ }= (await axios.post("https://chat.chatgptdemo.net/new_chat",{user_id: "crqryjoto2h3nlzsg"},{headers:{
+"Content-Type": "application/json",
+
+}})).data
+
+const json = {"question":query,"chat_id": id_,"timestamp":new Date().getTime()}
+
+
+const { data } = await axios.post("https://chat.chatgptdemo.net/chat_api_stream",json,{headers:{
+"Content-Type": "application/json",
+
+}})
+const cek = data.split("data: ")
+
+let res = []
+
+for (let i=1; i < cek.length; i++){
+if (cek[i].trim().length > 0){
+res.push(JSON.parse(cek[i].trim()))
+}}
+
+return res.map((a) => a.choices[0].delta.content).join("")
+
+} catch (error) {
+console.error("Error parsing JSON:",error)
+return 404
+}
+}
