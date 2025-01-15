2a25537021c26df772040df8a360a4197744d703
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-14 22:23:02 -0600

    Actualizar descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -1,114 +1,81 @@
-import axios from 'axios'
+/* ʙʏ ᴊᴛxꜱ 🐢 */
 import fetch from 'node-fetch'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import search from 'yt-search'
-async function spotifyxv(query) {
-let token = await tokens();
-let response = await axios({
-method: 'get',
-url: 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(query) + '&type=track',
-headers: {
-Authorization: 'Bearer ' + token,
-},
-})
-const tracks = response.data.tracks.items
-const results = tracks.map((track) => ({
-name: track.name,
-artista: track.artists.map((artist) => artist.name),
-album: track.album.name,
-duracion: timestamp(track.duration_ms),
-url: track.external_urls.spotify,
-imagen: track.album.images.length ? track.album.images[0].url : '',
-}))
-return results
-}
-async function tokens() {
-const response = await axios({
-method: 'post',
-url:
-'https://accounts.spotify.com/api/token',
-headers: {
-'Content-Type': 'application/x-www-form-urlencoded',
-Authorization: 'Basic ' + Buffer.from('acc6302297e040aeb6e4ac1fbdfd62c3:0e8439a1280a43aba9a5bc0a16f3f009').toString('base64'),
-},
-data: 'grant_type=client_credentials',
-})
-return response.data.access_token
-}
-function timestamp(time) {
-const minutes = Math.floor(time / 60000);
-const seconds = Math.floor((time % 60000) / 1000);
-return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-}
-async function getBuffer(url, options) {
-try {
-options = options || {};
-const res = await axios({
-method: 'get',
-url,
-headers: {
-DNT: 1,
-'Upgrade-Insecure-Request': 1,
-},
-...options,
-responseType: 'arraybuffer',
-});
-return res.data;
-} catch (err) {
-return err;
-}}
-async function getTinyURL(text) {
-try {
-let response = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`);
-return response.data;
-} catch (error) {
-return text;
-}}
+const { generateWAMessageContent, generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `╰⊱❗️⊱ *ACCIÓN MAL USADA* ⊱❗️⊱╮\n\n💛 *DEBE DE USAR EL COMANDO COMO EN ESTE EJEMPLO:*\n${usedPrefix + command} *tu foto*`
+if (!text) return m.reply('Ingresa el texto de lo que quieres buscar en Spotify 🤍');
+await m.react('🕓');
+
 try {
-conn.reply(m.chat, '🚩 *Enviando su música de Spotify*', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-m.react(rwait)
-let songInfo = await spotifyxv(text)
-if (!songInfo.length) throw `*No se encontró la canción*`
-let res = songInfo[0]
-let fileSizeInMB = (await getBuffer(res.url)).length / (1024 * 1024)
-let shortURL = await getTinyURL(res.url)
-const info = `🌸 *TITULO:*
-_${res.name}_
+async function createImage(url) {
+const { imageMessage } = await generateWAMessageContent({image: { url }}, {upload: conn.waUploadToServer})
+return imageMessage
+}
 
-💛 *ARTISTA:*
-» ${res.artista.join(', ')}
+let push = [];
+let api = await fetch(`https://deliriussapi-oficial.vercel.app/search/spotify?q=${encodeURIComponent(text)}`);
+let json = await api.json()
 
-🔗 *LINK:*
-» ${shortURL}
+for (let track of json.data) {
+let image = await createImage(track.image)
 
-✨️ *Enviando Canción....*
-${global.wm}`
+/* push.push({
+body: proto.Message.InteractiveMessage.Body.fromObject({
+text: '${track.title} - ${track.artist}'
+}),
+footer: proto.Message.InteractiveMessage.Footer.fromObject({text: `${dev}`}),
+header: proto.Message.InteractiveMessage.Header.fromObject({title: '', hasMediaAttachment: true, imageMessage: image}),
+nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+buttons: [ */ 
 
-let resImg = await fetch(res.imagen)
-let thumbb = await resImg.buffer()
-let { videos } = await search(res.name)
-let q = '128kbps'
-let v = videos[0].url
-let yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v))
-let dl_url = await yt.audio[q].download()
-let ttl = await yt.title
-let size = await yt.audio[q].fileSizeH
-let img = await getBuffer(res.imagen)
-conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${ttl}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
-await conn.sendMessage(m.chat, {text: info, contextInfo: {forwardingScore: 9999999, isForwarded: true, "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "thumbnailUrl": img, "mediaUrl": shortURL, "sourceUrl": shortURL}}}, {quoted: fkontak});
-m.react(done)
+        push.push({
+            body: proto.Message.InteractiveMessage.Body.fromObject({
+                text: `◦ *Título:* ${track.title} \n◦ *Artistas:* ${track.artist} \n◦ *Duración:* ${track.duration} \n◦ *Popularidad:* ${track.popularity} \n◦ *Fecha:* ${track.publish}`
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.fromObject({
+                text: '' 
+            }),
+            header: proto.Message.InteractiveMessage.Header.fromObject({
+                title: '',
+                hasMediaAttachment: true,
+                imageMessage: image 
+            }),
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+                buttons: [
+{
+"name": "cta_copy",
+"buttonParamsJson": "{\"display_text\":\"🎧 ¡Descargar Audio! 🎧\",\"id\":\"123456789\",\"copy_code\":\".spotify " + track.url + "\"}"
+},
+]
+})
+});
+}
+
+const msg = generateWAMessageFromContent(m.chat, {
+viewOnceMessage: {
+message: {
+messageContextInfo: {
+deviceListMetadata: {},
+deviceListMetadataVersion: 2
+},
+interactiveMessage: proto.Message.InteractiveMessage.fromObject({
+body: proto.Message.InteractiveMessage.Body.create({text: '*`\Resultados de:\`* ' + `${text}`}),
+footer: proto.Message.InteractiveMessage.Footer.create({text: '_\`ꜱ\` \`ᴘ\` \`-\` \`ꜱ\` \`ᴇ\` \`ᴀ\` \`ʀ\` \`ᴄ\` \`ʜ\`_'}),
+header: proto.Message.InteractiveMessage.Header.create({hasMediaAttachment: false}),
+carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({cards: [...push]})
+})
+}}}, {
+    'quoted': m
+  });
+
+await m.react('✅');
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 } catch (error) {
+console.error(error)
 }}
-handler.tags = ['descargas']
-handler.help = ['spotify']
-handler.command = /^(spotify|music)$/i
-handler.estrellas = 8;
+
+handler.help = ["spotifysearch *<texto>*"]
+handler.tags = ["search"]
+handler.command = /^(spotifysearch)$/i
 
 export default handler
