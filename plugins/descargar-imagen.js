4e5a69558f1e2abe48a2c55d24791d061b5bd4f0
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-14 22:03:31 -0600

    Actualizar descargar-imagen.js

--- plugins/descargar-imagen.js
+++ plugins/descargar-imagen.js
@@ -1,7 +1,7 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `*💛 Uso Correcto: ${usedPrefix + command} Nakano Nino*`, m, rcanal);
-conn.reply(m.chat, '💛 *Descargando su imagen...*', m, {
+if (!text) throw `*🎩 Uso Correcto: ${usedPrefix + command} Kakaroto*`;
+conn.reply(m.chat, '🍭 *Descargando su imagen...*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
@@ -12,11 +12,11 @@ const image = await res.getRandom();
 const link = image;
 const messages = [['Imagen 1', dev, await res.getRandom(),
 [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
-await conn.sendCarousel(m.chat, `💛 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
+await conn.sendCarousel(m.chat, `🎩 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
 };
 handler.help = ['imagen <query>'];
-handler.tags = ['buscador', 'descargas'];
-handler.command = ['image', 'imagen'];
-handler.register = true;
-handler.estrellas = 8;
+handler.tags = ['buscador','descargas'];
+handler.command = ['image','imagen', 'wimage'];
+handler.group = true;
+handler.register = true
 export default handler;
