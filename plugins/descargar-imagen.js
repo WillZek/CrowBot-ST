26f4420f1aa1bc9743e3788169435289779fb7bb
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-15 20:53:21 -0600

    Actualizar descargar-imagen.js

--- plugins/descargar-imagen.js
+++ plugins/descargar-imagen.js
@@ -1,15 +1,19 @@
+import Starlights from "@StarlightsTeam/Scraper"
 
-import { pinterest } from '@bochilteam/scraper'
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ ${mssg.example}: ${usedPrefix + command} Lil Peep`
-  const json = await pinterest(text)
-  conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
-*❖  Pinterest:*  ${text}
-`.trim(), m)
-}
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Crow De Brawl Stars`)
+await m.react('🕓')
+try {
+let { dl_url } = await Starlights.pinterest(text)
+await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*» Resultado* : ${text}`, m, null, rcanal)
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
 handler.help = ['pinterest', 'imagen']
-handler.tags = ['buscador']
-handler.command = ['pinterest', 'imagen'] 
+handler.tags = ['img']
+handler.command = ['pinterest', 'imagen']
+handler.register = true 
 handler.estrellas = 5;
 
 export default handler
