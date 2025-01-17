2ae7ee37a86c5812a5e8df35170dd94ce4e59f8f
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-17 16:36:49 -0600

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -144,7 +144,7 @@ serbot()
 }
 handler.help = ["serbot"]
 handler.tags = ["serbot"]
-handler.command = ["serbot", "jadibot", "qr", "botclone"]
+handler.command = ["serbot", "jadibot", "qr", "serbotqr"]
 handler.estrellas = 13;
 // handler.register = true
 export default handler
