c36d84e4d521740107a3be2cd1fba3d5caf273e3
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-17 13:40:35 -0600

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, isROwner, text }) => {
             await conn.sendMessage(m.chat, { text: progreso[i], edit: key });
         }
 
-        await conn.sendMessage(m.chat, { text: "『✅』*CrowBot* reiniciado con éxito espera unos segundos asta que el proceso termine.", edit: key });
+        await conn.sendMessage(m.chat, { text: "『✅』𝗖𝗿𝗼𝘄𝗕𝗼𝘁 reiniciado con éxito espera unos segundos asta que el proceso termine.", edit: key });
 
         await delay(2000);
         process.send('reset');
