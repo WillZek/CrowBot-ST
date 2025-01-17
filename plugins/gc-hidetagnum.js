1cc1f0c68e42b46b8cd02140dcfb9dd7c8720ba0
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-17 17:59:32 -0600

    Actualizar gc-hidetagnum.js

--- plugins/gc-hidetagnum.js
+++ plugins/gc-hidetagnum.js
@@ -3,6 +3,8 @@ const handler = async (m, {conn, args, groupMetadata, participants, usedPrefix, 
   if (!args[0]) return m.reply(`* 🎩 Ingrese EL Mensaje + El Prefijo\n> Ejemplo: ${usedPrefix + command} 52*`);
   if (isNaN(args[0])) return m.reply(`*🍭 Ingrese Algun Prefijo De Un Pais: ${usedPrefix + command} 52*`);
   const lol = args[0].replace(/[+]/g, '');
+  const pesan = args.join` `;
+  const colombia = `🎩 *Mensaje:* ${pesan}`;
   const ps = participants.map((u) => u.id).filter((v) => v !== conn.user.jid && v.startsWith(lol || lol));
   const bot = global.db.data.settings[conn.user.jid] || {};
   if (ps == '') return m.reply(`*🍭 Aqui No Hay Ningun Numero Con El Prefijo +${lol}*`);
@@ -10,7 +12,7 @@ const handler = async (m, {conn, args, groupMetadata, participants, usedPrefix, 
   const delay = (time) => new Promise((res)=>setTimeout(res, time));
   switch (command) {
     case 'hidnum': case 'hidetagnum':
-      conn.reply(m.chat, `*MENSAJE ESPECIAL PARA +${lol} QUE ESTAN EN ESTE GRUPO:*\n\n` + numeros.join`\n`, m, {mentions: ps});
+      conn.reply(m.chat, `*MENSAJE ESPECIAL PARA +${lol} QUE ESTAN EN ESTE GRUPO:*\n` + `${colombia}\n\n` + numeros.join`\n`, m, {mentions: ps});
       break;
       const ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net';
       const users = participants.map((u) => u.id).filter((v) => v !== conn.user.jid && v.startsWith(lol || lol));
