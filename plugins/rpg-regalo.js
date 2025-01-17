095f519a4008ba41bf112e2b169cac67932622df
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-17 11:44:57 -0600

    Actualizar rpg-regalo.js

--- plugins/rpg-regalo.js
+++ plugins/rpg-regalo.js
@@ -1,14 +1,16 @@
 
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `🎩 *@${m.sender.split('@')[0]} CrowBot Te Ha Regalado:*\n> 200 Estrellas\n> 200 Experiencia\n> 200 CrowCoins`, mentions: [m.sender]}, {quoted: fkontak});
-      global.db.data.users[m.sender].money = 200;
-    global.db.data.users[m.sender].estrellas = 200;
-  global.db.data.users[m.sender].level = 200;
- global.db.data.users[m.sender].exp = 200;
+    conn.sendMessage(m.chat, {text: `🎩 *@${m.sender.split('@')[0]} CrowBot Te Ha Regalado:*\n> 🌟 200 Estrellas\n> 💶 200 Experiencia\n> 🪙 200 CrowCoins`, mentions: [m.sender]}, {quoted: fkontak});
+     
+    user.money += 200;
+    user.estrellas += 200;
+    user.exp += 200;
 };
+
 handler.help = ['regalo'];
 handler.tags = ['rpg'];
 handler.command = /^(regalo|regalocrow|crowregalo)$/i;
 handler.fail = null;
+
 export default handler;
