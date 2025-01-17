1427f9ce2202c6c244e38774ea123cbd0bbc3802
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-17 11:52:00 -0600

    Actualizar rpg-regalo.js

--- plugins/rpg-regalo.js
+++ plugins/rpg-regalo.js
@@ -1,5 +1,8 @@
-
 const handler = async (m, { conn }) => {
+
+let time = global.db.data.users[m.sender].lastclaim + 864000000; //10 dias
+if (new Date - global.db.data.users[m.sender].lastclaim < 864000000) return conn.reply(m.chat, `*Ya Has Reclamado El Regalo De CrowBot💛, Vuelve En ${msToTime(time - new Date())}*`, m, );
+
     const user = global.db.data.users[m.sender];
     conn.sendMessage(m.chat, {text: `🎩 *@${m.sender.split('@')[0]} CrowBot Te Ha Regalado:*\n> 🌟 200 Estrellas\n> 💶 200 Experiencia\n> 🪙 200 CrowCoins`, mentions: [m.sender]}, {quoted: fkontak});
      
@@ -14,3 +17,20 @@ handler.command = /^(regalo|regalocrow|crowregalo)$/i;
 handler.fail = null;
 
 export default handler;
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
+
+function msToTime(duration) {
+var days = Math.floor(duration / (1000 * 60 * 60 * 24));
+var hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((duration % (1000 * 60)) / 1000);
+
+days = (days < 10) ? '0' + days : days;
+hours = (hours < 10) ? '0' + hours : hours;
+minutes = (minutes < 10) ? '0' + minutes : minutes;
+seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+return days + ' Días ' + hours + ' Horas ' + minutes + ' Minutos';
+}
