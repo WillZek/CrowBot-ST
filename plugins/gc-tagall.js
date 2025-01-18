087a9614a2ff9e23156e8785cbaeb4dcf6409b86
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-17 20:45:55 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,26 +1,29 @@
-
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
+
+  const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '🍭';
+  m.react(customEmoji);
+
   if (!(isAdmin || isOwner)) {
     global.dfail('admin', m, conn);
-    return;
+    throw false;
   }
-  const pesan = args.join` `;
-  const colombia = `💌 *Mensaje:* ${pesan}`;
-  let teks = `💛 *Revivan Plantas*\n${colombia}\n\n┏ ✰𝐂𝐫𝐨𝐰𝐁𝐨𝐭-𝐒𝐓\n`;
-  
-  for (const mem of participants) {
-    teks += `┋🔱@${mem.id.split('@')[0]}\n`;
-  }
-  
-  teks += `┗ ${dev}`;
 
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+  const pesan = args.join` `;
+  const oi = `*» INFO :* ${pesan}`;
+  let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄  ۪꒰ \`⡞᪲=͟͟͞🄲ꭈׁׅo͓̽ᨰׁׅʙo͓̽tׁׅ ≼᳞ׄ\` ꒱  ۟  𝅄 ┄\n`;
+  for (const mem of participants) {
+    teks += `┊${customEmoji} @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
+
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) });
 };
 
-handler.help = ['tagall *<mensaje>*', 'invocar *<mensaje>*'];
-handler.tags = ['grupo'];
-handler.command = ['tagall', 'invocar', 'todos'];
+handler.help = ['todos *<mensaje opcional>*'];
+handler.tags = ['group'];
+handler.command = /^(tagall|invocar|marcar|todos|invocación)$/i;
 handler.admin = true;
 handler.group = true;
+
 export default handler;
