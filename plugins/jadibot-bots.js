b89f9827981a1be4f9eae041a2f60c2ae63985b2
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-17 21:55:59 -0600

    Actualizar plugins

---
+++ plugins/jadibot-bots.js
@@ -0,0 +1,46 @@
+import fs from 'fs'
+import ws from 'ws'
+
+async function handler(m, { conn: stars, usedPrefix }) {
+  let uniqueUsers = new Map()
+
+  if (!global.conns || !Array.isArray(global.conns)) {
+    global.conns = []
+  }
+
+  global.conns.forEach((conn) => {
+    if (conn.user && conn.ws?.socket?.readyState !== ws.CLOSED) {
+      uniqueUsers.set(conn.user.jid, conn)
+    }
+  })
+
+  let users = [...uniqueUsers.values()]
+  let totalUsers = uniqueUsers.size
+
+  let img = fs.readFileSync('./storage/img/avatar_contact.png')
+
+  let message = users.map((v, index) => `
+*[ \`${index + 1}\` - ${v.user.name || 'Sin Nombre'} ]*
+🤍 *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}
+`).join('\n\n')
+
+  let responseMessage = `🍭 *Subbots Activos: ${totalUsers}*\n\n${message.trim() || '_No hay subbots activos en este momento._'}`
+
+  await stars.sendFile(
+    m.chat,
+    img,
+    'thumbnail.jpg',
+    responseMessage,
+    m,
+    null,
+    fake,
+    false,
+    { mentions: stars.parseMention(responseMessage) }
+  )
+}
+
+handler.command = ['listjadibot', 'bots']
+handler.help = ['bots']
+handler.tags = ['serbot']
+
+export default handler
