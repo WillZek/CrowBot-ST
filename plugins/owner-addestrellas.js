86c53ac27a56ca37314276c0451156904749ddef
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-19 12:36:59 -0600

    Actualizar owner-addestrellas.js

--- plugins/owner-addestrellas.js
+++ plugins/owner-addestrellas.js
@@ -9,6 +9,10 @@ let handler = async (m, { conn, text }) => {
     if (!who) return m.reply('⚠️️ *Taguea al usuario*')
     let txt = text.replace('@' + who.split`@`[0], '').trim()
     if (!txt) return m.reply('⚠️️ Ingrese la cantidad de *Estrellas* que quiere añadir')
+        const userNumber = user.split('@')[0];
+        if (!global.global.db.data.users[user] || global.global.db.data.users[user] == '') {
+            return conn.sendMessage(m.chat, {text: `⚠️ *El usuario @${userNumber} no se encuentra en mi base de datos.*`, mentions: [user]}, {quoted: fkontak});
+         }
     if (isNaN(txt)) return m.reply('⚠️ *sólo números*')
     let len = parseInt(txt)
     let estrellas = len
