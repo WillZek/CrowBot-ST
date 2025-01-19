186cca17628cd6861b6280a7f1f67edd5534a76c
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-18 22:58:45 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -11,7 +11,7 @@ try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`☁️ *¡El video no puede durar mas de 8 segundos!*`)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply(`🎩 *¡El video no puede durar mas de 10 segundos!*`)
 let img = await q.download?.()
 
 if (!img) return conn.reply(m.chat, `${emojis} *_Y el video ?, intenta enviar primero imagen/video/gif y luego responde con el comando._*`, m, rcanal)
