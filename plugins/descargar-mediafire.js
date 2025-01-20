185553d56760730a360483d5f4f9178674b24caf
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-19 18:10:00 -0600

    Actualizar descargar-mediafire.js

--- plugins/descargar-mediafire.js
+++ plugins/descargar-mediafire.js
@@ -8,11 +8,7 @@ let handler = async (m, { conn, text }) => {
         let api = await fetch(`https://restapi.apibotwa.biz.id/api/mediafire?url=${text}`)
         let json = await api.json()
         let { filename, type, size, uploaded, ext, mimetype, download: dl_url } = json.data.response
-        m.reply(`*${filename}*
-
-- *Tipo :* ${type}
-- *Tamaño :* ${size}
-- *Creado :* ${uploaded}`)
+        m.reply(`*${espera}`)
         await m.react('✅');
         await conn.sendFile(m.chat, dl_url, filename, null, m, null, { mimetype: ext, asDocument: true })
     } catch (error) {
