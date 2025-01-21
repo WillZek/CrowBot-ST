3756b9fa00b7d18751d6e440828d799140052920
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-21 13:18:37 -0600

    Actualizar _youtube-video.js

--- plugins/_youtube-video.js
+++ plugins/_youtube-video.js
@@ -2,7 +2,7 @@ import Starlights from '@StarlightsTeam/Scraper'
 let limit = 300
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
 if (!m.quoted) return conn.reply(m.chat, `Responde al resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
-if (!m.quoted.text.includes("Y O U T U B E")) return conn.reply(m.chat, `Responde al resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
+if (!m.quoted.text.includes("Youtube Download")) return conn.reply(m.chat, `Responde al resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
 let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
 if (!urls) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
 if (urls.length < text) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
@@ -21,7 +21,7 @@ await m.react('✅')
 await m.react('✖️')
 }}
 handler.help = ['Video']
-handler.tags = ['downloader']
+handler.tags = ['descargas']
 handler.customPrefix = /^(Video|video|vídeo|Vídeo)/
 handler.command = new RegExp
 //handler.limit = 1
