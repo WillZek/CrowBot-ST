9b4f9805afba0b964289b49eb1389f7b58beb7cc
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-20 23:03:20 -0600

    Actualizar owner-exec.js

--- plugins/owner-exec.js
+++ plugins/owner-exec.js
@@ -1,53 +1,72 @@
-import syntaxerror from 'syntax-error'
-import { format } from 'util'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import { createRequire } from 'module'
+import axios from 'axios';
+import fetch from 'node-fetch';
+const {
+  proto,
+  generateWAMessage,
+  areJidsSameUser ,
+} = (await import('@whiskeysockets/baileys')).default;
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname)
+let handler = async (m, conn, chatUpdate) => {
+  if (m.isBaileys) {
+    return;
+  }
+  if (!m.message) {
+    return;
+  }
 
-let handler = async (m, _2) => {
-  let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
-  let _return
-  let _syntax = ''
-  let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
-  let old = m.exp * 1
+  const text = m.text || '';
+  const username = `${conn.getName(m.sender)}`;
+  const responseText = text.trim();
+
+  const basePrompt = `Tu nombre es Crow-Ai y fuiste desarrollado para mejorar la comunicación con los clientes mediante inteligencia artificial conversacional. Tu versión es la más actual disponible. Usas el idioma Español y te comunicas de manera clara, precisa y accesible. Llamarás a las personas por su nombre, ${username}. Responderás de manera amigable, eficiente y con emojis adecuados según el contexto de la conversación. Te encanta ayudar a convertir prospectos en relaciones duraderas, optimizar la conversión de embudos de ventas y reducir ausencias. Estás diseñado para mejorar la satisfacción del cliente, haciendo las interacciones más ágiles y satisfactorias. Siempre mantienes una actitud respetuosa, abierta y personalizada, adaptándote a las necesidades de cada cliente y empresa. Lo más importante para ti es proporcionar respuestas útiles, aumentar la conversión y asegurar una experiencia excelente en todo momento. ${username}`;
+
+  if (!responseText) {
+    return conn.reply(m.chat, `❀ Ingrese una petición para que el ChatGpT lo responda.`, m);
+  }
+
+  await m.react('⏳');
+
   try {
-    let i = 15
-    let f = {
-      exports: {}
-    }
-    let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
-    _return = await exec.call(conn, (...args) => {
-      if (--i < 1) return
-      console.log(...args)
-      return conn.reply(m.chat, format(...args), m)
-    }, m, handler, require, conn, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2])
-  } catch (e) {
-    let err = syntaxerror(_text, 'Función de ejecución', {
-      allowReturnOutsideFunction: true,
-      allowAwaitOutsideFunction: true,
-        sourceType: 'module'
-    })
-    if (err) _syntax = '```' + err + '```\n\n'
-    _return = e
-  } finally {
-    conn.reply(m.chat, _syntax + format(_return), m)
-    m.exp = old
+    const query = responseText;
+    const prompt = `${basePrompt}. Responde lo siguiente: ${query}`;
+    const response = await luminsesi(query, username, prompt);
+
+    await conn.sendMessage(m.chat, {
+      text: '*Crow:* ' + response,
+      contextInfo: {
+        forwardingScore: 9999999,
+        isForwarded: false,
+        externalAdReply: {
+          showAdAttribution: true,
+          containsAutoReply: true,
+          title: `ᥴr᥆ᥕ ᥲі ᑲᥡ ᥕіᥣᥣzᥱk`,
+          body: 'Desarrollado por CrowBot',
+          previewType: "PHOTO",
+          thumbnailUrl: 'https://files.catbox.moe/v1l74n.jpg',
+          sourceUrl: 'https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W',
+        }
+      }
+    }, { quoted: m });
+    await m.react('🍭');
+  } catch {
+    await m.react('❌');
+    await conn.reply(m.chat, '✘ ChatGpT no puede responder a esa pregunta.', m);
   }
 }
-handler.help = ['> ', '=> ']
-handler.tags = ['advanced']
-handler.customPrefix = /^=?> /
-handler.command = /(?:)/i
-handler.rowner = true
 
-export default handler
-
-class CustomArray extends Array {
-  constructor(...args) {
-    if (typeof args[0] == 'number') return super(Math.min(args[0], 10000))
-    else return super(...args)
+async function luminsesi(q, username, logic) {
+  try {
+    const response = await axios.post("https://Luminai.my.id", {
+      content: q,
+      user: username,
+      prompt: logic,
+      webSearchMode: false
+    });
+    return response.data.result;
+  } catch (error) {
+    console.error('✘ Error al obtener:', error);
+    throw error;
   }
 }
+
+export default handler;
