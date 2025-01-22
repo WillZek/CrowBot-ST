7a31c4248a63f84675db0de476221e355f7d7bc6
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-22 13:36:18 -0600

    Actualizar descargas-spotifydl.js

--- plugins/descargas-spotifydl.js
+++ plugins/descargas-spotifydl.js
@@ -2,20 +2,21 @@ import Starlights from '@StarlightsTeam/Scraper';
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 Ingresa el enlace de algún Track, Playlist o Álbum de Spotify.', m);
+if (!text) return conn.reply(m.chat, '🍭 Ingresa el enlace de algún Track, Playlist o Álbum de Spotify Para Descargarlo.\n> Ejemplo: .spotifydl https://open.spotify.com/track/2Tk9rrNo99svFeD7vClX0M', m);
 
 let isSpotifyUrl = text.match(/^(https:\/\/open\.spotify\.com\/(album|track|playlist)\/[a-zA-Z0-9]+)/i);
-if (!isSpotifyUrl) return conn.reply(m.chat, '🚩 Ingresa un enlace válido de Track, Playlist o Álbum de Spotify.', m);
+if (!isSpotifyUrl) return conn.reply(m.chat, '🍭 Ingresa un enlace válido de Track, Playlist o Álbum de Spotify.\n> Ejemplo: .spotifydl https://open.spotify.com/track/2Tk9rrNo99svFeD7vClX0M', m);
 
 await m.react('🕓')
 try {
 let { title, artist, album, thumbnail, dl_url } = await Starlights.spotifydl(text);
 let img = await (await fetch(thumbnail)).buffer();
 
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`;
-    txt += `    ✩  *Título* : ${title}\n`;
-    txt += `    ✩  *Álbum* : ${album}\n`;
-    txt += `    ✩  *Artista* : ${artist}\n\n`;
+let txt = `*S P O T I F Y  -  D O W N L O A D*\n\n`;
+    txt += ` 💛  *Título* : ${title}\n`;
+    txt += ` 💛  *Álbum* : ${album}\n`;
+    txt += ` 💛  *Artista* : ${artist}\n\n`;
+    txt += ` 💛  *Link:* ${dl_url}\n`;
     txt += `*- ↻ Los audios se están enviando, espera un momento. . .*`;
 
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal);
