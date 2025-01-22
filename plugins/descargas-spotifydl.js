3ce1e8f00c1d6c5c02b1ac7f89b1519dd99f3f50
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-22 13:41:56 -0600

    Actualizar descargas-spotifydl.js

--- plugins/descargas-spotifydl.js
+++ plugins/descargas-spotifydl.js
@@ -17,7 +17,7 @@ let txt = `*S P O T I F Y  -  D O W N L O A D*\n\n`;
     txt += ` 💛  *Álbum* : ${album}\n`;
     txt += ` 💛  *Artista* : ${artist}\n\n`;
     txt += ` 💛  *Link:* ${dl_url}\n`;
-    txt += `*- ↻ Los audios se están enviando, espera un momento. . .*`;
+    txt += `*${espera}*`;
 
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal);
 await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m });
