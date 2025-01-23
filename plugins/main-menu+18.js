38084b03551ee22b893aa19becb994299f92538b
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-22 21:16:30 -0600

    Actualizar plugins

---
+++ plugins/main-menu+18.js
@@ -0,0 +1,38 @@
+let handler = async (m, { isPrems, conn }) => {
+
+let img = 'https://files.catbox.moe/16bm7g.jpg' 
+let texto = `• 𝐌𝐄𝐍𝐔 +10 •
+•°「 🔞 *COMANDOS NOPOR* 🔞 」°•
+
+║➤ ❤️‍🔥 _.xnxxsearch_ (texto)
+║➤ ❤️‍🔥 _.xnxxdl_ (link)
+║➤ ❤️‍🔥 _.xvideosearch_ (texto)
+║➤ ❤️‍🔥 _.xvideosdl_ (link)
+║➤ ❤️‍🔥 _.fuck_ (@tag)
+║➤ ❤️‍🔥 _.fuck2_ (@tag)
+║➤ ❤️‍🔥 _.cum_ (@tag)
+║➤ ❤️‍🔥 _.follar_ (@tag)
+║➤ ❤️‍🔥 _.penetrar_ (@tag)
+║➤ ❤️‍🔥 _.horny_ (@tag)
+║➤ ❤️‍🔥 _.violar_ (@tag)`
+
+const fkontak = {
+        "key": {
+    "participants":"0@s.whatsapp.net",
+                "remoteJid": "status@broadcast",
+                "fromMe": false,
+                "id": "Halo"
+        },
+        "message": {
+                "contactMessage": {
+                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                }
+        },
+        "participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
+}
+handler.help = ['menuhot (menu +18)']
+handler.tags = ['crow']
+handler.command = ['menu18', 'menuhorny', 'menunsfw', 'menuhot'] 
+export default handler;
