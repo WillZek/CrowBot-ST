9b054b9a1f0dace2f6f25c3a14da33796f14f2bb
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-23 12:03:24 -0600

    Actualizar stars-brawlfifus.js

--- plugins/stars-brawlfifus.js
+++ plugins/stars-brawlfifus.js
@@ -52,7 +52,8 @@ let rollHandler = async (m, { conn }) => {
         await conn.sendFile(m.chat, randomCharacter.img, `${randomCharacter.name}.jpg`, message, m);
         cooldowns[userId] = now + 15 * 60 * 1000;
 
-    user.estrellas += random.Character.value;
+const brawler = ${randomCharacter.value};
+    user.estrellas += brawler;
 
     } catch (error) {
         await conn.reply(m.chat, `✘ Error al cargar el personaje: ${error.message}`, m);
