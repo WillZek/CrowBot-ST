38daa2ff9039d9003e020ffc8e0a4423cc96c06a
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-25 12:07:03 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,22 +1,27 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-     if (!m.mentionedJid[0] && !m.quoted) { return conn.reply(m.chat, `🍭 *Etiqueta Ala persona que deseas mutar*`, m);
-}
+    if ((!m.mentionedJid || !m.mentionedJid.length) && !m.quoted) { 
+        return conn.reply(m.chat, `🍭 *Etiqueta a la persona que deseas mutar*`, m);
+    }
 
-    let delet = m.message.extendedTextMessage.contextInfo.participant;
-    let bang = m.message.extendedTextMessage.contextInfo.stanzaId;
+    let delet;
+    if (m.quoted) {
+        delet = m.quoted.message.extendedTextMessage.contextInfo.participant;
+    } else if (m.mentionedJid && m.mentionedJid.length > 0) {
+        delet = m.mentionedJid[0];
+    }
 
     await conn.groupParticipants(m.chat, [delet], 'mute');
 
     conn.reply(m.chat, `🔇 ${delet} ha sido muteado y sus mensajes serán eliminados.`, m);
-    
-    conn.on('chat-update', async (chatUpdate) => {
-        if (!chatUpdate.messages) return;
-        const message = chatUpdate.messages.all()[0];
-        if (message.key.participant === delet) {
-            await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: message.key.id, participant: delet }});
-        }
-    });
 }
+
+conn.on('chat-update', async (chatUpdate) => {
+    if (!chatUpdate.messages) return;
+    const message = chatUpdate.messages.all()[0];
+    if (message.key.participant === delet) {
+        await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: message.key.id, participant: delet }});
+    }
+});
 
 handler.help = ['mute']
 handler.tags = ['owner']
