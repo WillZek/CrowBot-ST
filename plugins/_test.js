2ac6704d3ed15fb6377fa23758c04f6a88ebd3c1
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-25 12:20:13 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -26,7 +26,7 @@ conn.on('chat-update', async (chatUpdate) => {
 
 handler.help = ['mute']
 handler.tags = ['owner']
-handler.command = /^mute$/i
+handler.command = /^mute|test$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
