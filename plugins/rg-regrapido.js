b6779cc85fbf8d6b5a2710a3bd170fe3b580e8c5
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-26 18:42:52 -0600

    Actualizar rg-regrapido.js

--- plugins/rg-regrapido.js
+++ plugins/rg-regrapido.js
@@ -1,13 +1,23 @@
 
 let handler = async (m, { conn, text }) => {
 
-    let img = 'https://i.ibb.co/GMTyKcN/file.jpg';
-
     let user = global.db.data.users[m.sender]
 
     user.registered = true;
-    return conn.sendMessage(m.chat, { image: { url: img, caption: '*『✅』 Usted Está Ahora Registrado En Mi Base De Datos*' } }, { quoted: m });
-}
+    await conn.sendMessage(m.chat, {
+        text: '『✅』 Usted Está Ahora Registrado En Mi Base De Datos',
+        contextInfo: {
+            externalAdReply: {
+                title: '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰',
+                body: dev,
+                thumbnailUrl: 'https://i.ibb.co/GMTyKcN/file.jpg',
+                sourceUrl: 'https://whatsapp.com/channel/0029Vb1kImN42Dcn99y1rW0E',
+                mediaType: 1,
+                showAdAttribution: true,
+                renderLargerThumbnail: true
+            }
+        }
+    }, { quoted: m });
 
 handler.help = ['regaut'];
 handler.tags = ['rg'];
