d45bb92a99124ed0b4fc871273dc64418983e317
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-26 18:52:41 -0600

    Actualizar rg-regrapido.js

--- plugins/rg-regrapido.js
+++ plugins/rg-regrapido.js
@@ -1,16 +1,49 @@
+import axios from 'axios'
+import { createHash } from 'crypto'
+import PhoneNumber from 'awesome-phonenumber'
+import moment from 'moment-timezone'
 
-let handler = async (m, { conn, text }) => {
-
+let handler = async function (m, { conn, usedPrefix, command }) {
     let user = global.db.data.users[m.sender]
+    let defaultName = conn.getName(m.sender) || "UsuarioDesconocido"
+    let defaultAge = 18
 
-    user.registered = true;
+    if (user.registered === true) {
+        return m.reply(`💛 𝗬𝗮 𝘁𝗲 𝗲𝗻𝗰𝘂𝗲𝗻𝘁𝗿𝗮𝘀 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗮𝗱𝗼.\n\n¿𝗤𝘂𝗶𝗲𝗿𝗲 𝘃𝗼𝗹𝘃𝗲𝗿 𝗮 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗮𝗿𝘀𝗲?\n\n𝗨𝘀𝗲 𝗲𝘀𝘁𝗲 𝗰𝗼𝗺𝗮𝗻𝗱𝗼 𝗽𝗮𝗿𝗮 𝗲𝗹𝗶𝗺𝗶𝗻𝗮𝗿 𝘀𝘂 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗼.\n*${usedPrefix}unreg*`)
+    }
+
+    user.name = defaultName.trim()
+    user.age = defaultAge
+    user.regTime = +new Date
+    user.registered = true
+  global.db.data.users[m.sender].estrellas += 5
+    global.db.data.users[m.sender].exp += 100
+    global.db.data.users[m.sender].joincount += 3    
+
+    let sn = createHash('md5').update(m.sender).digest('hex')
+    let regbot = `┏━━━━━━━━━━━━━━━━━━⬣
+┃⋄ *🎩 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐎 - 𝐂𝐑𝐎𝐖𝐁𝐎𝐓*
+┗━━━━━━━━━━━━━━━━━━⬣\n`
+    regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
+    regbot += `「💛」𝐍𝐨𝐦𝐛𝐫𝐞: ${defaultName}\n`
+    regbot += `「💛」𝐄𝐝𝐚𝐝: ${defaultAge} años\n`
+    regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
+    regbot += `「💝」𝐑𝐞𝐜𝐨𝐦𝐩𝐞𝐧𝐬𝐚𝐬:\n> `
+    regbot += `• 5 Estrellas 🌟\n> `
+    regbot += `• 3 CrowCoins 🪙\n> `
+    regbot += `• 100 Experiencia 💸\n> `
+    regbot += `• 5 Tokens 💰\n`
+    regbot += `꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷\n> `
+    regbot += `🍭 Verifica Tu Registro Aqui 👇🏻`
+
+    await m.react('📪')
     await conn.sendMessage(m.chat, {
-        text: '『✅』 Usted Está Ahora Registrado En Mi Base De Datos',
+        text: regbot,
         contextInfo: {
             externalAdReply: {
                 title: '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰',
-                body: dev,
-                thumbnailUrl: 'https://i.ibb.co/GMTyKcN/file.jpg',
+                body: '¡Bienvenido a CrowBot!',
+                thumbnailUrl: 'https://files.catbox.moe/lger9i.jpg',
                 sourceUrl: 'https://whatsapp.com/channel/0029Vb1kImN42Dcn99y1rW0E',
                 mediaType: 1,
                 showAdAttribution: true,
@@ -19,8 +52,16 @@ let handler = async (m, { conn, text }) => {
         }
     }, { quoted: m });
 
-handler.help = ['regaut'];
-handler.tags = ['rg'];
-handler.command = ['regaut', 'regautomatico', 'regautomatic', 'regrapido', 'regrap', 'verifyaut'];
-handler.register = false;
+    let channelID = '120363387375075395@newsletter';
+    let messageContent = `◉ *Usuarios:* ${m.pushName || 'Anónimo'}\n◉ *Verificación:* ${defaultName}\n◉ *Edad:* ${defaultAge} años\n◉ *Número de serie:*\n⤷ ${sn}\n\n🎁 *Recompensa:* 600 crowcoins 🪙\n*¡Bienvenido/a al bot!*`;
+
+    await conn.sendMessage(channelID, {
+        text: messageContent
+    });
+}
+
+handler.help = ['regaut']
+handler.tags = ['rg']
+handler.command = ['regaut']
+
 export default handler
