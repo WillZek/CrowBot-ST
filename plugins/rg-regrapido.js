88e49d41a89db0b79e15e6aa4b181d709fe41316
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-26 18:39:52 -0600

    Actualizar rg-regrapido.js

--- plugins/rg-regrapido.js
+++ plugins/rg-regrapido.js
@@ -1,13 +1,14 @@
 
 let handler = async (m, { conn, text }) => {
 
-let img = 'https://i.ibb.co/GMTyKcN/file.jpg';
+    let img = 'https://i.ibb.co/GMTyKcN/file.jpg';
 
-let user = global.db.data.users[m.sender]
+    let user = global.db.data.users[m.sender]
 
-user.registered = true
-return conn.sendMessage(m.chat, { image: { url: img, caption: '*『✅』 Usted Ya No Está En Mi Base De Datos*' } }, { quoted: m });
+    user.registered = true;
+    return conn.sendMessage(m.chat, { image: { url: img, caption: '*『✅』 Usted Está Ahora Registrado En Mi Base De Datos*' } }, { quoted: m });
 }
+
 handler.help = ['regaut'];
 handler.tags = ['rg'];
 handler.command = ['regaut', 'regautomatico', 'regautomatic', 'regrapido', 'regrap', 'verifyaut'];
