8b39f982d43f81f17ff445481ffb97d9326b6f4f
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-29 21:08:02 -0600

    Actualizar ComprarBot.js

--- plugins/ComprarBot.js
+++ plugins/ComprarBot.js
@@ -1,16 +1,11 @@
 // Créditos A Niño Piña
 let handler = async (m, { conn }) => {
-// No Quites Los Créditos🦌
-m.react('🎄');
-// Mensaje que se enviará
-const message = "〔⛄ *CROW - BOT* ❄️〕\n\n> *🦌FELIZ NAVIDAD🎄*\n\n*BOT PARA GRUPO* :\n> wa.me/50557865603\n\n*BOT PERZONALIZADO* :\n> wa.me/50557865603";
+m.react('🍭');
+const message = "〔🍭 *CROW - BOT* 📩〕\n\n> *🎩 HOLA ✨*\n\n*BOT PARA GRUPO* :\n> wa.me/50557865603\n\n*BOT PERZONALIZADO* :\n> wa.me/50557865603";
 if (m, rcanal) {
-// Feliz Navidad🎄
-const imageUrl = 'https://i.ibb.co/qJNL5Bg/file.jpg';
+const img = 'https://i.ibb.co/qJNL5Bg/file.jpg';
 try {
-// Que No Quites Los Créditos😑
-// Te Estoy Viendo😑
-await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message, mimetype: 'image/jpeg' }, { quoted: fkontak });
+await conn.sendMessage(m.chat, { image: { url: img }, caption: message, mimetype: 'image/jpeg' }, { quoted: fkontak });
 } catch (error) {
 console.error('Error al enviar el mensaje:', error);
 }
