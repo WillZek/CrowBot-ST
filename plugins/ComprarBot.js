9f827f9501a8afeca7f898ce50d814fade950fb0
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-29 21:14:34 -0600

    Actualizar ComprarBot.js

--- plugins/ComprarBot.js
+++ plugins/ComprarBot.js
@@ -1,9 +1,9 @@
-// Créditos A Niño Piña
+// Código sencillo 
 let handler = async (m, { conn }) => {
 m.react('🍭');
 const message = "〔🍭 *CROW - BOT* 📩〕\n\n> *🎩 HOLA ✨*\n\n*BOT PARA GRUPO* :\n> wa.me/50557865603\n\n*BOT PERZONALIZADO* :\n> wa.me/50557865603";
 if (m, rcanal) {
-const img = 'https://i.ibb.co/qJNL5Bg/file.jpg';
+const img = 'https://files.catbox.moe/kd7vs5.jpg';
 try {
 await conn.sendMessage(m.chat, { image: { url: img }, caption: message, mimetype: 'image/jpeg' }, { quoted: fkontak });
 } catch (error) {
