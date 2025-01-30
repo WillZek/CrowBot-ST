b42ef66c4666a8d3291af0138de2adeb18d6322d
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-29 19:30:03 -0600

    Actualizar descargas-pornhubdl.js

--- plugins/descargas-pornhubdl.js
+++ plugins/descargas-pornhubdl.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     return m.reply(m.chat, '[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw', m);
   }
-if (!text) return conn.reply(m.chat, '❀ ingresa un link de pornhub', m)
+if (!text) return conn.reply(m.chat, '❀ Ingresa un link de Pornhub Para Descargar Su Video 🍭', m)
 try {
 let api = await fetch(`https://www.dark-yasiya-api.site/download/phub?url=${text}`)
 let json = await api.json()
