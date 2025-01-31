724d87b2397fcb9c09c8fb045867a41f7c77bc8f
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-30 19:44:31 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -4,3 +4,5 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 
 if (!text) return m.reply('🎩 Ingresa Un Texto Que Deseas Buscar En Google');
 
+let api = `https://delirius-apiofc.vercel.app/search/gimage?query=${text}`;
+let response = await fetch(api);
