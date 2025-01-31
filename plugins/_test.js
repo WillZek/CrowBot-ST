49edf2b9f572da2a2fb5ca7ad5ae2aef73a18bb0
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-30 20:06:28 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -3,22 +3,21 @@
 import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-
-if (!text) return m.reply('🎩 Ingresa Un Texto Que Deseas Buscar En Google');
+  if (!text) return m.reply('🎩 Ingresa Un Texto Que Deseas Buscar En Google');
 
 try {
 let api = `https://delirius-apiofc.vercel.app/search/gimage?query=${text}`;
 let response = await fetch(api);
-let json = response.json();
+let json = await response.json();
 let data = json.data[0];
 
 let txt = `*Resultado De: ${text}`;
-let img = json.url;
+let img = data.url;
 
 conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
 
 } catch (e) {
-console.error(e)
+console.error(e);
 m.reply(`*Error:* ${e.message}`);
 m.react('✖️');
  }
@@ -27,5 +26,3 @@ m.react('✖️');
 handler.command = ['testgoogle', 'tgo'];
 
 export default handler;
-
-
