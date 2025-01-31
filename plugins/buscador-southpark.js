635d51655896fa756ac47dd67286fee0aac46015
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-30 20:57:28 -0600

    Actualizar buscador-southpark.js

--- plugins/buscador-southpark.js
+++ plugins/buscador-southpark.js
@@ -1,5 +1,26 @@
-// By WillZek 
+// By WillZek (probando)
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
 if (text) return m.reply('🍭 Ingresa Un Texto Para Buscar En South Park');
+
+try {
+let api = `https://delirius-apiofc.vercel.app/search/southpark?query=${text}`;
+
+let response = await fetch(api);
+let json = response.json();
+let result = json.data[0];
+
+let park = `*Resultado De Tu Búsqueda:* ${text}\n\n*Título:* ${data.title}\n*Duración:* ${data.duration}\n*Episodio:* ${data.episode}\n*Descripción:* ${data.description}\n*Link:* ${data.url}\> ${dev}`;
+
+conn.sendMessage(m.chat, { image: { url: null }, caption: park }, { quoted: fkontak });
+
+} catch (e) {
+m.reply(`*Error:* ${e.message}`);
+m.react('✖️');
+  }
+};
+
+handler.command = ['southparksearch', 'sopsearch'];
+
+export default handler;
