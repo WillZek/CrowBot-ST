5af19144fad2be55cb73225c82b6616571204735
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-03 13:58:18 -0600

    Actualizar descargas-pornhubdl.js

--- plugins/descargas-pornhubdl.js
+++ plugins/descargas-pornhubdl.js
@@ -6,11 +6,18 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 
 if (!text) return conn.reply(m.chat, '❀ Ingresa un link de Pornhub Para Descargar Su Video 🍭', m)
 try {
-let api = await fetch(`https://www.dark-yasiya-api.site/download/phub?url=${text}`)
-let json = await api.json()
+let api = `https://www.dark-yasiya-api.site/download/phub?url=${text}`
+let res = await fetch(api);
+let json = await res.json()
+let resu = json.result.format[0];
+
 let { video_title, video_uploader } = json.result
-let { download_url, resolution, } = json.result.format[1]
-await conn.sendMessage(m.chat, { video: { url: download_url }, caption: video_title }, { quoted: m })
+let { resolution, } = json.result.format[1]
+
+let url = resu.download_url;
+
+await conn.sendMessage(m.chat, { video: { url: url }, caption: video_title }, { quoted: m });
+
 } catch (error) {
 console.error(error)
 }}
