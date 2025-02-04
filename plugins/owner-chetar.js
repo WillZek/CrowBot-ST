85567f79bd915fd14ff79b445bb2f822932d8640
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-03 21:31:28 -0600

    Actualizar owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -10,6 +10,6 @@ const handler = async (m, { conn }) => {
 handler.help = ['cheat'];
 handler.tags = ['owner'];
 handler.command = /^(ilimitado|infiniy|chetar)$/i;
-handler.nods = true;
+handler.mods = true;
 handler.fail = null;
 export default handler;
