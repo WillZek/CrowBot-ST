354db3a93ed1fd151637d9b95201523d3a82ad3f
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-03 19:45:19 -0600

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,8 +1,8 @@
-import { igdl } from 'ruhend-scraper';
+ import { igdl } from 'ruhend-scraper';
 
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, 'Por favor, envia un link de Facebook valido.', m, rcanal);
+    return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🤍\`*', m, fake);
   }
 
   await m.react('🕒');
@@ -10,7 +10,7 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   try {
     res = await igdl(args[0]);
   } catch (error) {
-    return conn.reply(m.chat, 'Por favor, envia un link de Facebook valido.', m);
+    return conn.reply(m.chat, '*`Error al obtener datos. Verifica el enlace.`*', m);
   }
 
   let result = res.data;
@@ -22,32 +22,26 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   try {
     data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
   } catch (error) {
-    return conn.reply(m.chat, 'Error al procesar los datos.', m);
+    return conn.reply(m.chat, '*`Error al procesar los datos.`*', m);
   }
 
   if (!data) {
-    return conn.reply(m.chat, 'No se encontró una resolución adecuada.', m);
+    return conn.reply(m.chat, '*`No se encontró una resolución adecuada.`*', m);
   }
 
   await m.react('✅');
   let video = data.url;
 
-  // Pueden Cambiar Lo De Video Descargado Con Exito, Por otro mensaje, Solo Lo hice Par Definir El dev
-  const dev = 'Video descargado con éxito!';
-
   try {
-    await conn.sendMessage(m.chat, { video: { url: video }, caption: dev, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
+    await conn.sendMessage(m.chat, { video: { url: video }, caption: '*Descargado Con Exito ✓*', fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
   } catch (error) {
-    return conn.reply(m.chat, 'Hubo un error al intentar acceder al link.\n> Si el problema persiste, reportalo en el grupo de soporte.', m);
+    return conn.reply(m.chat, '*`Error al enviar el video.`*', m);
   await m.react('❌');
   }
 };
 
 handler.help = ['fb *<link>*'];
-handler.estrellas = 2
-handler.tags = ['descargas']
+handler.tags = ['dl']
 handler.command = /^(fb|facebook|fbdl)$/i;
-handler.estrellas = 8;
-handler.register = true
 
-export default handler;
+export default handler;                                                                                                                                                                                                                                          
