5bb7f95c92deadb71169d0314dd4920ead9fb5a9
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-05 14:35:32 -0600

    Actualizar descargar-mediafire.js

--- plugins/descargar-mediafire.js
+++ plugins/descargar-mediafire.js
@@ -2,31 +2,22 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, `🍭 Ingresa un link de mediafire`, m)
-    await m.react('🕓');
+  await m.react('🕓');
 
     try {
-        let api = await fetch(`https://dark-core-api.vercel.app/api/download/mediafire?key=user1&url=${text}`)
+        let api = await fetch(`https://restapi.apibotwa.biz.id/api/mediafire?url=${text}`)
         let json = await api.json()
-        if (!json.success) return m.reply('❌ Error al obtener los detalles del enlace, por favor intenta nuevamente.');
-
-        let { name, size, date, mime, link } = json.result;
-        let caption = `*「✐」${name}*
-
-> ❒ Tamaño » *${size}*
-> ✰ Fecha » *${date}*
-> 🜸 Tipo » *${mime}*`;
-
-        await conn.sendFile(m.chat, link, name, caption, m, null, { mimetype: mime, asDocument: true });
-
+        let { filename, type, size, uploaded, ext, mimetype, download: dl_url } = json.data.response
+        m.reply(`${waitttt}`)
         await m.react('✅');
+        await conn.sendFile(m.chat, dl_url, filename, null, m, null, { mimetype: ext, asDocument: true })
     } catch (error) {
         console.error(error)
-        m.reply('❌ Ocurrió un error al procesar la solicitud.')
     }
 }
 
 handler.help = ['mediafire *<url>*']
 handler.tags = ['descargas']
-handler.command = ['mediafire', 'mfdl', 'mf']
+handler.command = ['mediafire', 'mf']
 
 export default handler;
