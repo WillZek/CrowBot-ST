a6338105c9141f605e06c839ff1c2e991af9c807
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-05 14:49:17 -0600

    Actualizar descargar-mediafire.js

--- plugins/descargar-mediafire.js
+++ plugins/descargar-mediafire.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text }) => {
     try {
         let api = await fetch(`https://delirius-apiofc.vercel.app/download/mediafire?url=${text}`)
         let json = await api.json()
-        let { filename, type, size, uploaded, extension, mimetype, download: dl_url } = json.data
+        let { filename, type, size, uploaded, extension, mimetype, download: dl_url } = json.data[0]
         m.reply(`${wait}`)
         await conn.sendFile(m.chat, dl_url, filename, null, m, null, { mimetype: extension, asDocument: true });
 await m.react('✅');
