dd04a378cf0db90576cbbd0bfbd138982ffc1851
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-04 22:19:20 -0600

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -29,52 +29,49 @@ handler.register = true;
 export default handler;
 */
 
+/* 
+- Downloader Ytmp4 By DarkCore
+- https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
+- Parchado por DarkCore... vip plus
+*/
+
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let handler = async (m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, '🍟 Ingresa un link de YouTube', m);
 
     try {
         await m.react('🕒');
 
-        const apiKey = 'xenzpedo';
-        const apiUrl = `https://api.botcahx.eu.org/api/dowloader/yt?url=${encodeURIComponent(text)}&apikey=${apiKey}`;
+        const apiKey = '777izumi';
+        const apiUrl = `https://dark-core-api.vercel.app/api/download/ytmp4?url=${encodeURIComponent(text)}&type=video&quality=hdHigh&key=${apiKey}`;
         const response = await fetch(apiUrl);
         const result = await response.json();
 
-        if (!result.status || !result.result) {
+        if (!result.success || !result.downloadLink) {
             throw new Error('Error al obtener datos de la API.');
         }
 
-        const { title, duration, mp3, mp4 } = result.result;
+        const videoUrl = result.downloadLink;
+        const fileName = 'video.mp4';
 
-        const durationInSeconds = parseInt(duration);
+        let CB = `🍃 *Video descargado correctamente*`;
 
-        let HS = `🍃 *Título :* ${title}\n🍃 *Duración :* ${(durationInSeconds / 60).toFixed(2)} minutos`;
-
-        if (durationInSeconds >= 2400) { 
-            await conn.sendMessage(m.chat, { 
-                document: { url: mp4 }, 
-                mimetype: 'video/mp4', 
-                fileName: `${title}.mp4`, 
-                caption: HS 
-            }, { quoted: m });
-        } else {
-            await conn.sendMessage(m.chat, { 
-                video: { url: mp4 }, 
-                caption: HS 
-            }, { quoted: m });
-        }
+        await conn.sendMessage(m.chat, { 
+            video: { url: videoUrl }, 
+            caption: CB 
+        }, { quoted: m });
 
         await m.react('✅');
     } catch (error) {
         console.error(error);
         await m.react('✖'); 
+        m.reply(`❌ *Error:* ${error.message || 'Ocurrió un error desconocido'}`);
     }
 };
 
 handler.help = ['ytmp4 *<url>*']; 
-handler.command = ['ytmp4', 'ytv'];
+handler.command = ['ytmp4'];
 handler.tags = ['descargas'];
 
 export default handler;
