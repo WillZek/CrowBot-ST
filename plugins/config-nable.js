d470bd4c574558e9d5705b1c1ef6f2a0e790cd8a
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-07 19:10:43 -0600

    Actualizar config-nable.js

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isMods, isAd
     case 'antiprivado':
     case 'antipriv':
      isAll = true
-        if (!isMods) {
+        if (!isOwner) {
           global.dfail('rowner', m, conn)
           throw false
       }
