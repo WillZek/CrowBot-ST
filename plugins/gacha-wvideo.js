84a9fbec950053d2a62a96cf0b5dbcdd0adbf040
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-09 14:29:14 -0600

    Actualizar gacha-wvideo.js

--- plugins/gacha-wvideo.js
+++ plugins/gacha-wvideo.js
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
-const charactersFilePath = './src/database/characters.json';
+const charactersFilePath = './media/database/characters.json';
 const haremFilePath = './src/database/harem.json';
 
 async function loadCharacters() {
