f5043a5ea4a848164337c3ef9eff58340c66b87a
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-09 14:31:22 -0600

    Actualizar gacha-robarrw.js

--- plugins/gacha-robarrw.js
+++ plugins/gacha-robarrw.js
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
-const charactersFilePath = './src/database/characters.json';
+const charactersFilePath = './media/database/characters.json';
 
 async function loadCharacters() {
     try {
