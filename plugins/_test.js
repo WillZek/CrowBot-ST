391c2d5d0e9b5ed8803e42bfeb538afc6828f6a1
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-10 18:53:13 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -12,8 +12,8 @@ let img = 'https://files.catbox.moe/l6jzeo.jpeg';
 
 conn.sendMessage(m.chat, { image: { url: img }, caption: ff }, { quoted: fkontak });
 m.react('🔫');
+}
 
 handler.command = ['menuff'];
 
 export default handler
-
