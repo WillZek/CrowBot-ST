b5639d807f04855bbd5a481176e33ca0c338b35c
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-11 18:45:56 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -2,18 +2,16 @@ import { sticker } from '../lib/sticker.js';
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
-if (!text) m.reply('Por favor, proporciona un texto para el sticker.');
+if (!text) return m.reply('Por favor, proporciona un texto para el sticker.');
 
 try {
 const response = await fetch(`https://brat.caliphdev.com/api/brat?text=${encodeURIComponent(text)}`);
-if (!response.ok) m.reply('Error en la respuesta de la API.');
+if (!response.ok) return m.reply('Error en la respuesta de la API.');
 
 const buffer = await response.buffer();
 let stiker = await sticker(false, buffer, global.packname, global.author);
 
-// if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
-
-conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+conn.sendFile(m.chat, stiker, null, { asSticker: true }, m);
 
 } catch (error) {
 m.reply(`Error: ${error}`);
