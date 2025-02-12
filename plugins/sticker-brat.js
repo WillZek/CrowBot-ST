dcea2ff552f29038887128479044ea250ad5b8e2
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-11 18:32:22 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -10,13 +10,14 @@ if (!response.ok) m.reply('Error en la respuesta de la API.');
 
 const buffer = await response.buffer();
 let stiker = await sticker(false, buffer, global.packname, global.author);
-} catch (error) {
-m.reply(`Error: ${error}`);
-}
 
 // if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
 
 conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+
+} catch (error) {
+m.reply(`Error: ${error}`);
+  }
 }
 
 handler.help = ['brat <texto>'];
