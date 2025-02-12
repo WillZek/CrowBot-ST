abf22cd162b76bcfbba74399b599160352effa39
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-11 18:58:04 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -4,15 +4,20 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, text }) => {
 if (!text) return m.reply('Por favor, proporciona un texto para el sticker.');
 
-try {
-const response = await fetch(`https://brat.caliphdev.com/api/brat?text=${encodeURIComponent(text)}`);
+/* try {
+ const response = await fetch(`https://brat.caliphdev.com/api/brat?text=${encodeURIComponent(text)}`);
 if (!response.ok) return m.reply('Error en la respuesta de la API.');
 
 const buffer = await response.buffer();
 let stiker = await sticker(null, buffer, global.packname, global.author);
 
-// conn.sendFile(m.chat, stiker, null, { asSticker: true }, m);
+ conn.sendFile(m.chat, stiker, null, { asSticker: true }, m);
 if (stiker) return conn.sendFile(m.chat, stiker, 'pene.webp', '', m);
+/*
+
+try {
+let pene = `https://api.fgmods.xyz/api/maker/carbon?text=${text}&apikey=${elrebelde21}`
+await conn.sendFile(m.chat, pene, 'error.jpg', null, m, null, fake)
 
 } catch (error) {
 m.reply(`Error: ${error}`);
