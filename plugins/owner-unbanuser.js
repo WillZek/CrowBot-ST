f5f169725594ca535e88878671e0ef86104046aa
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-15 08:42:36 -0600

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -23,6 +23,6 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
 handler.help = ['unbanuser <@tag>'];
 handler.command = ['unbanuser'];
 handler.tags = ['owner'];
-handler.prems = true;
+handler.owner = true;
 handler.group = true;
 export default handler;
