6e997de268efb6d97295dd4fbf0bf66ec0df0585
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-15 19:06:25 -0600

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -5,7 +5,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isRowner, i
     if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
     const chat = global.db.data.chats[m.chat];
     const bot = global.db.data.settings[this.user.jid] || {};
-    if (bot.antiPrivate && !isOwner && !isROwner && isMods) {
+    if (bot.antiPrivate && !isOwner && !isROwner && !isMods) {
       await m.reply(`> 🚫 @${m.sender.split`@`[0]} Está Prohibido Escribir Al Privado Del Bot 🍡\n> Si Deseas Probar Nuestro Bot Sigue El Canal Oficial✨ : ${channel}`, false, {mentions: [m.sender]});
       await this.updateBlockStatus(m.chat, 'block');
     }
