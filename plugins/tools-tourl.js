0628ac1519f2921ec72906309c8de9ebc0a6af13
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-17 09:02:28 -0600

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,23 +1,36 @@
-import uploadFile from '../lib/uploadFile.js';
-import uploadImage from '../lib/uploadImage.js';
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
-const handler = async (m) => {
-const q = m.quoted ? m.quoted : m;
-const mime = (q.msg || q).mimetype || '';
-if (!mime) return m.reply('🍭 Responde A Una Imagen o Video✨');
-const media = await q.download();
-try {
-const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
-const link = await (isTele ? uploadImage : uploadFile)(media);
-m.reply(link);
-} catch (e) {
-console.log(e) 
+
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) return conn.reply(m.chat, `🍭 Responde A Una Imagen o Video✨`, m)
+  await m.react(rwait)
+  try {
+  let media = await q.download()
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+  let link = await (isTele ? uploadImage : uploadFile)(media)
+  let img = await (await fetch(`${link}`)).buffer()
+  let txt = `乂  *L I N K - E N L A C E*  乂\n\n`
+      txt += `*» Enlace* : ${link}\n`
+      txt += `*» Acortado* : ${await shortUrl(link)}\n`
+      txt += `*» Tamaño* : ${formatBytes(media.length)}\n`
+      txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
+      txt += `> *${dev}*`
+
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak)
+await m.react(done)
+} catch {
+await m.react(error)
 }}
-handler.help = ['tourl <reply image>'];
+handler.help = ['tourl']
 handler.tags = ['tools']
-handler.command = /^(tourl)$/i;
 handler.register = true
-export default handler;
+handler.command = ['tourl']
+handler.estrellas = 3;
+
+export default handler
 
 function formatBytes(bytes) {
   if (bytes === 0) {
