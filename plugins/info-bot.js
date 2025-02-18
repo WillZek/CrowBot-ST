0c2b429c8546ecd41e1c2e0a087a9e45a1259ae5
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-18 08:58:43 -0600

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -45,7 +45,7 @@ conn.reply(m.chat, `*Tu Mamá we 🍆🥵*`, m, rcanal, )
 
 if (/^Crow$/i.test(m.text)) {
 conn.reply(m.chat, `*Hola Eres Fan De* *CrowBot o Brawl Stars*
-*Entonces Sigue El Canal Oficial!*\n> https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W\n\n*O Puedes Ir Al Sitio Web Oficial De CrowBot!*\n> https://crow-bot-dashboard.vercel.app/\n\n*Gracias por utilizar CrowBot-ST* `, m, rcanal, )
+*Entonces Sigue El Canal Oficial!*\n> https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W\n\n*O Puedes Ir Al Sitio Web Oficial De CrowBot!*\n> https://crowbot-web.vercel.app/\n\n*Gracias por utilizar CrowBot-ST* `, m, rcanal, )
 }
 
 if (/^reglasgp|.reglasgp$/i.test(m.text)) {
