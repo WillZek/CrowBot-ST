3dc508112c5796d04cff03dee038595f2b3b420d
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-22 15:58:13 -0400

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -2,40 +2,35 @@
 
 let muted = {};
 
-let handler = async (m, { conn, isAdmin, command, text, groupMetadata, participants }) => {
-  if (!m.isGroup) return m.reply('Este Comando Solo Puede Ser Usado En Grupos');
-  if (!text) return m.reply('Etiqueta A Una Persona ✨');
-
-  const user = m.sender;
+const handler = async (m, { conn, isAdmin, command, text, groupMetadata, participants }) => {
+if (!m.isGroup) return m.reply('Este Comando Solo Puede Ser Usado En Grupos');
 
 if (command === 'test') {
     const target = text.substring(5).trim();
-    const targetUser  = participants.find(p => p.id.startsWith('@') ? p.id.replace('@s.whatsapp.net', '') === target : p.id === target);
-
-if (targetUser ) {
-      muted[targetUser .id] = true;
-      conn.reply(m.chat, `Usuario ${targetUser .id.replace('@s.whatsapp.net', '')} silenciado.`, m);
-
+    const targetUser = participants.find(p => p.id.startsWith('@') ? p.id.replace('@s.whatsapp.net', '') === target : p.id === target);
+if (targetUser) {
+      muted[targetUser.id] = true;
+      conn.reply(m.chat, `Usuario ${targetUser.id.replace('@s.whatsapp.net', '')} silenciado.`, m);
 } else {
 conn.reply(m.chat, `Usuario no encontrado.`, m);
     }
 } else if (command === 'test2') {
-const target = text.substring(7).trim();
-const targetUser  = participants.find(p => p.id.startsWith('@') ? p.id.replace('@s.whatsapp.net', '') === target : p.id === target);
-
-if (targetUser ) {
-delete muted[targetUser .id];
-conn.reply(m.chat, `Usuario ${targetUser .id.replace('@s.whatsapp.net', '')} desmuteado.`, m);
+    const target = text.substring(7).trim();
+    const targetUser = participants.find(p => p.id.startsWith('@') ? p.id.replace('@s.whatsapp.net', '') === target : p.id === target);
+if (targetUser) {
+      delete muted[targetUser.id];
+conn.reply(m.chat, `Usuario ${targetUser.id.replace('@s.whatsapp.net', '')} desmuteado.`, m);
 } else {
 conn.reply(m.chat, `Usuario no encontrado.`, m);
     }
-} else {
-conn.reply(m.chat, 'Uso: .mute <@tag> (etiqueta al user en otras palabras)', m);
-  }
-
+} else if (m.isGroup){
 if (muted[m.sender]) {
-    await conn.sendMessage(m.chat, { delete: m.key });
-    return;
+try {
+await conn.sendMessage(m.chat, { delete: m.key });
+} catch (error) {
+m.reply(`*Error:* ${error.message}`);
+        }
+    }
   }
 };
 
