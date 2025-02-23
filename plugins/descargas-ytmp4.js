e8996d53b226504287d627170853679b033f9306
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-22 20:52:25 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -32,7 +32,7 @@ if (!link) return m.reply('No se pudo obtener el video.');
 await conn.sendMessage(m.chat, {
       video: { url: link },
       mimetype: "video/mp4",
-      caption: `${resp}`,
+      caption: `Toma tu mrd`,
     }, { quoted: m });
     m.react(done);
 }
