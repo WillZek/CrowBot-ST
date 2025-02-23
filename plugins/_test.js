71111ed74c971fe98387dd38b7385046f6b669f2
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-23 10:04:41 -0400

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,52 +1,40 @@
-import yts from 'yt-search';
-import axios from "axios";
 
-const handler = async (m, { conn, text }) => {
-  try {
-    if (!text) return conn.reply('🍭 Ingrese el nombre De Alguna Música');
+import yts from 'yt-search'
 
-    const search = await yts(text);
-    if (!search || !search.all || search.all.length === 0) {
-      return m.reply('No se encontraron resultados para tu búsqueda.');
-    }
+var handler = async (m, { text, conn, args, command, usedPrefix }) => {
 
-    const videoInfo = search.all[0];
-    const { title, thumbnail, timestamp, views, ago, url } = videoInfo;
-    const vistas = formatViews(views);
-    const infoMessage = `「 *𝐂𝐫𝐨𝐰𝐁𝐨𝐭 - 𝐒𝐓* 」\n> ♡ *Título:* ${title}\n> ♡ *Duración:* ${timestamp}\n> ♡ *Vistas:* ${vistas}\n> ♡ *Canal:* ${videoInfo.author.name || 'Desconocido'}\n> ♡ *Publicado:* ${ago}\n> ♡ *Enlace:* ${url}\n> Powered By Crow's Club`;
+if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingresa una busqueda de Youtube.`, m)
 
-    const thumbData = await conn.getFile(thumbnail);
-    if (!thumbData || !thumbData.data) {
-      return m.reply('Error al obtener la miniatura.');
-    }
-    const thumb = thumbData.data;
+conn.reply(m.chat, wait, m)
+
+let results = await yts(text)
+let tes = results.all
+let teks = results.all.map(v => {
+switch (v.type) {
+case 'video': return `Resultados De : *<${text}>*
+
+> ✦ Título » *${v.title}*
+> ✧ Canal » *${v.author.name}*
+> ✦ Duración » *${v.timestamp}*
+> ✧ Subido » *${v.ago}*
+> ✦ Vistas » *${v.views}*
+> ✧ Enlace » ${v.url}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
 
 conn.sendMessage(m.chat, { 
-        image: { url: thumb }, 
-        caption: infoMessage, 
+        image: { url: tes[0].thumbnail }, 
+        caption: teks, 
         footer: dev, 
         buttons: [
             {
-                buttonId: `.ytmp4`,
+                buttonId: `.ytmp4 ${v.url}`,
                 buttonText: { displayText: 'Obtener Vídeo' }
             }
         ],
         viewOnce: true,
         headerType: 4
     }, { quoted: m });
-  
-} catch (e) {
-m.reply(`Error: ${e.message}`);
-  }
-};
+}
 
 handler.command = ['test'];
-export default handler;
 
-function formatViews(views) {
-  if (views >= 1000) {
-    return (views / 1000).toFixed(1) + 'k (' + views.toLocaleString() + ')';
-  } else {
-    return views.toString();
-  }
-}
+export default handler
