b3cf4ca17c47017f95ee878e60184d9afff2b42a
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-23 00:12:46 -0400

    Actualizar emox-videoxxx.js

--- plugins/emox-videoxxx.js
+++ plugins/emox-videoxxx.js
@@ -1,7 +1,9 @@
 // Para los pajeros xd
 let handler = async(m, { conn }) => {
 
-conn.sendMessage(m.chat, { video: { url: global.vidxxx }, caption: '🍭 Disfruta!' }, { quoted: m });
+let rvid = global.vidxxx[Math.floor(Math.random() * global.vidxxx.length)];
+
+conn.sendMessage(m.chat, { video: { url: rvid }, caption: '🍭 Disfruta!' }, { quoted: m });
 
 
 handler.command = ['videoxxx', 'vxxx'];
@@ -31,4 +33,4 @@ global.vidxxx = [
   'https://telegra.ph/file/411d8f59a5cefc2a1d227.mp4',
   'https://telegra.ph/file/76ba0dc2a07f491756377.mp4',
   'https://telegra.ph/file/831bb88f562bef3f1a15d.mp4',
-].getRandom();
+];
