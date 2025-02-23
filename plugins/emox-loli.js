2d635a1851a97e67e6b2eb8214329c41c4f8fb3e
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-23 16:21:57 -0400

    Actualizar emox-loli.js

--- plugins/emox-loli.js
+++ plugins/emox-loli.js
@@ -9,6 +9,10 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
 m.react('🕑');
 
+const gp = global.db.data.chats[m.chat] || {};
+
+if (!gp.nsfw && m.isGroup) return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
+
 let txt = 'Pack🔥🔥🔥';
 
 let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
