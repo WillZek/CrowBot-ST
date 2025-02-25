78a4079a2e096d943569a4845188c51acc058449
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-25 15:40:33 -0400

    Actualizar fun-memes.js

--- plugins/fun-memes.js
+++ plugins/fun-memes.js
@@ -2,7 +2,22 @@
 import hispamemes from 'hispamemes'
 let handler = async (m, { conn, usedPrefix, command }) => {
 const meme = hispamemes.meme()
-conn.sendFile(m.chat, meme, '', '', fkontak)
+// conn.sendFile(m.chat, meme, '', '', fkontak)
+
+conn.sendMessage(m.chat, { 
+        image: { url: meme }, 
+        caption: '¡Aqui Está Tu Meme!🤣', 
+        footer: dev, 
+        buttons: [
+            {
+                buttonId: `.meme`,
+                buttonText: { displayText: 'Siguiente Meme' }
+            }
+        ],
+        viewOnce: true,
+        headerType: 4
+    }, { quoted: m });
+
 m.react(emoji2)
 }
 handler.help = ['meme']
