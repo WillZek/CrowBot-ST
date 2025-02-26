6609fa4df0f7882ab80a568b692935357ce855dd
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-26 13:39:51 -0400

    Actualizar rpg-cofre.js

--- plugins/rpg-cofre.js
+++ plugins/rpg-cofre.js
@@ -8,7 +8,7 @@ const handler = async (m, {isPrems, conn}) => {
   const ai = Math.floor(Math.random() * 4000);
   const expp = Math.floor(Math.random() * 5000);
 
-  global.db.data.users[m.sender].cookies += dia;
+  global.db.data.users[m.sender].estrellas += dia;
   global.db.data.users[m.sender].money += ai;
   global.db.data.users[m.sender].joincount += tok;
   global.db.data.users[m.sender].exp += expp;
@@ -20,8 +20,7 @@ const handler = async (m, {isPrems, conn}) => {
 ╰━━━━━━━━━━━━⬣
 
 ╭━〔 ${global.namebot} 〕⬣
-┃ *${dia} Cookies* 🍪
-┃ *${tok} Tokens* 💰
+┃ *${dia} Estrellas* 🌟
 ┃ *${ai} CrowCoins* 🪙
 ┃ *${expp} Exp* ⚡
 ╰━━━━━━━━━━━━⬣`;
