f70953303fdf7e1441425f5da5a49c820fe8dc68
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-25 20:47:46 -0400

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -1,17 +1,19 @@
-
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
 if (!text) return m.reply(`🌸 Ingresa el enlace del Grupo.`)
+try {
 let [_, code] = text.match(linkRegex) || []
 if (!code) return m.reply('🌺 Enlace invalido.')
 let res = await conn.groupAcceptInvite(code)
 m.reply(`🚩 Me uní correctamente al Grupo`)
-}
+} catch (e) {
+return m.reply(`✘ Ocurrió un error. ${e.message}`)}}
+
 handler.help = ['join <link>']
 handler.tags = ['owner']
 handler.command = ['join', 'entrar'] 
-handler.prems = true
+handler.rowner = true
 
 export default handler
