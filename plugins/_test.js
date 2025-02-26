f7c985d3cbc294ff65ee3d6fda3918708c602322
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-26 17:50:29 -0400

    Actualizar _test.js

---
+++ plugins/_test.js
@@ -0,0 +1,20 @@
+// By WillZek 
+import fetch from 'node-fetch';
+
+let handler = async(m, { conn, command }) => {
+
+if (command == 'rempalago' || command == 'nomeloesperaba' || command == 'bobesponja') {
+
+let res = await (await fetch(`https://dark-core-api.vercel.app/api/search/meme?key=api&q=${command}`).json;
+
+let aud = res.resultados[0];
+
+conn.sendMessage(m.chat, { audio: aud.url }, { quoted: m });
+    }
+};
+
+handler.command = ['rempalago', 'bobesponja', 'nomeloesperaba'];
+
+export default handler;
+
+
