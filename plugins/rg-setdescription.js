5faafa1f8cb5fab15fc8fda8abb2a88dee7ae5e6
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-26 20:41:32 -0400

    🍭 𝑪𝒓𝒐𝒘𝑩𝒐𝒕-𝑺𝑻

--- plugins/rg-setdescription.js
+++ plugins/rg-setdescription.js
@@ -6,17 +6,17 @@ const handler = async (m, { conn, command, usedPrefix, text }) => {
 let user = global.db.data.users[m.sender];
 
 if (user.description) {
-return conn.reply(m.chat, `⛄ Ya tienes una descripción establecida, si quieres borrar la descripcion actual usa:\n> » ${usedPrefix}deldescription`, m);
+return conn.reply(m.chat, `❀ Ya tienes una descripción establecida, si quieres borrar la descripcion actual usa:\n> » ${usedPrefix}deldescription`, m);
 }
 
-if (!text) return conn.reply(m.chat, `🎅  Especifica tu descripcion valida para tu perfil.\n\n> ✐ Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`, m);
+if (!text) return conn.reply(m.chat, `❀ Especifica tu descripcion valida para tu perfil.\n\n> ✐ Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`, m);
 
 user.description = text;
 
 return conn.reply(m.chat, `✐ Se ha establecido tu descripcion.\n\n> *${user.description}*`, m);
 };
 
-handler.help = ['setdescription']
+handler.help = ['setdescription <establece tu descripción>']
 handler.tags = ['rg']
 handler.command = ['setdescription', 'setdesc']
 handler.estrellas = 3;
