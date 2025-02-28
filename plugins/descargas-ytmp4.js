cbcce2b2f4a044a4c76e6c13443b181cb1b806d6
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-27 22:50:27 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -92,7 +92,7 @@ try {
       video = await (await fetch(`https://api.fgmods.xyz/api/downloader/ytmp4?url=${text}&quality=480p&apikey=be9NqGwC`)).json();
 } catch (error) {
 try {
-      video = await (await fetch(`https://api.alyachan.dev/api/ytv?url=${text}&apikey=DitzOfc`)).json();
+      video = await (await fetch(`https://api.alyachan.dev/api/ytv?url=${text}&apikey=zstapi09`)).json();
 } catch (error) {
       video = await (await fetch(`https://good-camel-seemingly.ngrok-free.app/download/mp4?url=${text}`)).json();
       }
