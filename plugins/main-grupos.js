7b9ddb638a8bee5e2ac0709c7d92ad2a5812af5c
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-01 10:07:06 -0400

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -5,7 +5,8 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let grupos = `╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈≫\n\n☕︎︎ *Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad oficial* 💛
 
 1- 『✯ ⏤͟͞ू⃪𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗖𝗿𝗼𝘄𝗕𝗼𝘁⁞࿐ ✯』
-*❑* ${grupo}
+*❑ Grupo1* ${grupo}
+*❑ Grupo2* ${gteam}
 
 ✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
 
