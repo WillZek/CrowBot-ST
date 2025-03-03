adcd1a0aedbd7a3cb0b0b8175212eee5f63cdaf4
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-02 23:51:03 -0400

    Actualizar sticker-ttp_attp.js

--- plugins/sticker-ttp_attp.js
+++ plugins/sticker-ttp_attp.js
@@ -10,15 +10,11 @@ let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/attp?text=${te
 conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: channel, thumbnail: imagen1 }}}, { quoted: m })}
 
     if (command == 'ttp') {
-        const data = {
-            text: `${text}`,"outlineColor":"255,0,0,255", "textColor":"0,0,0,255"
-        };
-        const response = await axios.post('https://salism3api.pythonanywhere.com/text2img', data);
-        const x=response.data.image;
-        let stiker = await sticker(null,x,global.packname, global.author)
-        conn.sendFile(m.chat, stiker, null, { asSticker: true })
-    }
+let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/attp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: imagen1 }}}, { quoted: m })
 }
+}
+
 handler.command = handler.help = ['ttp', 'attp']
 handler.tags = ['sticker']
 export default handler
