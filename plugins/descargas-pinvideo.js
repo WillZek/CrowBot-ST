a6300ea8b08a4bf76f1368305071bd090d166762
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-02 21:50:35 -0400

    Actualizar descargas-pinvideo.js

--- plugins/descargas-pinvideo.js
+++ plugins/descargas-pinvideo.js
@@ -2,11 +2,14 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw m.reply(`🍭 Por favor, ingresa el link de un video/imagen de Pinterest.`);
-conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
+m.react('🕒');
         let ouh = await fetch(`https://api.agatz.xyz/api/pinterest?url=${text}`)
   let gyh = await ouh.json()
-        await conn.sendFile(m.chat, gyh.data.result, `pinvideobykeni.mp4`, `*${emoji} Url:* ${gyh.data.url}`, m)
-        await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
+
+await conn.sendMessage(m.chat, { video: { url: gyh.data.result }, caption: '✅ Descargado Con Exito.' }, { quoted: m });
+
+// await conn.sendFile(m.chat, gyh.data.result, `pinvideobykeni.mp4`, `*${emoji} Url:* ${gyh.data.url}`, m)
+await m.react('✅');
 }
 handler.help = ['pinvid *<link>*']
 handler.tags = ['descargas']
