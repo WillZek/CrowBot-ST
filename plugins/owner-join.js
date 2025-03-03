27de0094a81746646de6a45cef8fdbf89867bee0
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-03 15:07:33 -0400

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -1,4 +1,4 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+/* let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
@@ -17,3 +17,31 @@ handler.command = ['join', 'entrar']
 handler.rowner = true
 
 export default handler
+*/
+
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+
+let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
+
+        if (!text) return m.reply(`🚩 Ingresa el enlace del Grupo.`)
+    let [_, code, expired] = text.match(linkRegex) || []
+    if (!code) return m.reply('🚩 Enlace invalido.')
+    let res = await conn.groupAcceptInvite(code)
+    expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
+    m.reply(`🚩 Me uní correctamente al Grupo *${res}${expired ? `* Durante *${expired}* días.` : ''}`)
+    let chats = global.db.data.chats[res]
+    if (!chats) chats = global.db.data.chats[res] = {}
+    if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
+  const pp = './media/menus/Menu3.jpg';
+  conn.sendFile(m.chat, pp, 'error.jpg', text, m, false);
+}
+
+handler.help = ['join *<link> <días>*']
+handler.tags = ['owner']
+
+handler.command = ['join', 'entrar'] 
+handler.owner = true
+
+export default handler
+
+const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
