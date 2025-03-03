3e7deaaccce5006af39d50f368cebf92343a75b1
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-02 20:23:17 -0400

    Actualizar descargas-whatmusic.js

--- plugins/descargas-whatmusic.js
+++ plugins/descargas-whatmusic.js
@@ -1,4 +1,46 @@
-import acrcloud from 'acrcloud'
+import fetch from 'node-fetch'
+import FormData from 'form-data'
+import fs from 'fs'
+
+let handler = async (m) => {
+let q = m.quoted ? m.quoted : m
+let mime = q.mediaType || ''
+if (/audio|video/.test(mime)) {
+let media = await q.download(true)
+let upload = await uploadFile(media)
+let shp = await fetch(`https://apis-starlights-team.koyeb.app/starlight/chazam?url=${upload.files[0].url}`, { headers: { 'Content-Type': 'application/json' }})
+let json = await shp.json()
+let app = { title: json.title, artist: json.artist, type: json.type, url: json.url }
+let txt = `*\`-• C H A Z A M - M U S I C •-\`*\n\n` +
+`🍟 *Nombre:* ${app.title}\n` +
+`🍟 *Artista:* ${app.artist}\n` +
+`🍟 *Tipo:* ${app.type}\n` +
+`🍟 *Link:* ${app.url}`
+m.reply(txt)
+} else {
+return conn.reply(m.chat, '🚩 Responde a un *Audio/Video.*', m, rcanal)
+}}
+handler.help = ['shazam *<Audio/Video>*']
+handler.tags = ['tools']
+handler.command = /^(shazam | whatmusic)$/i
+handler.register = true
+
+export default handler
+
+async function uploadFile(path) {
+let form = new FormData()
+form.append('files[]', fs.createReadStream(path))
+let res = await (await fetch('https://uguu.se/upload.php', { method: 'post',
+headers: {
+...form.getHeaders()
+},
+body: form
+})).json()
+await fs.promises.unlink(path)
+return res
+}
+
+/* import acrcloud from 'acrcloud'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 import yts from 'yt-search'
 
@@ -39,3 +81,4 @@ handler.command = ['whatmusic', 'shazam']
 //handler.limit = 1
 handler.register = true 
 export default handler
+*/
