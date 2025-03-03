292195d1632fa6d2fef9859c3dc43c64117d93b5
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-03 18:57:43 -0400

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -18,7 +18,7 @@ if (stiker) return conn.sendFile(m.chat, stiker, 'pene.webp', '', m);
 try {
 let pene = `https://api.fgmods.xyz/api/maker/carbon?text=${text}&apikey=elrebelde21`
 // await conn.sendMessage(m.chat, { sticker: pene }, { quoted: m });
-await conn.sendFile(m.chat, pene, 'sticker.webp', '', m, null);
+await conn.sendFile(m.chat, pene, 'error.jpg', null, m, null, fake)
 
 } catch (error) {
 m.reply(`Error: ${error}`);
