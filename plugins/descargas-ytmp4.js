f4e1921c2a8c76f620d99af890a2309e19cda35e
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-03 22:57:49 -0400

    xDDDD

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -30,7 +30,7 @@ let link = video?.data?.url || video?.download_url || video?.result?.dl_url || v
 if (!link) return m.reply('《✧》Hubo un error al intentar acceder al link.\n> Si el problema persiste, reportalo en el grupo de soporte.');
 
 let limit = 10 * 1024 * 1024; // 10MB
-if (!video.data.size < limit) {
+if (!video.data.size > limit) {
 await conn.sendMessage(m.chat, {
       document: { url: link },
       mimeType: "video/mp4",
