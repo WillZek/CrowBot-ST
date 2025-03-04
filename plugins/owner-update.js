97cb308724e8e33e7060e0fab892ef7866be1fbb
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-04 18:37:08 -0400

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -3,7 +3,8 @@ let handler = async (m, { conn, text }) => {
 await m.react('🕓')
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-await conn.reply(m.chat, stdout.toString(), m)
+// await conn.reply(m.chat, stdout.toString(), m)
+await conn.reply(m.chat, `《★》𝘼𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙙𝙤 𝘾𝙤𝙣 𝙀𝙭𝙞𝙩𝙤 ✔ \n${stdout}`, m)
 await m.react('✅')
 }}
 handler.help = ['update']
