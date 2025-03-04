f03a3ef181322760d46655e96888b94997f546df
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-04 17:57:55 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -505,16 +505,16 @@ console.error(e)
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '「💛」 *Esta función solo puede ser usada por mi creador*', 
-owner: '「💛」 *Esta función solo puede ser usada por mi desarrollador.*', 
-mods: '「💛」 *Esta función solo puede ser usada por los moderadores del bot*', 
-premium: '「💛」 *Esta función solo es para usuarios Premium.*', 
-group: '「💛」 *Esta funcion solo puede ser ejecutada en grupos.*', 
-private: '「💛」 *Esta función solo puede ser usada en chat privado.*', 
-admin: '「💛」 *Este comando solo puede ser usado por admins.*', 
-botAdmin: '「💛」 *Para usar esta función debo ser admin.*',
-unreg: `「💛」 *No te encuentras registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*Ejemplo* : */reg Crow.18*`,
-restrict: '「💛」 *Esta característica esta desactivada.*'
+rowner: '《★》Esta función solo puede ser usada por mi creador.', 
+owner: '《★》Esta función solo puede ser usada por mi desarrollador.', 
+mods: '《★》Esta función solo puede ser usada por los moderadores del bot', 
+premium: '《★》Esta función solo es para usuarios Premium.', 
+group: '《★》Esta funcion solo puede ser ejecutada en grupos.', 
+private: '《★》Esta función solo puede ser usada en chat privado.', 
+admin: '《★》Este comando solo puede ser usado por admins.', 
+botAdmin: '《★》Para usar esta función debo ser admin.',
+unreg: `《★》No te encuentras registrado, registrese para usar esta función\n\n*/reg nombre.edad*\n\n*Ejemplo* : */reg Crow.18*`,
+restrict: '《★》Esta característica esta desactivada.'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
 
