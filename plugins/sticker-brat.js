c746845ed3cea263cf1b4a4b418095fde1ba2d55
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-04 23:00:35 -0400

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 import axios from 'axios';
 
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-    if (!text) return m.reply(`🍭 Ingresa Un Texto Para Realizar Tu Sticker\n> *Ejemplo:* ${usedPrefix + command} CrowBot`);
+    if (!text) return m.reply(`《★》Ingresa Un Texto Para Realizar Tu Sticker\n> *Ejemplo:* ${usedPrefix + command} CrowBot`);
 
 let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/carbon?text=${text}&apikey=elrebelde21`,global.packname, global.author)
 conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: imagen1 }}}, { quoted: m })
