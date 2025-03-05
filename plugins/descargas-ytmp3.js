3477ebfbc467d5769e7a1ef4f45a0f45db7e19c5
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-04 23:52:44 -0400

    🔥🔥

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -6,12 +6,14 @@ if (!args[0]) return m.reply('🍭 Ingresa Un Link De Youtube');
 
 let api = await(await fetch(`https://dark-core-api.vercel.app/api/download/YTMP3?key=api&url=${args[0]}`)).json();
 
-let txt = `Titulo: ${api.title}`;
+let txt = `「✦」𝗧𝗶𝘁𝘂𝗹𝗼: ${api.title}`;
 conn.reply(m.chat, txt, m, rcanal);
 
 conn.sendMessage(m.chat, { audio: { url: api.download }, mimetype: 'audio/mpeg' }, { quoted: m });
  }
 
-handler.command = ['ytmp3', 'ytest'];
+handler.help = ['ytmp3'];
+handler.tag = ['descargas'];
+handler.command = ['ytmp3', 'mp3'];
 
 export default handler;
