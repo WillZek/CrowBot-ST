d791f6e6003fd9e554b017d53d25de875499eeb9
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-07 15:26:39 -0400

    Actualizar descargas-soundcloud.js

--- plugins/descargas-soundcloud.js
+++ plugins/descargas-soundcloud.js
@@ -9,11 +9,12 @@ try {
 let api = await fetch(`https://delirius-apiofc.vercel.app/search/soundcloud?q=${text}&limit=10`);
 let json = await api.json();
 
+let link = json[0].data.link;
+
 let api2 = await fetch(`https://delirius-apiofc.vercel.app/download/soundcloud?url=${link}`);
 let json2 = await api2.json();
 
 let { imageURL } = json[0];
-let link = json[0].data.link;
 let audio = json2.data.url;
 
 let txt = `*\`- S O U N C L O U D - M U S I C -\`*\n\n`;
