5c0b2904d2585fcd467797fb9f6c10f2ac85abec
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-07 15:38:01 -0400

    Actualizar descargas-soundcloud.js

--- plugins/descargas-soundcloud.js
+++ plugins/descargas-soundcloud.js
@@ -6,10 +6,10 @@ if (!text) return conn.reply(m.chat, `🎩 Ingrese el nombre de la cancion de *S
 
 await m.react('🕒');
 try {
-let api = await fetch(`https://delirius-apiofc.vercel.app/search/soundcloud?q=${text}&limit=10`);
+let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/soundcloud-search?text=${text}`);
 let json = await api.json();
 
-let link = json[0].data.link;
+let link = json[0].url;
 
 let api2 = await fetch(`https://delirius-apiofc.vercel.app/download/soundcloud?url=${link}`);
 let json2 = await api2.json();
