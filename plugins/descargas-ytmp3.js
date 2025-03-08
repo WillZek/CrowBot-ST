9761641fbe97ff34504789c9abc576305c0f8039
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-08 15:50:12 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn, args }) => {
 
 if (!args[0]) return m.reply(`🍭 Ingresa Un Link De YouTube.`);
 
-let api = await(await fetch(`https://dark-core-api.vercel.app/api/download/YTMP3?key=dk-vip&url=${args[0]}`)).json();
+let api = await(await fetch(`https://api.fgmods.xyz/api/downloader/ytmp3?url=${args[0]}&quality=480p&apikey=elrebelde21`)).json();
 
-if (!api?.download) return m.reply('No Se  Encontraron Resultados');
+if (!api?.dl_url) return m.reply('No Se  Encontraron Resultados');
 
 let txt = `「✦」𝗧𝗶𝘁𝘂𝗹𝗼: ${api.title}`;
 conn.reply(m.chat, txt, m, rcanal);
 
-conn.sendMessage(m.chat, { audio: { url: api.download }, mimetype: 'audio/mpeg' }, { quoted: m });
+conn.sendMessage(m.chat, { audio: { url: api.dl_url }, mimetype: 'audio/mpeg' }, { quoted: m });
  }
 
 handler.help = ['ytmp3'];
