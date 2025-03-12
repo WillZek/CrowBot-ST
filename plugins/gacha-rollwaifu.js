ece57a48505cbfbfc1ade9143cc5f63b4d9eef9e
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-11 21:59:33 -0400

    ✨

--- plugins/gacha-rollwaifu.js
+++ plugins/gacha-rollwaifu.js
@@ -91,9 +91,9 @@ ID: *${randomCharacter.id}*`;
     }
 };
 
-handler.help = ['ver', 'rw', 'rollwaifu'];
+handler.help = ['rollwaifu'];
 handler.tags = ['gacha'];
-handler.command = ['ver', 'rw', 'rollwaifu'];
+handler.command = ['rw', 'rollwaifu'];
 handler.group = true;
 handler.register = true;
 
