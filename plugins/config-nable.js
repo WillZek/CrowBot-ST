e4f2ae556236314f32c03894bb9c0396575e7204
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-20 21:17:35 -0400

    Actualizar config-nable.js

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -209,6 +209,20 @@ break
       bot.autobio = isEnable
       break
 
+    case 'autoresponder':
+    case 'autorespond':
+      if (!m.isGroup) {
+        if (!isOwner) {
+          global.dfail('group', m, conn);
+          throw false;
+        }
+      } else if (!isAdmin) {
+        global.dfail('admin', m, conn);
+        throw false;
+      }
+      chat.autoresponder = isEnable;
+      break;
+
   case 'jadibotmd':
     case 'serbot':
     case 'subbots':
