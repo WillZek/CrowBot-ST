8b416fbab1908f9f515265c541a4930e3a092d5b
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-23 14:38:10 -0400

    Actualizar descargas-tiktokmp3.js

--- plugins/descargas-tiktokmp3.js
+++ plugins/descargas-tiktokmp3.js
@@ -1,10 +1,43 @@
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw m.reply(`Ingresa un link de Tiktok\n*✧ Ejemplo:* ${usedPrefix}${command} https://vm.tiktok.com/ZMhAk8tLx/`);
+conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
+
+  let d2 = await fetch(`https://eliasar-yt-api.vercel.app/api/search/tiktok?query=${text}`)
+  let dp = await d2.json()
+      const doc = {
+      audio: { url: dp.results.audio },
+      mimetype: 'audio/mp4',
+      fileName: `ttbykeni.mp3`,
+      contextInfo: {
+        externalAdReply: {
+          showAdAttribution: true,
+          mediaType: 2,
+          mediaUrl: text,
+          title: dp.results.title,
+          sourceUrl: text,
+          thumbnail: await (await conn.getFile(dp.results.thumbnail)).data
+        }
+      }
+    };
+    await conn.sendMessage(m.chat, doc, { quoted: m })
+await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }});
+}
+handler.help = ['tiktokmp3 *<url>*']
+handler.tags = ['descargas']
+handler.command = /^(tiktokmp3|ttmp3)$/i
+
+export default handler
+
 /* Tiktok MP3 By WillZek 
 - >> https://github.com/WillZek
 */
 
 // [💥] 𝗧𝗜𝗞𝗧𝗢𝗞 𝗠𝗣3
 
-import fetch from 'node-fetch';
+/* import fetch from 'node-fetch';
 
 let handler = async(m, { conn, args, usedPrefix, command }) => {
 
@@ -37,3 +70,4 @@ m.react('✖️');
 handler.command = ['tiktokmp3', 'ttmp3'];
 
 export default handler;
+*/
