0368f2ca55ada1a457d2e5df1ada6d4c6b0af820
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-23 22:52:19 -0400

    Actualizar jadibot-bots.js

--- plugins/jadibot-bots.js
+++ plugins/jadibot-bots.js
@@ -1,46 +1,25 @@
-import fs from 'fs'
 import ws from 'ws'
 
-async function handler(m, { conn: stars, usedPrefix }) {
-  let uniqueUsers = new Map()
+let handler = async (m, { conn }) => {
+   let uniqueUsers = new Map()
 
-  if (!global.conns || !Array.isArray(global.conns)) {
-    global.conns = []
-  }
+   if (!global.conns || !Array.isArray(global.conns)) {
+     global.conns = []
+   }
 
-  global.conns.forEach((conn) => {
-    if (conn.user && conn.ws?.socket?.readyState !== ws.CLOSED) {
-      uniqueUsers.set(conn.user.jid, conn)
-    }
-  })
+   global.conns.forEach((conn) => {
+     if (conn.user && conn.ws?.socket?.readyState !== ws.CLOSED) {
+       uniqueUsers.set(conn.user.jid, conn)
+     }
+   })
 
-  let users = [...uniqueUsers.values()]
-  let totalUsers = uniqueUsers.size
+   let totalUsers = uniqueUsers.size
+   let txt = '*`🍭 *Subbots Activos:`*' + ` » *${totalUsers || 0}*`
 
-  let img = 'https://i.ibb.co/HT2NNf6r/file.jpg';
-
-  let message = users.map((v, index) => `
-*[ \`${index + 1}\` - ${v.user.name || 'Sin Nombre'} ]*
-🤍 *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}
-`).join('\n\n')
-
-  let responseMessage = `🍭 *Subbots Activos: ${totalUsers}*\n\n${message.trim() || '_No hay subbots activos en este momento._'}``
-
-  await stars.sendFile(
-    m.chat,
-    img,
-    'thumbnail.jpg',
-    responseMessage,
-    m,
-    null,
-    fake,
-    false,
-    { mentions: stars.parseMention(responseMessage) }
-  )
+   await conn.reply(m.chat, txt, m, rcanal)
 }
 
 handler.command = ['listjadibot', 'bots']
 handler.help = ['bots']
 handler.tags = ['serbot']
-
 export default handler
