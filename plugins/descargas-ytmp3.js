c26901649a80c049580196daf19e582feb4ee513
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-27 17:33:55 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -4,14 +4,15 @@ let handler = async (m, { conn, args }) => {
 
 if (!args[0]) return m.reply(`🍭 Ingresa Un Link De YouTube.`);
 
-let api = await(await fetch(`https://api.agungny.my.id/api/youtube-audio?url=${args[0]}`)).json();
+let api = await(await fetch(`https://api.neoxr.eu/api/youtube?url=${args[0]}&type=audio&quality=128kbps&apikey=GataDios`)).json();
 
 // if (!api?.result?.dl_url) return m.reply('No Se  Encontraron Resultados');
 
-let txt = `「✦」𝗧𝗶𝘁𝘂𝗹𝗼: ${api.result.result.title}`;
+/* let txt = `「✦」𝗧𝗶𝘁𝘂𝗹𝗼: ${api.result.result.title}`;
 conn.reply(m.chat, txt, m, rcanal);
+*/
 
-conn.sendMessage(m.chat, { audio: { url: api.result.result.download }, mimetype: 'audio/mpeg' }, { quoted: m });
+conn.sendMessage(m.chat, { audio: { url: api.data.url }, mimetype: 'audio/mpeg' }, { quoted: m });
  }
 
 handler.help = ['ytmp3'];
