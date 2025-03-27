ce2645acff725ad951ccc2345259e3accd8d3f89
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-27 17:34:42 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -4,6 +4,7 @@ let handler = async (m, { conn, args }) => {
 
 if (!args[0]) return m.reply(`🍭 Ingresa Un Link De YouTube.`);
 
+m.react('🕒')
 let api = await(await fetch(`https://api.neoxr.eu/api/youtube?url=${args[0]}&type=audio&quality=128kbps&apikey=GataDios`)).json();
 
 // if (!api?.result?.dl_url) return m.reply('No Se  Encontraron Resultados');
@@ -13,6 +14,7 @@ conn.reply(m.chat, txt, m, rcanal);
 */
 
 conn.sendMessage(m.chat, { audio: { url: api.data.url }, mimetype: 'audio/mpeg' }, { quoted: m });
+m.react(done)
  }
 
 handler.help = ['ytmp3'];
