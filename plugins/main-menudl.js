77f79c5dfdfdd8239db771a146253b558fff2918
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-04 17:09:48 -0600

    Actualizar main-menudl.js

--- plugins/main-menudl.js
+++ plugins/main-menudl.js
@@ -24,7 +24,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'descargas': 'MenuDescargas',
 }
-let img = 'https://files.catbox.moe/w3s79j.jpg'
+// let img = 'https://files.catbox.moe/w3s79j.jpg'
+let img = './media/menus/Menu.jpg';
 
   try {
           // DEFAULT MENU
