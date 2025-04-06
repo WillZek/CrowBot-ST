027dc01f861bba7ec2fe353d277871ca77f14537
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-05 20:14:23 -0600

    Actualizar descargas-spotifydl.js

--- plugins/descargas-spotifydl.js
+++ plugins/descargas-spotifydl.js
@@ -11,7 +11,7 @@ import fetch from 'node-fetch';
 
 let MF = async (m, { conn, args, command, usedPrefix }) => {
 
-if (!args[0]) return m.reply(`🌙 INGRESE UN Link De Spotify\n> *Ejemplo:* ${usedPrefix + command} https://open.spotify.com/track/0jH15Y9z2EpwTWRQI11xbj`);
+if (!args[0]) return m.reply(`🌙 Ingrese Un Link De Spotify\n> *Ejemplo:* ${usedPrefix + command} https://open.spotify.com/track/0jH15Y9z2EpwTWRQI11xbj`);
 
 let api = await (await fetch(`https://archive-ui.tanakadomp.biz.id/download/spotify?url=${args[0]}`)).json();
 
