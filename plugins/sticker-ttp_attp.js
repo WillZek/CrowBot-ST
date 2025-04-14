2941830a51f53ad92aa95ad10b064351f11fa597
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-14 16:34:41 -0600

    Actualizar sticker-ttp_attp.js

--- plugins/sticker-ttp_attp.js
+++ plugins/sticker-ttp_attp.js
@@ -6,11 +6,11 @@ let handler = async(m, { conn, text, args, usedPrefix, command }) => {
     if (!text) return m.reply(`《★》Ingresa Un Texto Para Realizar Tu Sticker\n> *Ejemplo:* ${usedPrefix + command} CrowBot`)
 
     if (command == 'attp') {
-let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/attp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
+let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/attp?text=${text}&apikey=dylux`,global.packname, global.author)
 conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: channel, thumbnail: imagen1 }}}, { quoted: m })}
 
     if (command == 'ttp') {
-let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/ttp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
+let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/ttp?text=${text}&apikey=dylux`,global.packname, global.author)
 conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: imagen1 }}}, { quoted: m })
 }
 }
