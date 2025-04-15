4c2d57167152f9fc1674b6f949e9b00bb2fbabc5
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-14 22:52:07 -0600

    Crear ff-reglasLideres.js

---
+++ plugins/ff-reglasLideres.js
@@ -0,0 +1,28 @@
+let handler = async (m, { conn }) => {
+
+let img = 'https://telegra.ph/file/f49d69baf52c38286c16b.jpg' // este link todavía sirve alv
+ 
+let texto = `» 𝙍𝙀𝙂𝙇𝘼𝙎 𝙇𝙄𝘿𝙀𝙍𝙀𝙎 𝘽𝘼𝙎𝙄𝘾𝘼𝙎`
+
+const fkontak = {
+        "key": {
+    "participants":"0@s.whatsapp.net",
+                "remoteJid": "status@broadcast",
+                "fromMe": false,
+                "id": "Halo"
+        },
+        "message": {
+                "contactMessage": {
+                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                }
+        },
+        "participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
+}
+
+handler.command = ['reglaslideres'] 
+handler.register = true
+handler.admin = true
+
+export default handler
