643e2380998f809519a6493cdec480cef3309712
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-15 09:22:04 -0600

    Actualizar tools-hd.js

--- plugins/tools-hd.js
+++ plugins/tools-hd.js
@@ -1,52 +1,39 @@
+let handler = async (m, { conn }) => {
+      if (!m.quoted) return m.reply(`🌙 Responde a una imagen.`);
+      const imageBuffer = await m.quoted.download();
+      m.react("🕑");
+      try {
+         const r = await Upscale(imageBuffer);
+         if (!r) return m.reply("Error al mejorar la imagen.");
 
-import FormData from "form-data";
-import Jimp from "jimp";
-const handler = async (m, {conn, usedPrefix, command}) => {
- try {    
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) return m.reply(`🚩 Envie una imagen o responda a la imagen utilizando el comando: ${usedPrefix + command}`);
-  if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`🍂 El formato del archivo (${mime}) no es compatible, envía o responda a una imagen`);
-  conn.reply(m.chat, '🚩 Mejorando la calidad de la imagen....', m, {
-  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-  title: packname,
-  body: wm,
-  previewType: 0, thumbnail: icons,
-  sourceUrl: channel }}})
-  let img = await q.download?.();
-  let pr = await remini(img, "enhance");
-  conn.sendMessage(m.chat, {image: pr}, {quoted: fkontak});
- } catch {
- return m.reply("🚩 Ocurrió un error");
- }
-};
-handler.help = ["remini", "hd", "enhance"];
+         await conn.sendFile(m.chat, r, 'image.jpg', '', m);
+         m.react("✅");
+      } catch (e) {
+         console.error(e);
+         m.reply("Ocurrió un error al procesar la imagen.");
+      }
+   }
+
+handler.command = ["hd", "hdr", "remini"]
+handler.help = ["remini"];
 handler.tags = ["tools"];
-handler.command = ["remini", "hd", "enhance"];
-export default handler;
 
-async function remini(imageData, operation) {
-  return new Promise(async (resolve, reject) => {
-    const availableOperations = ["enhance", "recolor", "dehaze"];
-    if (availableOperations.includes(operation)) {
-      operation = operation;
-    } else {
-      operation = availableOperations[0];
-    }
-    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
-    const formData = new FormData();
-    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
-    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
-    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
-      function (err, res) {
-        if (err) reject(err);
-        const chunks = [];
-        res.on("data", function (chunk) {chunks.push(chunk)});
-        res.on("end", function () {resolve(Buffer.concat(chunks))});
-        res.on("error", function (err) {
-        reject(err);
-        });
-      },
-    );
-  });
+export default handler
+
+async function Upscale(imageBuffer) {
+   try {
+      const response = await fetch("https://lexica.qewertyy.dev/upscale", {
+         body: JSON.stringify({
+            image_data: imageBuffer.toString("base64"),
+            format: "binary",
+         }),
+         headers: { "Content-Type": "application/json" },
+         method: "POST",
+      });
+      if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+      return Buffer.from(await response.arrayBuffer());
+   } catch (e) {
+      console.error("Error en Upscale():", e);
+      return null;
+   }
 }
