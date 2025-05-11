4c5cbe86f4990b2be3eaab20b5aba652c7574775
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-05-11 12:43:49 -0600

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -1,48 +1,27 @@
-/* let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+// By Destroy >> https://github.com/The-King-Destroy/
 
-let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
+let linkRegex = /https:\/\/chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i;
 
-if (!text) return m.reply(`🌸 Ingresa el enlace del Grupo.`)
-try {
-let [_, code] = text.match(linkRegex) || []
-if (!code) return m.reply('🌺 Enlace invalido.')
-let res = await conn.groupAcceptInvite(code)
-m.reply(`🚩 Me uní correctamente al Grupo`)
-} catch (e) {
-return m.reply(`✘ Ocurrió un error. ${e.message}`)}}
+let handler = async (m, { conn, text, isOwner }) => {
+    if (!text) return m.reply(`${emoji} Debes enviar una invitacion para que *${botname}* se una al grupo.`);
 
-handler.help = ['join <link>']
-handler.tags = ['owner']
-handler.command = ['join', 'entrar'] 
-handler.rowner = true
+    let [_, code] = text.match(linkRegex) || [];
 
-export default handler
-*/
+    if (!code) return m.reply(`${emoji2} Enlace de invitación no válido.`);
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+    if (isOwner) {
+        await conn.groupAcceptInvite(code)
+            .then(res => m.reply(`${emoji} Me he unido exitosamente al grupo.`))
+            .catch(err => m.reply(`${msm} Error al unirme al grupo.`));
+    } else {
+        let message = `${emoji} Invitación a un grupo:\n${text}\n\nPor: @${m.sender.split('@')[0]}`;
+        await conn.sendMessage(`${suittag}` + '@s.whatsapp.net', { text: message, mentions: [m.sender] }, { quoted: m });
+        m.reply(`${emoji} El link del grupo ha sido enviado, gracias por tu invitacion. ฅ^•ﻌ•^ฅ`);
+    }
+};
 
-let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
+handler.help = ['invite'];
+handler.tags = ['owner', 'tools'];
+handler.command = ['invite', 'join'];
 
-        if (!text) return m.reply(`🚩 Ingresa el enlace del Grupo.\n> *Ejemplo:* ${usedPrefix + command} <enlace> <número de días>.`)
-    let [_, code, expired] = text.match(linkRegex) || []
-    if (!code) return m.reply('🚩 Enlace invalido.')
-    let res = await conn.groupAcceptInvite(code)
-    expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`🚩 Me uní correctamente al Grupo Durante *${expired}* días.`)
-    let chats = global.db.data.chats[res]
-    if (!chats) chats = global.db.data.chats[res] = {}
-    if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
-/*  const pp = './media/menus/Menu3.jpg';
-  conn.sendFile(m.chat, pp, 'error.jpg', text, m, false);
-*/
-}
-
-handler.help = ['join *<link> <días>*']
-handler.tags = ['owner']
-
-handler.command = ['join', 'entrar'] 
-handler.owner = true
-
-export default handler
-
-const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
+export default handler;
